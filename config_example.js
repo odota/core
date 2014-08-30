9cf0b8e888f774fd66a71f73b7fa1091ceff1c09
albertcui <albertqcui+github@gmail.com>
2014-08-30 02:10:30 +0000

    add example config file

---
+++ config_example.js
@@ -0,0 +1,26 @@
+var config = {};
+
+config.steam_name = "";
+config.steam_user = "";
+config.steam_pass = "";
+config.steam_guard_code = "";
+config.cwd = "/home/codio/workspace/Dota2ApiGetter/"; // Because node-forever is silly.
+
+config.request_timeout = 1000 * 30;
+config.steam_response_timeout = 1000 * 30;
+
+config.mongodb_host = "localhost";
+config.mongodb_port = 27017;
+
+config.steam_api_key = "";
+
+config.account_ids = [
+    "102344608"
+];
+
+//Number of matches to request per API call
+config.matchCount = 2;
+
+config.replaysFolder = "";
+
+module.exports = config;
