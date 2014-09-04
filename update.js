7c87faa7404706d6ad8a74d8a1c3f9c72472a001
howardc93 <howardc93@gmail.com>
2014-09-04 22:08:34 +0000

    adapted update utility, added foreman
    Former-commit-id: 719acb764eccd13bdbbbe3bd113cbb27aeff8f21

---
+++ update.js
@@ -0,0 +1,30 @@
+/*
+ * Utility for re-parsing matches already in db after a parser update without re-downloading files
+ */
+var fs = require('fs'),
+    async = require('async'),
+    spawn = require('child_process').spawn;
+
+var files = fs.readdirSync("./replays/")
+
+function parseFile(file, cb) {
+    var cp = spawn(
+        "java",
+        ["-jar",
+         "./parser/target/stats-0.1.0.jar",
+         "./replays/" + file
+        ]
+    );
+
+    cp.stderr.on('data', function (data) {
+        cb(data)
+    });
+
+    cp.on('close', function (code) {
+        cb(null, code)
+    });
+}
+
+async.eachSeries(files, parseFile, function(err) {
+    console.log(err)
+})
