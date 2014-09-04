6b1a17e1298d78a80bd038f42b9cb3ffbde39f02
albertcui <albertqcui+github@gmail.com>
2014-09-04 03:23:48 +0000

    remove logging, add config for parser path
    Former-commit-id: c6b9dfa37ffb5aaddf010313308afa24e5049758

--- config_example.js
+++ config_example.js
@@ -6,6 +6,17 @@ config.steam_pass = "";
 config.steam_guard_code = "";
 config.steam_api_key = "";
 
+config.account_ids = [""];
+
+config.matchCount = 5;
+
+config.replaysFolder = "";
+
+config.parserPath = "";
+config.logFile = "";
+config.logEmail = "";
+
+
 config.request_timeout = 1000 * 30;
 config.steam_response_timeout = 1000 * 30;
 
