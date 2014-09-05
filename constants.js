52b218dbef1b59bae637f43b33e25162ca2a569e
albertcui <albertqcui+github@gmail.com>
2014-09-05 03:00:40 +0000

    add constatns file
    Former-commit-id: ae9edfb09dd04f08a659addeeed876a03ecd4dff

---
+++ constants.js
@@ -0,0 +1,21 @@
+var constants = exports;
+
+constants.gameModes = {
+        "0": "None",
+        "1": "All Pick",
+        "2": "Captain's Mode",
+        "3": "Random Draft",
+        "4": "Single Draft",
+        "5": "All Random",
+        "6": "Intro",
+        "7": "Diretide",
+        "8": "Reverse Captain's Mode",
+        "9": "The Greeviling",
+        "10": "Tutorial",
+        "11": "Mid Only",
+        "12": "Least Played",
+        "13": "New Player Pool",
+        "14": "Compendium Matchmaking",
+        "16": "Captain's Draft",
+    	"18": "Ability Draft"
+    };
