c78d187460c0a8a10e920ebaf2a9ccf05da44f9c
howardc93 <howardc93@gmail.com>
2014-09-06 08:37:19 +0000

    just a staging point

--- MatchService.js
+++ MatchService.js
@@ -1,7 +1,7 @@
 var request = require('request'),
     path = require("path"),
     fs = require("fs"),
-    http = require('http'),
+    async = require("async"),
     Steam = require("./MatchProvider-steam").MatchProvider,
     spawn = require('child_process').spawn,
     constants = require('./constants.json'),
@@ -17,7 +17,8 @@ var steam = new Steam(
     process.env.STEAM_RESPONSE_TIMEOUT);
 
 matches.index('match_id', {unique: true});
-setInterval(getMatches, constants.match_poll_interval);
+
+setInterval(poll, constants.match_poll_interval);
 
 /**
  * Generates Match History URL
@@ -36,59 +37,76 @@ function generateGetMatchDetailsURL(match_id) {
     + "&match_id=" + match_id;
 }
 
+function poll() {
+    //TODO use a db instead of array here
+    var account_ids = ["102344608", "88367253"];
+    async.mapSeries(account_ids, getNewGames, function(err){
+        console.log ("Looking for unparsed games since %s", moment().subtract(7, 'days').format('X'));
+        matches.find({"playerNames": {$exists:false}},{"start_time":{ $gt: cutoff }}, function(err, docs){
+            console.log('Found %s matches needing parse', docs.length);
+            async.mapLimit(docs, 10, download, function(err, results){
+                async.mapLimit(results, 1, parse, function(err, results){})
+            })
+        })
+
+    })
+});
+}
+
 /**
- * Makes request for match history
+ * Makes request for match history and puts new games in db
  */
-function requestGetMatchHistory(player_id, num) {
-    console.log("requestGetMatchHistory called")
-    request(generateGetMatchHistoryURL(player_id, num), function(err, res, body){
+function getNewGames(player_id, cb) {
+    console.log("requestGetMatchHistory for %s", player_id)
+    request(generateGetMatchHistoryURL(player_id), function(err, res, body){
         if (!err && res.statusCode == 200) {
             var j = JSON.parse(body);
-            j.result.matches.forEach(function(match, i) {
-                matches.findOne({match_id: match.match_id}, function(err, data) {
-                    if (err) throw err
-                    if (!data) {
-                        setTimeout(requestGetMatchDetails, i * 1000, match.match_id)
-                    }
-                })
+            async.mapSeries(j.result.matches, insertMatch, function(err, results){
+                console.log(results);
+            })
+
+        }
+    })
+}
+
+/**
+ * Inserts a new match into the db if it doesn't exist
+ */
+function insertMatch(match, cb){
+    matches.findOne({match_id: match.match_id}, function(data) {
+        if (!data) {
+            console.log("requestGetMatchDetails for %s", match_id)
+            request(generateGetMatchDetailsURL(match_id), function(err, res, body){
+                if (!err && res.statusCode == 200) {
+                    var result = JSON.parse(body).result
+                    matches.insert(result);
+                    cb(null, match_id)
+                }
             })
         }
     })
 }
 
 /**
- * Makes request for match details
+ * Get the replay url for this match, callback with it
  */
-function requestGetMatchDetails(match_id) {
-    console.log("requestGetMatchDetails called")
-    request(generateGetMatchDetailsURL(match_id), function(err, res, body){
-        if (!err && res.statusCode == 200) {
-            var result = JSON.parse(body).result
-            matches.insert(result);
-        }
-    })
-}
-
-/**
- * Gets replay URL, either from steam or from database if we already have it
- */
-function tryToGetReplayUrl(id, cb) {
+function getReplayUrl(id, cb) {
     matches.findOne({match_id: id}, function(err, data){
         // Error occurred
         if (err) {
-            cb(err)
+            cb(err);
         }
         // Already have the replay url
         if (data.replay_url) {
-            cb(false, data.replay_url, parse)
+            cb(null, data.replay_url)
         } 
         else{
             if (steam.ready) {
-                steam.getMatchDetails(id, function(err, data) {
+                steam.getReplayDetails(id, function(err, data) {
                     if (!err && data) {
                         console.log(data)
                         var result={};
-                        result.replay_url = util.format("http://replay%s.valve.net/570/%s_%s.dem.bz2", data.cluster, data.id, data.salt);
+                        result.replay_url = "http://replay"+data.cluster+".valve.net/570/"+data.match_id+"_"+data.replay_salt+".dem.bz2";
                         result.salt = data.salt
                         matches.update({match_id: id}, {$set: result})
                         cb(false, result.replay_url, parse)
@@ -100,7 +118,7 @@ function tryToGetReplayUrl(id, cb) {
                 })
             } 
             else {
-                console.log("steam is not ready")
+                console.log("Steam is not ready")
                 cb(true)
             }
         }
@@ -108,48 +126,46 @@ function tryToGetReplayUrl(id, cb) {
 }
 
 /**
- * Downloads replay file from specified url
+ * Downloads replay file for match, callback with the file name
  */
-function download(err, url, cb) {
-    if (err){ return; }
-    var fileName = "./replays/"+url.substr(url.lastIndexOf("/") + 1).slice(0,-4);
-    if (!fs.existsSync(fileName)){
-        fs.openSync(fileName, 'w')
-        console.log('Downloading file from %s', url)
-        http.get(url, function(res) {
-            if (res.statusCode !== 200) {
-                console.log("[DL] failed to download from %s", url)
-                return;
-            }
-
-            var data = [], dataLen = 0; 
-
-            res.on('data', function(chunk) {
-                data.push(chunk);
-                dataLen += chunk.length;
-            }).on('end', function() {
-                var buf = new Buffer(dataLen);
-                for (var i=0, len = data.length, pos = 0; i < len; i++) { 
-                    data[i].copy(buf, pos); 
-                    pos += data[i].length; 
-                } 
-                var decomp= Bunzip.decode(buf);
+function download(match, cb) {
+    var match_id = match.match_id
+    getReplayUrl(match_id, function(err, url){
+        if (err){
+            cb(err);
+        }
+        var fileName = "./replays/"+url.substr(url.lastIndexOf("/") + 1).slice(0,-4);
+        if (!fs.existsSync(fileName)){
+            fs.openSync(fileName, 'w')
+            console.log('[DL] Downloading file from %s', url)
+            request({url:url, encoding:null}, function (error, response, body) {
+                if (response.statusCode !== 200) {
+                    console.log("[DL] failed to download from %s", url)
+                    //TODO retry by timeout
+                }
+                var decomp= Bunzip.decode(body);
                 console.log('[DL] writing decompressed file %s', fileName);
                 fs.writeFileSync(fileName, decomp);
-                cb(fileName);
+                parse(fileName);
             });
-        });    
-    }
-    else{
-        //file already exists, don't parse this
-        //it could be in mid-parse/mid-download
-        //use the utility to re-parse all present files
-        console.log("%s already exists", fileName)
-    }
-
+        }
+        else{
+            console.log("%s already exists", fileName)
+            parse(fileName);
+        }
+    })
 }
 
+/*
+ * Parses the given file
+ */
 function parse(fileName){
+    //TODO get id from filename
+    //TODO mark parsed
+    var result = {}
+    result.playerNames={}
+    matches.update({match_id: id}, {$set: result})
+
     var parserFile = "./parser/target/stats-0.1.0.jar";
 
     console.log("[PARSER] starting parse: %s", fileName);
@@ -162,7 +178,7 @@ function parse(fileName){
     );
 
     cp.stdout.on('data', function (data) {
-        //TODO JSON output of parse should output here
+        //TODO output of parse should output here
         console.log('[PARSER] stdout: %s - %s', data, fileName);
     });
 
@@ -175,28 +191,4 @@ function parse(fileName){
         //maybe delete/move the replay file too
         console.log('[PARSER] exited with code %s - %s', code, fileName);
     }); 
-}
-
-function getMatches() {
-    //TODO add a trackedUsers database to determine users to follow
-    var account_ids = ["102344608", "88367253"];
-    account_ids.forEach(function(id) {
-        requestGetMatchHistory(id, 10);
-    });
-    parseNewReplays();
-}
-
-function parseNewReplays() {
-    var cutoff = moment().subtract(7, 'days').format('X')
-    console.log ("Looking for unparsed games since %s", cutoff);
-    matches.find({"playerNames": {$exists:false}},{"start_time":{ $gt: cutoff }}, function(err, docs){
-        if (err) throw err;
-        else {
-            console.log('Found %s matches needing parse', docs.length);
-            docs.forEach(function(doc, i) {
-                console.log(doc.match_id)
-                tryToGetReplayUrl(doc.match_id, download);
-            })
-        }
-    });
 }
--- util.js
+++ util.js
@@ -1,39 +1,10 @@
-var util = exports;
-var request = require('request');
-var constants = require('./constants.json');
-var fs = require('fs');
+var util = exports
+var request = require('request')
+var constants = require('./constants.json')
+var fs = require('fs')
 
-util.db = require('monk')(process.env.MONGOHQ_URL || "localhost/dota"),
-    matches = util.db.get('matchStats');
-
-util.getMatch = function(id) {
-    return matches.findOne({"match_id": id})
-}
-
-util.getAllMatches = function() {
-    return matches.find({}, {sort: {match_id: -1}})
-}
-
-util.extractPlayerInfo = function(match) {
-    var playerInfoList= []
-    for (var j=0; j<match.players.length;j++){
-        var player = match.players[j];
-        var playerInfo = {};
-        playerInfo.hero = constants.heroes[player.hero_id.toString()]
-        playerInfo.items= []
-        for(var i = 0; player["item_" + i] !== undefined; i++) {
-            playerInfo.items.push(constants.items[player["item_" + i].toString()])
-        }
-        if (player.additional_units) {
-            playerInfo.additional_items=[];
-            for(var i = 0; player.additional_units[0]["item_" + i] !== undefined; i++) {
-                playerInfo.additional_items.push(constants.items[player.additional_units[0]["item_" + i].toString()])
-            }	    
-        }
-        playerInfoList.push(playerInfo);
-    }
-    return playerInfoList
-}
+util.db = require('monk')(process.env.MONGOHQ_URL || "localhost/dota");
+var matches = util.db.get('matchStats');
 
 /**
  * Gets a single match from db
@@ -48,70 +19,3 @@ util.getMatch = function(id) {
 util.getAllMatches = function() {
     return matches.find({}, {sort: {match_id: -1}})
 }
-
-util.updateHeroes = function updateHeroes(cb){
-    request("https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key="+process.env.STEAM_API_KEY+"&language=en-us", function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log("updating heroes");
-            var array = JSON.parse(body).result.heroes;
-            var lookup={}
-            for (var i = 0; i < array.length;i++) {
-                lookup[array[i].id] = array[i];
-            }
-            constants.heroes = lookup;
-            cb()
-        }
-    })
-}
-
-util.updateItems = function updateItems(cb){
-    request("http://www.dota2.com/jsfeed/itemdata", function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log("updating items");
-            var objects = JSON.parse(body).itemdata;
-            var lookup={}
-            for(var key in objects) {
-                lookup[objects[key].id] = objects[key];
-            }
-            constants.items = lookup;
-            cb()
-        }
-    })
-}
-
-util.writeConstants = function writeConstants(cb){    
-    console.log("writing constants file");
-    fs.writeFileSync("./constants.json", JSON.stringify(constants, null, 4));
-    cb()
-}
-
-/*
- * Utility for re-parsing all downloaded replays
-var fs = require('fs'),
-    async = require('async'),
-    spawn = require('child_process').spawn;
-
-var files = fs.readdirSync("./replays/")
-
-function parseFile(file, cb) {
-    var cp = spawn(
-        "java",
-        ["-jar",
-         "./parser/target/stats-0.1.0.jar",
-         "./replays/" + file
-        ]
-    );
-
-    cp.stderr.on('data', function (data) {
-        cb(data)
-    });
-
-    cp.on('close', function (code) {
-        cb(null, code)
-    });
-}
-
-async.eachSeries(files, parseFile, function(err) {
-    console.log(err)
-})
- */
