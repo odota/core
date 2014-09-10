2dafd40cdf9cb0179d776e2d98fb5ab11657fc97
howardc93 <howardc93@gmail.com>
2014-09-10 15:11:39 +0000

    added full dump of teammate history, dotabuff probably hates me

--- utility.js
+++ utility.js
@@ -2,12 +2,15 @@ var utility = exports
 var request = require('request')
 var async = require('async')
 var BigNumber = require('big-number').n
+var $ = require('cheerio')
 
 utility.db = require('monk')(process.env.MONGOHQ_URL || "localhost/dota");
 utility.matches = utility.db.get('matchStats');
 utility.matches.index('match_id', {unique: true});
 utility.players = utility.db.get('players');
-utility.players.index('account_id', {unique: true});
+utility.players.index('account_id', {unique: true})
+utility.dotabuffMatches = utility.db.get('dotabuffMatches')
+utility.dotabuffMatches.index('match_id', {unique: true})
 
 utility.getData = function(url, cb){
     request(url, function (err, res, body) {
@@ -55,4 +58,111 @@ utility.getMatch = function(id) {
  */
 utility.getAllMatches = function() {
     return utility.matches.find({"duration":{$exists:true}}, {sort: {match_id: -1}})
+}
+
+var host = "http://www.dotabuff.com"
+/*
+ * Gets the teammate counts for a particular player
+ */
+utility.getCounts =function(account_id, paginate, callback) {
+    utility.getMatches(host+"/players/"+account_id+"/matches", paginate, function(err){
+        utility.dotabuffMatches.find({players: { $elemMatch: { id: account_id }}}, function(err, data){
+            var counts = {};
+            for (i=0;i<data.length;i++){
+                for (j=0;j<data[i].players.length; j++){
+                    var player = data[i].players[j]
+                    if (player.id==account_id){
+                        var playerResult = player.winner;
+                    }
+                }
+                for (j=0;j<data[i].players.length; j++){
+                    var player = data[i].players[j]
+                    if (player.winner == playerResult){
+                        if (!counts[player.id]){
+                            counts[player.id]={};
+                            counts[player.id]["win"]=0;
+                            counts[player.id]["lose"]=0;
+                        }
+                        if (player.winner){
+                            counts[player.id]["win"]+=1;
+                        }
+                        else{
+                            counts[player.id]["lose"]+=1;
+                        }
+                    }
+                }
+            }
+            callback(counts);
+        })
+    })
+}
+
+utility.getMatches=function(player_url, paginate, callback){
+    request(player_url, function processMatches (err, resp, html) {
+        console.log(player_url)
+        if (err) throw err
+        var parsedHTML = $.load(html);
+        var matchCells = parsedHTML('td[class=cell-xlarge]')
+        var vals = [];
+        var i = 0
+        while(matchCells[i.toString]){
+            vals.push(matchCells[i.toString()])
+            i++
+        }
+        async.map(vals, function(matchCell, cb){
+            var match_url = host+$(matchCell).children().first().attr('href'); 
+            console.log(match_url)
+            utility.dotabuffMatches.findOne({match_id: getIdFromPath(match_url)}, function(err, data) {
+                if (err) throw err
+                if (!data) {
+                    request(match_url, function (err, resp, body) {
+                        if (err) throw err
+                        var matchHTML = $.load(body)
+                        var radiant_win=matchHTML('.match-result').hasClass('radiant');
+                        var match = {};
+                        match.match_id = getIdFromPath(match_url);
+                        match.players=[];
+                        matchHTML('a[class=player-radiant]').map(function(i, link) {
+                            player={};
+                            player.id=getIdFromPath($(link).attr('href'));
+                            player.winner=radiant_win;
+                            match.players.push(player);
+                        })
+                        matchHTML('a[class=player-dire]').map(function(i, link) {
+                            player={};
+                            player.id=getIdFromPath($(link).attr('href'));
+                            player.winner=!radiant_win;
+                            match.players.push(player);                        
+                        })
+                        utility.dotabuffMatches.insert(match);
+                        cb(null)
+                    })
+                }
+                else{
+                    cb(null)
+                }
+            })   
+        }, function(err){
+            var nextPath = parsedHTML('a[rel=next]').first().attr('href')
+            if (paginate && nextPath){
+                console.log("going to next page")
+                utility.getMatches(host+nextPath, true, callback);
+            }
+            else{
+                callback(null)
+            }  
+        })
+    })
+}
+
+function getIdFromPath(input){
+    return Number(input.split(/[/]+/).pop());
+}
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
 }
