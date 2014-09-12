69950761b23abae7d88407acc276c5442e16f27d
howardc93 <howardc93@gmail.com>
2014-09-12 12:17:23 +0000

    fixes #11
    Former-commit-id: 4ee48e48b46f8298b0f0fb689972f9d1e0434a15

--- backend.js
+++ backend.js
@@ -11,33 +11,26 @@ var request = require('request'),
     players = utility.players,
     AWS = require('aws-sdk'),
     BigNumber = require('big-number').n,
-    $ = require('cheerio')
-
-
-var steam = new Steam(
-    process.env.STEAM_USER,
-    process.env.STEAM_PASS,
-    process.env.STEAM_GUARD_CODE);
-
+    $ = require('cheerio'),
+    steam;
 var aq = async.queue(apiRequest, 1)
 var pq = async.queue(parseReplay, 1)
 var api_delay = 1000
 var replay_dir = process.env.REPLAY_DIR || "replays/"
 var parser_file = process.env.PARSER_FILE || "./parser/target/stats-0.1.0.jar"
 var api_url = "https://api.steampowered.com/IDOTA2Match_570"
-var summaries_url =  "http://api.steampowered.com/ISteamUser"
+var summaries_url = "http://api.steampowered.com/ISteamUser"
 var host = "http://www.dotabuff.com"
-
-if (!fs.existsSync(replay_dir)){
+if(!fs.existsSync(replay_dir)) {
     fs.mkdir(replay_dir)
 }
-aq.empty = function(){
+aq.empty = function() {
     queueRequests()
 }
-pq.empty = function(){
+pq.empty = function() {
     parseMatches()
 }
-setInterval(updateAllPlayers, 86400*1000)
+setInterval(updatePlayerNames, 86400 * 1000)
 queueRequests()
 parseMatches()
 //todo insert parsed data under its own key
@@ -45,89 +38,102 @@ parseMatches()
 //todo update views to work with new format
 //todo implement cool dynatables/pagination
 
-function updateAllPlayers(){
+function updatePlayerNames() {
     //daily, go through all the matches and update the players
     //todo albert implement this
 }
 
-function queueRequests(){
-    players.find({track: 1}, function(err,docs){
-        aq.push(docs, function(err){})
+function queueRequests() {
+    players.find({
+        track: 1
+    }, function(err, docs) {
+        aq.push(docs, function(err) {})
     })
-    matches.find({"duration":{$exists:false}}, function(err,docs){
-        aq.push(docs, function(err){})
+    matches.find({
+        "duration": {
+            $exists: false
+        }
+    }, function(err, docs) {
+        aq.push(docs, function(err) {})
     })
 }
 
-function parseMatches(){
-    matches.find({parse_status : 0}, function(err, docs) {
-        pq.push(docs, function (err){})
-    })    
+function parseMatches() {
+    matches.find({
+        parse_status: 0
+    }, function(err, docs) {
+        pq.push(docs, function(err) {})
+    })
 }
 
-function createSummaryRequest(players){
+function createSummaryRequest(players) {
     summaries = {}
-    summaries.summaries_id=1
-    summaries.players=players
+    summaries.summaries_id = 1
+    summaries.players = players
     return summaries
 }
 
 function generateURL(req) {
-    if (req.account_id){
-        return api_url + "/GetMatchHistory/V001/?key=" + process.env.STEAM_API_KEY
-        + "&account_id=" + req.account_id
-        + "&matches_requested=" + (process.env.MATCHES_REQUESTED || 10)
+    if(req.account_id) {
+        return api_url + "/GetMatchHistory/V001/?key=" + process.env.STEAM_API_KEY + "&account_id=" + req.account_id + "&matches_requested=" + (process.env.MATCHES_REQUESTED || 10)
     }
-    if (req.match_id){
-        return api_url + "/GetMatchDetails/V001/?key=" + process.env.STEAM_API_KEY
-        + "&match_id=" + req.match_id;
+    if(req.match_id) {
+        return api_url + "/GetMatchDetails/V001/?key=" + process.env.STEAM_API_KEY + "&match_id=" + req.match_id;
     }
-    if (req.summaries_id){
-        var steamids=[]
-        req.players.forEach(function(player){
-            var steamid64=BigNumber('76561197960265728').plus(player.account_id).toString()
+    if(req.summaries_id) {
+        var steamids = []
+        req.players.forEach(function(player) {
+            var steamid64 = BigNumber('76561197960265728').plus(player.account_id).toString()
             steamids.push(steamid64)
         })
         var query = steamids.join()
-        return summaries_url + "/GetPlayerSummaries/v0002/?key="+process.env.STEAM_API_KEY+"&steamids="+query  
+        return summaries_url + "/GetPlayerSummaries/v0002/?key=" + process.env.STEAM_API_KEY + "&steamids=" + query
     }
 }
 
-function apiRequest(req, cb){
+function apiRequest(req, cb) {
     console.log('[QUEUES] %s api, %s parse', aq.length(), pq.length())
-    utility.getData(generateURL(req), function(err, data){
-        if (err) {return cb(err)}
-        if (req.full_history){
-            getFullMatchHistory(req.account_id, function(err){
+    utility.getData(generateURL(req), function(err, data) {
+        if(err) {
+            return cb(err)
+        }
+        if(req.full_history) {
+            getFullMatchHistory(req.account_id, function(err) {
                 setTimeout(cb, api_delay, null)
             })
         }
-        if (req.account_id){
+        if(req.account_id) {
             console.log("[API] games for player %s", req.account_id)
-            async.map(data.result.matches, insertMatch, function(err){
+            async.map(data.result.matches, insertMatch, function(err) {
                 setTimeout(cb, api_delay, null)
             })
         }
-        if (req.match_id){
+        if(req.match_id) {
             var match = data.result
             console.log("[API] details for match %s", match.match_id)
-            matches.update({match_id:req.match_id},{$set:match})
-            aq.unshift(createSummaryRequest(match.players), function(err){})
-            pq.push(match, function(err){})
+            matches.update({
+                match_id: req.match_id
+            }, {
+                $set: match
+            })
+            aq.unshift(createSummaryRequest(match.players), function(err) {})
+            pq.push(match, function(err) {})
             setTimeout(cb, api_delay, null)
         }
-        if (req.summaries_id){
+        if(req.summaries_id) {
             console.log("[API] summaries for players (batch)")
-            async.map(data.response.players, insertPlayer, function(err){
+            async.map(data.response.players, insertPlayer, function(err) {
                 setTimeout(cb, api_delay, null)
             })
         }
     })
 }
 
-function insertMatch(match, cb){
-    matches.findOne({ match_id: match.match_id }, function(err, doc) {
-        if(!doc){
+function insertMatch(match, cb) {
+    matches.findOne({
+        match_id: match.match_id
+    }, function(err, doc) {
+        if(!doc) {
             match.parse_status = 0
             matches.insert(match)
         }
@@ -135,37 +141,50 @@ function insertMatch(match, cb){
     })
 }
 
-function insertPlayer(player, cb){
-    var steamid32=BigNumber(player.steamid).minus('76561197960265728')
+function insertPlayer(player, cb) {
+    var steamid32 = BigNumber(player.steamid).minus('76561197960265728')
     console.log("[API] updating display name for id %s to %s", steamid32, player.personaname)
-    players.update({account_id:Number(steamid32)},{$set: {display_name:player.personaname}}, {upsert: true})
+    players.update({
+        account_id: Number(steamid32)
+    }, {
+        $set: {
+            display_name: player.personaname
+        }
+    }, {
+        upsert: true
+    })
     cb(null)
 }
 
-function getFullMatchHistory (account_id, cb){
-    var player_url = host+"/players/"+account_id+"/matches"
-    players.update({account_id: account_id}, {$set: {full_history : 0}})
-    getMatchPage(player_url, function(err){
+function getFullMatchHistory(account_id, cb) {
+    var player_url = host + "/players/" + account_id + "/matches"
+    players.update({
+        account_id: account_id
+    }, {
+        $set: {
+            full_history: 0
+        }
+    })
+    getMatchPage(player_url, function(err) {
         cb(null)
     })
 }
 
-function getMatchPage(url, cb){
+function getMatchPage(url, cb) {
     request(url, function(err, resp, body) {
         console.log("[DOTABUFF] %s", url)
         var parsedHTML = $.load(body);
         var matchCells = parsedHTML('td[class=cell-xlarge]')
         matchCells.each(function(i, matchCell) {
-            var match_url = host+$(matchCell).children().first().attr('href');
+            var match_url = host + $(matchCell).children().first().attr('href');
             var match = {}
             match.match_id = Number(match_url.split(/[/]+/).pop());
-            insertMatch(match, function(err){})
-        })  
+            insertMatch(match, function(err) {})
+        })
         var nextPath = parsedHTML('a[rel=next]').first().attr('href')
-        if (nextPath){
-            getMatchPage(host+nextPath, cb);
-        }
-        else{
+        if(nextPath) {
+            getMatchPage(host + nextPath, cb);
+        } else {
             cb(null)
         }
     })
@@ -173,140 +192,165 @@ function getMatchPage(url, cb){
 
 function download(match, cb) {
     var match_id = match.match_id
-    var fileName = replay_dir+match_id+".dem"
-
-    if (fs.existsSync(fileName)){
+    var fileName = replay_dir + match_id + ".dem"
+    if(fs.existsSync(fileName)) {
         console.log("[PARSER] found local replay for match %s", match_id)
         cb(null, fileName);
-    }
-    else{
-        if (match.start_time > moment().subtract(7, 'days').format('X')) {
-            getReplayUrl(match, function(url){
-                downloadFromSteam(url, fileName, 1000, function(){
+    } else {
+        if(match.start_time > moment().subtract(7, 'days').format('X')) {
+            getReplayUrl(match, function(url) {
+                downloadFromSteam(url, fileName, 1000, function() {
                     console.log("[PARSER] downloaded steam replay for match %s", match_id)
-                    uploadToS3(fileName, function(err){
+                    uploadToS3(fileName, function(err) {
                         cb(null, fileName)
                     })
                 })
             })
-        }
-        else{
-            if (process.env.AWS_S3_BUCKET){
+        } else {
+            if(process.env.AWS_S3_BUCKET) {
                 var s3 = new AWS.S3()
-                var params = {Bucket: process.env.AWS_S3_BUCKET, Key: fileName}
+                var params = {
+                    Bucket: process.env.AWS_S3_BUCKET,
+                    Key: fileName
+                }
                 s3.getObject(params, function(err, data) {
-                    if (err){
+                    if(err) {
                         cb("Replay expired (and not in S3)")
-                    }
-                    else{
+                    } else {
                         console.log("[PARSER] downloaded S3 replay for match %s", match_id)
                         fs.writeFileSync(fileName, data.Body);
                         cb(null, fileName)
                     }
                 })
-            }
-            else{
+            } else {
                 console.log('[S3] S3 is not defined')
                 cb("Replay expired")
             }
-
         }
     }
 }
-function uploadToS3(fileName, cb){
-    if (!process.env.AWS_S3_BUCKET){ 
+
+function uploadToS3(fileName, cb) {
+    if(!process.env.AWS_S3_BUCKET) {
         console.log('[S3] S3 is not defined')
         return cb(null)
     }
     var s3 = new AWS.S3()
-    var params = { Bucket: process.env.AWS_S3_BUCKET, Key: fileName}
-    s3.headObject(params, function(err, data){
-        if (err){
+    var params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: fileName
+    }
+    s3.headObject(params, function(err, data) {
+        if(err) {
             params.Body = fs.readFileSync(fileName)
-            s3.putObject(params,function (err, data) {
-                if (err){
+            s3.putObject(params, function(err, data) {
+                if(err) {
                     console.log('[S3] could not upload to S3')
                     cb(true);
-                }
-                else{
+                } else {
                     console.log('[S3] Successfully uploaded replay to S3: %s ', fileName)
                     cb(null)
                 }
             })
-        }
-        else{
+        } else {
             console.log('[S3] replay already exists in S3')
             cb(null)
         }
     })
 }
-function getReplayUrl(match, cb){
-    if (match.replay_url) { 
+
+function getReplayUrl(match, cb) {
+    if(match.replay_url) {
         console.log("[STEAM] found replay_url in db")
         return cb(match.replay_url)
     }
-    if (!steam.ready){
+    if(!steam) {
+        steam = new Steam(process.env.STEAM_USER, process.env.STEAM_PASS, process.env.STEAM_GUARD_CODE);
+    }
+    if(!steam.ready) {
         console.log("[STEAM] not ready yet")
         setTimeout(getReplayUrl, 5000, match, cb)
-    }
-    else{
+    } else {
         steam.getReplayDetails(match.match_id, function(err, data) {
-            if (err) {
+            if(err) {
                 //todo login with another account and try again
                 console.log(err)
                 setTimeout(getReplayUrl, 5000, match, cb)
-            }
-            else{
-                var url = "http://replay"+data.cluster+".valve.net/570/"+data.id+"_"+data.salt+".dem.bz2";
-                matches.update({match_id: match.match_id}, {$set: {replay_url:url}})
+            } else {
+                var url = "http://replay" + data.cluster + ".valve.net/570/" + data.id + "_" + data.salt + ".dem.bz2";
+                matches.update({
+                    match_id: match.match_id
+                }, {
+                    $set: {
+                        replay_url: url
+                    }
+                })
                 cb(url)
             }
-        })   
+        })
     }
 }
 
-function downloadFromSteam(url, fileName, timeout, cb){
-    request({url:url, encoding:null}, function (err, response, body) {
-        if (err || response.statusCode !== 200) {
-            console.log("[PARSER] failed to download from %s, retrying in %ds", url, timeout/1000)
-            setTimeout(downloadFromSteam, timeout, url, fileName, timeout*2, cb);
-        }
-        else{
-            body = Bunzip.decode(body);  
+function downloadFromSteam(url, fileName, timeout, cb) {
+    request({
+        url: url,
+        encoding: null
+    }, function(err, response, body) {
+        if(err || response.statusCode !== 200) {
+            console.log("[PARSER] failed to download from %s, retrying in %ds", url, timeout / 1000)
+            setTimeout(downloadFromSteam, timeout, url, fileName, timeout * 2, cb);
+        } else {
+            body = Bunzip.decode(body);
             fs.writeFileSync(fileName, body);
             cb(null)
         }
     })
 }
 
-function parseReplay(match, cb){
+function parseReplay(match, cb) {
     var match_id = match.match_id
-    download(match, function(err, fileName){
-        if (err) {
+    download(match, function(err, fileName) {
+        if(err) {
             console.log("[PARSER] Error for match %s: %s", match_id, err)
-            matches.update({match_id: match_id}, {$set: {parse_status : 1}})
+            matches.update({
+                match_id: match_id
+            }, {
+                $set: {
+                    parse_status: 1
+                }
+            })
             return cb(err)
         }
         console.log("[PARSER] started on %s", fileName)
-        var output=""
-        var cp = spawn(
-            "java",
-            ["-jar",
-             parser_file,
-             fileName
-            ])
-        cp.stdout.on('data', function (data) {
-            output+=data
+        var output = ""
+        var cp = spawn("java", ["-jar",
+            parser_file,
+            fileName
+        ])
+        cp.stdout.on('data', function(data) {
+            output += data
         })
-        cp.stderr.on('data', function (data) {
+        cp.stderr.on('data', function(data) {
             console.log('[PARSER] match: %s, stderr: %s', match_id, data);
         })
-        cp.on('close', function (code) {
+        cp.on('close', function(code) {
             console.log('[PARSER] match: %s, exit code: %s', match_id, code);
-            if (!code){
-                matches.update({match_id: match_id}, {$set: JSON.parse(output)})
-                matches.update({match_id: match_id}, {$set: {parse_status : 2}})
-                if (process.env.DELETE_REPLAYS){fs.unlink(fileName)}
+            if(!code) {
+                matches.update({
+                    match_id: match_id
+                }, {
+                    $set: JSON.parse(output)
+                })
+                matches.update({
+                    match_id: match_id
+                }, {
+                    $set: {
+                        parse_status: 2
+                    }
+                })
+                if(process.env.DELETE_REPLAYS) {
+                    fs.unlink(fileName)
+                }
             }
             cb(code)
         })
--- utility.js
+++ utility.js
@@ -1,88 +1,101 @@
 var utility = exports
 var request = require('request')
 var async = require('async')
-
 utility.db = require('monk')(process.env.MONGOHQ_URL || "localhost/dota");
 utility.matches = utility.db.get('matches');
-utility.matches.index('match_id', {unique: true});
+utility.matches.index('match_id', {
+    unique: true
+});
 utility.players = utility.db.get('players');
-utility.players.index('account_id', {unique: true})
-
-utility.getData = function(url, cb){
-    request(url, function (err, res, body) {
-        if (err) {cb(err)}
-        else if (res.statusCode != 200){
+utility.players.index('account_id', {
+    unique: true
+})
+utility.getData = function(url, cb) {
+    request(url, function(err, res, body) {
+        if(err) {
+            cb(err)
+        } else if(res.statusCode != 200) {
             cb("response code != 200");
-        }
-        else{
+        } else {
             cb(null, JSON.parse(body))
         }
     })
 }
-
-utility.fillPlayerNames = function(players, cb){
-    async.mapSeries(players, function(player, cb){
-        utility.players.findOne({account_id:player.account_id}, function(err, dbPlayer){
-            if (dbPlayer){
+utility.fillPlayerNames = function(players, cb) {
+    async.mapSeries(players, function(player, cb) {
+        utility.players.findOne({
+            account_id: player.account_id
+        }, function(err, dbPlayer) {
+            if(dbPlayer) {
                 player.display_name = dbPlayer.display_name
-            }
-            else{
+            } else {
                 player.display_name = "Anonymous"
             }
             cb(null)
         })
-    }, function(err){
+    }, function(err) {
         cb(null, players)
     })
 }
-
-utility.getMatches = function (account_id, cb){
-    utility.matches.find({players: { $elemMatch: { account_id: account_id }},duration:{$exists:true}}, {sort: {match_id: -1}}, function(err, docs){
+utility.getMatches = function(account_id, cb) {
+    utility.matches.find({
+        players: {
+            $elemMatch: {
+                account_id: account_id
+            }
+        },
+        duration: {
+            $exists: true
+        }
+    }, {
+        sort: {
+            match_id: -1
+        }
+    }, function(err, docs) {
         cb(err, docs)
     })
 }
-
-utility.getTrackedPlayers = function (cb){
-    utility.players.find({track:1}, function(err, docs){
+utility.getTrackedPlayers = function(cb) {
+    utility.players.find({
+        track: 1
+    }, function(err, docs) {
         cb(err, docs)
     })
 }
-
 utility.getTeammates = function(account_id, cb) {
-    utility.getMatches(account_id, function(err, docs){
+    utility.getMatches(account_id, function(err, docs) {
         var counts = {};
-        for (i=0;i<docs.length;i++){ //matches
-            for (j=0;j<docs[i].players.length; j++){ //match players
+        for(i = 0; i < docs.length; i++) { //matches
+            for(j = 0; j < docs[i].players.length; j++) { //match players
                 var player = docs[i].players[j]
-                if (player.account_id==account_id){
+                if(player.account_id == account_id) {
                     var playerSide = isRadiant(player)
                     var playerWinner = (playerSide && docs[i].radiant_win) || (!playerSide && docs[i].radiant_win)
-                    }
+                }
             }
-            for (j=0;j<docs[i].players.length; j++){  //match players
+            for(j = 0; j < docs[i].players.length; j++) { //match players
                 var player = docs[i].players[j]
-                if (isRadiant(player)==playerSide){ //only check teammates of player
-                    if (!counts[player.account_id]){
-                        counts[player.account_id]={}
-                        counts[player.account_id]["account_id"]=player.account_id;
-                        counts[player.account_id]["win"]=0;
-                        counts[player.account_id]["lose"]=0;
+                if(isRadiant(player) == playerSide) { //only check teammates of player
+                    if(!counts[player.account_id]) {
+                        counts[player.account_id] = {}
+                        counts[player.account_id]["account_id"] = player.account_id;
+                        counts[player.account_id]["win"] = 0;
+                        counts[player.account_id]["lose"] = 0;
                     }
-                    if (playerWinner){
-                        counts[player.account_id]["win"]+=1
-                    }
-                    else{
-                        counts[player.account_id]["lose"]+=1
+                    if(playerWinner) {
+                        counts[player.account_id]["win"] += 1
+                    } else {
+                        counts[player.account_id]["lose"] += 1
                     }
                 }
             }
         }
         //convert counts to array and filter
-        var arr=[]
+        var arr = []
         var min_matches = 5
-        for (var id in counts){
+        for(var id in counts) {
             var count = counts[id]
-            if (count.win+count.lose>=min_matches){
+            if(count.win + count.lose >= min_matches) {
                 arr.push(count)
             }
         }
@@ -90,6 +103,6 @@ utility.getTeammates = function(account_id, cb) {
     })
 }
 
-function isRadiant(player){
-    return player.player_slot<64
+function isRadiant(player) {
+    return player.player_slot < 64
 }
