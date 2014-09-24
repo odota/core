16e47724066c496683d90dc7436cc581a8af9f29
howardc93 <howardc93@gmail.com>
2014-09-24 07:45:18 +0000

    compiled new parser
    Former-commit-id: 46c22d2051a5568ef163aac7ccb5c14bf8e59109

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,30 +1,37 @@
 match.parsed_data.players.forEach(function (player, i){
-    var items = []
+    var items = new vis.DataSet();
     var heroes = 0
     for (var i=0;i<player.timeline.length;i++){
         var event = player.timeline[i]
         var bar = {}
+        bar.group=1
         if (event.time <0 ){
             time = moment().startOf('day').seconds(event.time*-1).format("-m:ss");
         }
         else{
             time = moment().startOf('day').seconds(event.time).format("m:ss");
         }
-        bar.content = event.key + time
         bar.start=moment().startOf('day').seconds(event.time)
+        if (event.type=="buybacks"){
+            bar.content = "<div class='small'>"+event.key+"</div>"+"<div class='small'>"+time+"</div>"
+            bar.type="box"
+        }
         if (event.type=="itembuys"){
             bar.content = "<img src='"+event.img+"' width=25 />"+"<div class='small'>"+time+"</div>"
             bar.type="box"
         }
         if (event.type=="hero_history"){
-            bar.className = "background-"+(heroes % 5)
+            bar.className = "background-"+(heroes % 8)
             heroes+=1
-            bar.content = "<div class='small'>"+event.key+"<br>("+time+")</div>"
+            bar.content = "<div class='small'>"+event.key+"</div>"+"<img src='"+event.img+"'/>"+"<div class='small'>"+time+"</div>"
             bar.end = moment().startOf('day').seconds(event.end)
             bar.type="background"
         }
-        items.push(bar)
+        items.add(bar)
     }
+
+    var groups=new vis.DataSet();
+    groups.add([{id:1, content:"Timeline"}])
 
     // create visualization
     var container = document.getElementById('chart-timeline');
@@ -40,14 +47,12 @@ match.parsed_data.players.forEach(function (player, i){
         showCurrentTime: false,
         start: moment().startOf('day').subtract(180, 'seconds'),
         end: moment().startOf('day').seconds(match.duration).add(180, 'seconds'),
-        margin: {
-
-        },
         showMajorLabels: false
     };
 
     var timeline = new vis.Timeline(iDiv);
     timeline.setOptions(options);
     timeline.setItems(items);
+    timeline.setGroups(groups);
 })
 
