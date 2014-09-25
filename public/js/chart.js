618632641ffb9841f3cbe15735dbcefb40b678fa
howardc93 <howardc93@gmail.com>
2014-09-25 06:19:56 +0000

    updated views
    Former-commit-id: b7be38b59d59206607fc522a5f217dd14046da6b

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,10 +1,20 @@
 var height = 350
 var goldDifference = ['Gold']
 var xpDifference = ['XP']
+var time = ["time"].concat(match.parsed_data.times)
+var gold = [time]
+var xp = [time]
+var groups = [
+    []
+]
 for(var i = 0; i < match.parsed_data.times.length; i++) {
     var goldtotal = 0
     var xptotal = 0
+    var absGoldTotal = 0
+    var absXpTotal = 0
     match.parsed_data.players.forEach(function(elem, j) {
+        absGoldTotal +=elem.gold[i]
+        absXpTotal +=elem.xp[i]
         if(j < 5) {
             goldtotal += elem.gold[i]
             xptotal += elem.xp[i]
@@ -15,23 +25,18 @@ for(var i = 0; i < match.parsed_data.times.length; i++) {
     })
     goldDifference.push(goldtotal)
     xpDifference.push(xptotal)
+    match.parsed_data.players.forEach(function(elem, j) {
+        elem.gold[i]=elem.gold[i]/(absGoldTotal + 1)
+        elem.xp[i]=elem.xp[i]/(absXpTotal + 1)
+    })
 }
-var time = ["time"].concat(match.parsed_data.times)
-var lh = [time]
-var gold = [time]
-var xp = [time]
-var groups = [
-    []
-]
 match.parsed_data.players.forEach(function(elem, i) {
     var hero = constants.heroes[match.players[i].hero_id].localized_name
     groups[0].push(hero)
-    elem.lh = [hero].concat(elem.lh)
-    lh.push(elem.lh)
     elem.gold = [hero].concat(elem.gold)
     gold.push(elem.gold)
     elem.xp = [hero].concat(elem.xp)
-    xp.push(elem.xp)  
+    xp.push(elem.xp)
 })
 c3.generate({
     bindto: "#chart-diff",
@@ -55,32 +60,6 @@ c3.generate({
         },
         y: {
             label: 'Radiant Advantage'
-        }
-    }
-})
-c3.generate({
-    bindto: "#chart-lh",
-    size: {
-        height: height
-    },
-    data: {
-        x: 'time',
-        columns: lh,
-        type: "area-spline",
-        groups: groups,
-        order: null
-    },
-    axis: {
-        x: {
-            type: 'timeseries',
-            tick: {
-                format: function(x) {
-                    return moment().startOf('day').seconds(x).format("H:mm")
-                }
-            },
-        },
-        y: {
-            label: 'Last Hits'
         }
     }
 })
@@ -121,7 +100,6 @@ c3.generate({
         type: "area-spline",
         groups: groups,
         order: null
-
     },
     axis: {
         x: {
