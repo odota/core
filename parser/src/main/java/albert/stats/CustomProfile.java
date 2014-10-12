0b0774c6e0acb146523380f0dfa62b06db255508
howardc93 <howardc93@gmail.com>
2014-10-12 17:18:58 +0000

    adding stuns/playing with entities
    Former-commit-id: 5f1b710b284f2fdcf7793ef032c7aa76918380bc

--- parser/src/main/java/albert/stats/CustomProfile.java
+++ parser/src/main/java/albert/stats/CustomProfile.java
@@ -1,5 +1,6 @@
 package albert.stats;
 
+import com.dota2.proto.DotaUsermessages;
 import com.dota2.proto.Usermessages;
 import skadistats.clarity.parser.Profile;
 
@@ -7,7 +8,5 @@ public class CustomProfile extends Profile{
     public static Profile ALL_CHAT = new Profile()
         .dependsOn(USERMESSAGE_CONTAINER)
         .append(
-        Usermessages.CUserMsg_SayText2.class
-    );
-
+        Usermessages.CUserMsg_SayText2.class);
 }
