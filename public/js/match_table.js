18314a7b24c8aa46f31e965252097900b2142193
albertcui <albertqcui+github@gmail.com>
2014-12-09 00:45:16 +0000

    add datatables rendering

--- public/js/match_table.js
+++ public/js/match_table.js
@@ -3,18 +3,48 @@ $('#table').dataTable({
     ajax: '/api/matches',
     serverSide: true,
     columns: [
-        { data: 'match_id',
-         title: 'Match ID'  },
-        { data: 'game_mode',
-         title: 'Game Mode' },
-        { data: 'cluster',
-         title: 'Region' },
-        { data: 'duration',
-         title: 'Duration' },
-        { data: 'start_time',
-         title: 'Played' },
-        { data: 'parse_status',
-         title: 'Status' }
+        {
+            data: 'match_id',
+            title: 'Match ID',
+            render: function(data, type, row, meta) {
+                return '<a href="/matches/' + data + '">' + data + '</a>'
+            }
+        },
+        {
+            data: 'game_mode',
+            title: 'Game Mode',
+            render: function(data, type, row, meta) {
+                return modes[data] ? modes[data].name : data
+            }
+        },
+        {
+            data: 'cluster',
+            title: 'Region',
+            render: function(data, type, row, meta) {
+                return regions[data] ? regions[data] : data
+            }
+        },
+        {
+            data: 'duration',
+            title: 'Duration',
+            render: function(data, type, row, meta) {
+                return moment().startOf('day').seconds(data).format("H:mm:ss")
+            }
+        },
+        {
+            data: 'start_time',
+            title: 'Played',
+            render: function(data, type, row, meta) {
+                return moment.unix(data + row.duration).fromNow()
+            }
+        },
+        {
+            data: 'parse_status',
+            title: 'Status',
+            render: function(data, type, row, meta) {
+                return parse_status[data] ? parse_status[data] : data
+            }
+        }
     ]
 });
 
