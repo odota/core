6a62e84bcc46daf1978908db022a382a07ba266e
Bitnami <howardc93@gmail.com>
2015-01-18 23:23:30 -0500

    modified parser

--- test/test_parser.sh
+++ test/test_parser.sh
@@ -1,1 +1,4 @@
-mvn -f parser/pom.xml package && java -jar parser/target/stats-0.1.0.jar replays/1151783218.dem > output.json
+mvn -f parser/pom.xml package;
+java -jar parser/target/stats-0.1.0.jar replays/1151783218.dem > output.json;
+#java -jar parser/target/stats-0.1.0.jar replays/1170654668.dem > output.json;
+#java -jar parser/target/stats-0.1.0.jar replays/1151783218.dem -epilogue;
