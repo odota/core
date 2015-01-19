4c85bfb7274c2f49aa2f3dc9cb83ca4083e43778
Howard Chung <howardc93@gmail.com>
2015-01-19 18:32:23 -0500

    added more endpoint tests, travis mongo/redis
    Former-commit-id: 128611309fc34104830e4477baa42dd6981ae9c3

--- test/test.js
+++ test/test.js
@@ -13,3 +13,38 @@ describe('GET /', function() {
       });
   });
 });
+
+describe('GET /stats', function() {
+  it('got 200 response', function(done) {
+    request(app)
+      .get('/stats')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        done(err);
+      });
+  });
+});
+
+describe('GET /upload', function() {
+  it('got 200 response', function(done) {
+    request(app)
+      .get('/upload')
+      .expect(200)
+      .end(function(err, res) {
+        done(err);
+      });
+  });
+});
+
+describe('GET /matches', function() {
+  it('got 200 response', function(done) {
+    request(app)
+      .get('/matches')
+      .expect(200)
+      .end(function(err, res) {
+        done(err);
+      });
+  });
+});
+
