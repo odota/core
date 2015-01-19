f31a5a28d441a4124b25ed13b9ec51080f8899ce
Bitnami <howardc93@gmail.com>
2015-01-19 04:44:19 -0500

    edited deploy
    Former-commit-id: ce44c9441b6451e43b0bd1830b20e96c4c5a8787

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if (( EUID != 0 )); then
    echo "You must be root to do this." 1>&2
    exit 100
@@ -8,7 +7,7 @@ fi
 
 git pull origin mastser
 
-npm install
+sudo npm install
 
 mvn -f parser/pom.xml package
 
