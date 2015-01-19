df18b15dfef1c767a29f6e19fc405cb24fb68d46
Bitnami <howardc93@gmail.com>
2015-01-19 01:30:56 -0500

    fixed beastmaster/oracle abilities

--- test/test_parser.sh
+++ test/test_parser.sh
@@ -1,4 +1,4 @@
 mvn -f parser/pom.xml package;
-java -jar parser/target/stats-0.1.0.jar replays/1151783218.dem > output.json;
-#java -jar parser/target/stats-0.1.0.jar replays/1170654668.dem > output.json;
+#java -jar parser/target/stats-0.1.0.jar replays/1151783218.dem > output.json;
+java -jar -Xmx96m parser/target/stats-0.1.0.jar replays/1170654668.dem > output.json;
 #java -jar parser/target/stats-0.1.0.jar replays/1151783218.dem -epilogue;
