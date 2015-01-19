96bb9ecaab2b0858aba48aff0a3ae176a79d6418
Howard Chung <howardc93@gmail.com>
2015-01-19 17:58:41 -0500

    test on homepage

--- test/test.js
+++ test/test.js
@@ -3,11 +3,10 @@ var request = require('supertest');
 // Here we get hold of the express application 
 var app = require("../web").app;
 
-describe('GET /stats', function() {
-  it('should respond with JSON', function(done) {
+describe('GET /', function() {
+  it('got 200 response', function(done) {
     request(app)
-      .get('/stats')
-      .expect('Content-Type', /json/)
+      .get('/')
       .expect(200)
       .end(function(err, res) {
         done(err);
