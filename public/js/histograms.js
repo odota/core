84c7b4e73d1b6362f5ed3f0ff2c3f569a037f1a2
Howard Chung <howardc93@gmail.com>
2015-02-04 07:05:11 -0500

    added on ready to client side js
    Former-commit-id: 8108a94fb257e1e0af25fcce33f8578c284f88f1

--- public/js/histograms.js
+++ public/js/histograms.js
@@ -3,72 +3,74 @@ var CalHeatMap = require('cal-heatmap');
 var moment = require('moment');
 
 module.exports = function generateHistograms(data) {
-    console.log(data);
-    c3.generate({
-        bindto: "#chart-duration",
-        data: {
-            columns: [
-                ['Matches'].concat(data.durations)
-            ],
-            type: 'bar'
-        },
-        bar: {
-            width: {
-                ratio: 0.8
-            }
-        },
-        axis: {
-            x: {
-                label: 'Minutes'
+    $(document).on('ready', function() {
+        console.log(data);
+        c3.generate({
+            bindto: "#chart-duration",
+            data: {
+                columns: [
+                    ['Matches'].concat(data.durations)
+                ],
+                type: 'bar'
             },
-            y: {
-                label: 'Matches'
-            }
-        }
-    });
-    c3.generate({
-        bindto: "#chart-gpms",
-        data: {
-            columns: [
-                ['Matches'].concat(data.gpms)
-            ],
-            type: 'bar'
-        },
-        bar: {
-            width: {
-                ratio: 0.8
-            }
-        },
-        axis: {
-            x: {
-                label: 'GPM',
-                tick: {
-                    format: function(x) {
-                        return String(x * 10);
-                    }
+            bar: {
+                width: {
+                    ratio: 0.8
                 }
             },
-            y: {
-                label: 'Matches'
+            axis: {
+                x: {
+                    label: 'Minutes'
+                },
+                y: {
+                    label: 'Matches'
+                }
             }
-        }
-    })
-    var cal = new CalHeatMap();
-    cal.init({
-        start: new Date(moment().subtract(1, 'year')),
-        range: 13,
-        domain: "month",
-        subDomain: "day",
-        data: data.calheatmap,
-        tooltip: true,
-        legend: [1, 2, 3, 4],
-        highlight: new Date(),
-        itemName: ["match", "matches"],
-        subDomainTextFormat: function(date, value) {
-            return value;
-        },
-        cellSize: 15,
-        previousSelector: "#prev",
-        nextSelector: "#next"
+        });
+        c3.generate({
+            bindto: "#chart-gpms",
+            data: {
+                columns: [
+                    ['Matches'].concat(data.gpms)
+                ],
+                type: 'bar'
+            },
+            bar: {
+                width: {
+                    ratio: 0.8
+                }
+            },
+            axis: {
+                x: {
+                    label: 'GPM',
+                    tick: {
+                        format: function(x) {
+                            return String(x * 10);
+                        }
+                    }
+                },
+                y: {
+                    label: 'Matches'
+                }
+            }
+        });
+        var cal = new CalHeatMap();
+        cal.init({
+            start: new Date(moment().subtract(1, 'year')),
+            range: 13,
+            domain: "month",
+            subDomain: "day",
+            data: data.calheatmap,
+            tooltip: true,
+            legend: [1, 2, 3, 4],
+            highlight: new Date(),
+            itemName: ["match", "matches"],
+            subDomainTextFormat: function(date, value) {
+                return value;
+            },
+            cellSize: 15,
+            previousSelector: "#prev",
+            nextSelector: "#next"
+        });
     });
-}
+};
