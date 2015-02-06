bbf9b8cb352673a4de654882e0af0596f61d1639
Howard Chung <howardc93@gmail.com>
2015-02-05 20:50:54 -0500

    added loader

---
+++ test/loader.js
@@ -0,0 +1,10 @@
+var ids = require("./ids.json");
+var utility = require("../utility");
+var db = utility.db;
+
+ids.forEach(function(id) {
+    db.players.insert({
+        account_id: id,
+        track: 1
+    });
+});
