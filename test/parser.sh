e9481534fff5743f01643c615d382cd80ced7c45
Howard Chung <howardc93@gmail.com>
2015-02-14 18:43:21 -0500

    throwing error on download fail

--- test/parser.sh
+++ test/parser.sh
@@ -1,3 +1,3 @@
 #mvn -f parser/pom.xml package && java -jar parser/target/stats-0.1.0.jar < ../testfiles/1193091757.dem > output.json
 #mvn -f parser/pom.xml package && java -jar parser/target/stats-0.1.0.jar < ../testfiles/1232722145_683c.dem > output.json
-mvn -f parser/pom.xml package && java -jar parser/target/stats-0.1.0.jar < ../testfiles/1235525604_683c.dem > output.json
+mvn -f parser/pom.xml package && java -jar parser/target/stats-0.1.0.jar < ../testfiles/1238853003_369108933.dem > output.json
