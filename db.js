5a3ae67273fc17712bb3c48ab7b11fbee5719f93
Howard Chung <howardc93@gmail.com>
2015-02-14 00:01:46 -0500

    added indexes to improve status page performance

--- db.js
+++ db.js
@@ -1,21 +1,32 @@
 var db = require('monk')(process.env.MONGO_URL || "mongodb://localhost/dota");
-db.get('matches').index({
+db.players = db.get('players');
+db.matches = db.get('matches');
+db.ratings = db.get('ratings');
+db.matches.index({
     'match_id': -1
 }, {
     unique: true
 });
-db.get('matches').index({
+db.matches.index({
     'match_seq_num': -1
 }, {
     unique: true
 });
-db.get('matches').index('players.account_id');
-db.matches = db.get('matches');
-db.get('players').index('account_id', {
+db.matches.index('players.account_id');
+db.players.index('account_id', {
     unique: true
 });
-db.players = db.get('players');
-db.get('ratings').index({'match_id':-1, 'account_id': 1}, {unique:true});
-db.ratings = db.get('ratings');
+db.plaers.index({
+    'last_visited': -1
+});
+db.plaers.index({
+    'full_history_time': 1
+});
+db.ratings.index({
+    'match_id': -1,
+    'account_id': 1
+}, {
+    unique: true
+});
 
 module.exports = db;
