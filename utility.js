9e5cf1d92c9cd36b9f01c5e997e772e197225696
Howard Chung <howardc93@gmail.com>
2015-02-18 00:36:11 -0500

    added error for private profiles

--- utility.js
+++ utility.js
@@ -106,9 +106,15 @@ function getData(url, cb) {
                 logger.info("retrying: %s", target);
                 return getData(url, cb);
             }
+            else if (body.error) {
+                //body contained an error (probably from retriever)
+                //non-retryable
+                return cb(body);
+            }
             else if (body.result) {
                 if (body.result.status === 15 || body.result.error === "Practice matches are not available via GetMatchDetails" || body.result.error === "No Match ID specified" || body.result.error === "Match ID not found") {
                     //user does not have stats enabled or attempting to get private match/invalid id, don't retry
+                    //non-retryable
                     return cb(body);
                 }
                 else if (body.result.error || body.result.status === 2) {
@@ -132,7 +138,7 @@ function runParse(cb) {
         //console.log(data);
         output += data;
     });
-    parser.stderr.on('data', function(data){
+    parser.stderr.on('data', function(data) {
         //console.log(data);
     });
     parser.on('exit', function(code) {
@@ -153,37 +159,33 @@ function runParse(cb) {
 }
 
 function getRetrieverUrls() {
-    return retrievers.map(function(r) {
-        return "http://" + r;
-    });
-}
-
-/*
- * Converts a steamid 64 to a steamid 32
- *
- * Returns a BigNumber
- */
+        return retrievers.map(function(r) {
+            return "http://" + r;
+        });
+    }
+    /*
+     * Converts a steamid 64 to a steamid 32
+     *
+     * Returns a BigNumber
+     */
 function convert64to32(id) {
-    return new BigNumber(id).minus('76561197960265728');
-
-}
-
-/*
- * Converts a steamid 64 to a steamid 32
- *
- * Returns a BigNumber
- */
+        return new BigNumber(id).minus('76561197960265728');
+    }
+    /*
+     * Converts a steamid 64 to a steamid 32
+     *
+     * Returns a BigNumber
+     */
 function convert32to64(id) {
     return new BigNumber('76561197960265728').plus(id);
 }
 
 function isRadiant(player) {
-    return player.player_slot < 64;
-}
-
-/*
- * Makes sort from a datatables call
- */
+        return player.player_slot < 64;
+    }
+    /*
+     * Makes sort from a datatables call
+     */
 function makeSort(order, columns) {
     var sort = {
         match_id: -1
@@ -199,8 +201,6 @@ function makeSort(order, columns) {
     }
     return sort;
 }
-
-
 module.exports = {
     logger: logger,
     generateJob: generateJob,
@@ -212,7 +212,6 @@ module.exports = {
     isRadiant: isRadiant,
     makeSort: makeSort
 };
-
 /*
 function getS3Url(match_id, cb) {
     var archiveName = match_id + ".dem.bz2";
