fcbd387b8f674f67140a875c2fbd0aba3dddbf80
Mei-Vern Then <mt2837@columbia.edu>
2015-02-28 03:14:40 -0500

    hacky buttons, basically finished blog

---
+++ public/js/scripts.js
@@ -0,0 +1,46 @@
+
+// $(document).ready(function(){
+// 	Polymer('.fab', {
+// 		clickEvent: function() {
+			
+// 		}
+// 	});
+
+// });
+
+$(document).ready(function(){
+	$('.fab').hover(
+	function() {
+		$('.fab-button').attr("icon", "expand-less");
+
+		var e = $('<a href="#philosophy"></a>');
+		e.wrapInner('<paper-fab mini class="appended-fab">');
+		var el = $('<core-tooltip noarrow label="TL;DR" position="left">').wrapInner(e);
+		var tldr = $('<div class="fab-buttons"></di>').append(el);
+
+		var features = $(tldr.clone());
+		var cheese = $(tldr.clone());
+		var main = $('<div class="fab-buttons" id="fab-button-extension"></div>');
+
+		style="color: blue;"
+		
+		$(tldr).find( "paper-fab" ).attr("id","tldr").attr("icon","filter-list").attr("style", "background-color:red;");
+		$(features).find( "paper-fab" ).attr("id","features").attr("icon","star").attr("style", "background-color:green;");
+		$(features).find("a").attr("href","#match_statistics");
+		$(features).find("core-tooltip").attr("label","Go to match statistics");
+		$(cheese).find( "paper-fab" ).attr("id","cheese").attr("icon","redeem");
+		$(cheese).find("a").attr("href","#cheese");
+		$(cheese).find("core-tooltip").attr("label","Help us out and buy cheese!");
+		
+		main.append($(tldr));
+		main.append($(features));
+		// main.append($(cheese));
+
+		$(this).prepend($(main));
+  	}, 
+  	function() {
+  	$('.fab-button').attr("icon", "dashboard");
+    $( "#fab-button-extension" ).remove();
+  })
+
+});
