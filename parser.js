bcdac9e086ba8b45b1b22eb07606af6296f416da
Howard Chung <howardc93@gmail.com>
2015-02-28 21:59:41 -0500

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,4 +1,4 @@
 var processors = require('./processors');
 var jobs = require('./redis').jobs;
 console.log("[PARSER] starting parser");
-jobs.process('parse', processors.processParse);
+jobs.process('parse', 4, processors.processParse);
