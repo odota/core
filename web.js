31ca2858f2a03537ede8a8c94e2cce586686821a
Howard Chung <howardc93@gmail.com>
2015-03-18 17:39:04 -0400

    fixes #313

--- web.js
+++ web.js
@@ -91,9 +91,9 @@ app.use("/bower_components", express.static(path.join(__dirname, '/bower_compone
 app.use(session({
     store: new RedisStore({
         client: redis,
-        ttl: 8 * 7 * 24 * 60 * 60
+        ttl: 52 * 7 * 24 * 60 * 60
     }),
-    maxAge: 8 * 7 * 24 * 60 * 60 * 1000,
+    cookie: { maxAge: 52 * 7 * 24 * 60 * 60 * 1000},
     secret: config.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
