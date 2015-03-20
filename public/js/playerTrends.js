32e52d528de235a7ff0ee6b8430ecc6fa9cb1ebd
Albert Cui <aqc2109@columbia.edu>
2015-03-20 14:00:38 -0400

    allow linking/back button

--- public/js/playerTrends.js
+++ public/js/playerTrends.js
@@ -3,8 +3,8 @@ var $records = $(".records"),
     $wards = $(".wards"),
     $charts = $(".charts"),
     $content = $("#content");
+
 $("#the_stats").empty();
-$content.append($records);
 
 function makeDT() {
     if (! $.fn.dataTable.isDataTable( '#heroes' )) {
@@ -12,30 +12,69 @@ function makeDT() {
     }
 }
 
-$("#charts").click(function() {
+function getCharts() {
     $content.empty();
     $content.append($charts);
     generateHistograms(aggData);
     if ($("#cal-heatmap").children().length < 1) {
         generateActivity(aggData);
     }
-})
-$("#wards").click(function() {
+}
+
+function getWards() {
     $content.empty();
     $content.append($wards);
     $(".activate").on('mouseover', function(){
       heatmap.setData(posData[0][$(this).attr('id')]);
       heatmap.repaint();
     })
-})     
-$("#records").click(function(){
-  $content.empty();
-  $content.append($records);
-})
+}
 
+function getMatchups() {
+    $content.empty();
+    $content.append($matchups);
+    makeDT();
+    tooltips();
+}
+
+function getRecords() {
+    $content.empty();
+    $content.append($records);
+}
+
+function showSection() {
+    if (window.location.hash) {
+        var hash = window.location.hash.substring(1);
+        console.log(hash);
+        if (hash === "charts") {
+            getCharts();
+        } else if (hash === "wards") {
+            getWards();
+        } else if (hash === "matchups") {
+            getMatchups();
+        } else {
+            getRecords();
+        }
+    } else {
+        getRecords();
+    }
+}
+
+showSection();
+
+window.onhashchange = function() {
+    showSection();
+}
+
+$("#charts").click(function(){
+    getCharts();
+});
+$("#wards").click(function(){
+    getWards();
+});  
+$("#records").click(function(){
+    getRecords();
+});
 $("#matchups").click(function(){
-  $content.empty();
-  $content.append($matchups);
-  makeDT();
-  tooltips();
-})
+    getMatchups();
+});
