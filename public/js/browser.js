eba02b4fff3a0266ff48d68aaf0c4eb368095176
Albert Cui <aqc2109@columbia.edu>
2015-03-20 00:54:37 -0400

    add charts tab to trends

--- public/js/browser.js
+++ public/js/browser.js
@@ -6,6 +6,7 @@ global.playerTables = require('./playerTables');
 global.playerMatches = require('./playerMatches');
 global.buildMap = require('./map');
 global.generateHistograms = require('./histograms');
+global.generateActivity = require('./activity');
 global.statusHandler = require('./statusHandler');
 global.c3 = require('c3');
 global.h337 = require('heatmap.js');
--- public/js/histograms.js
+++ public/js/histograms.js
@@ -1,32 +1,6 @@
 var c3 = require('c3');
-var CalHeatMap = require('cal-heatmap');
 var moment = require('moment');
 module.exports = function generateHistograms(data) {
-    var cal = new CalHeatMap();
-    cal.init({
-        start: new Date(moment().subtract(1, 'year')),
-        range: 13,
-        domain: "month",
-        subDomain: "day",
-        data: data.start_time.counts,
-        verticalOrientation: true,
-        label: {
-            position: "left"
-        },
-        colLimit: 31,
-        tooltip: true,
-        legend: [1, 2, 3, 4],
-        highlight: new Date(),
-        itemName: ["match", "matches"],
-        subDomainTextFormat: function(date, value) {
-            return value;
-        },
-        cellSize: 13,
-        domainGutter: 5,
-        previousSelector: "#prev",
-        nextSelector: "#next",
-        legendHorizontalPosition: "right"
-    });
     //need a param to scale the x-axis by, e.g., gpms are divided by 10 for binning, durations divided by 60
     //need a max to determine how many bins we should have
     //need a param to define the label on the x axis
