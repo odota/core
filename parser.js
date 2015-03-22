5d94db997fcca975f649f5bcc2cfcb22f35bcfeb
Howard Chung <howardc93@gmail.com>
2015-03-22 14:41:44 -0400

    reduced parallelism

--- parser.js
+++ parser.js
@@ -15,5 +15,5 @@ if (cluster.isMaster) {
 }
 else {
     console.log("[PARSER] starting parser worker");
-    jobs.process('parse', numCPUs, processors.processParse);
+    jobs.process('parse', 4, processors.processParse);
 }
