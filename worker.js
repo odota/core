e47c1aa0f206a06fb309cd382d9f9ecbff7b58e6
Albert Cui <aqc2109@columbia.edu>
2015-03-24 14:05:08 -0400

    increase delay to 6 min

--- worker.js
+++ worker.js
@@ -52,7 +52,7 @@ d.run(function() {
         jobs.process('api', config.STEAM_API_KEY.split(",").length, processors.processApi);
         jobs.process('mmr', processors.processMmr);
         jobs.process('request', processors.processApi);
-        setInterval(fullhistory, 3 * 60 * 1000, function() {});
+        setInterval(fullhistory, 6 * 60 * 1000, function() {});
         setInterval(updatenames, 3 * 60 * 1000, function() {});
         setInterval(build, 3 * 60 * 1000, function() {});
         //todo implement redis window check 
