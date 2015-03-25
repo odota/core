d19d638d9454069a0b787cff930e04545709d7ea
Howard Chung <howardc93@gmail.com>
2015-03-25 06:33:54 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y make g++ build-essential git redis-server mongodb maven default-jdk nodejs-legacy npm
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y make g++ build-essential git redis-server mongodb maven default-jdk nodejs-legacy npm
 sudo npm install -g n && n stable
