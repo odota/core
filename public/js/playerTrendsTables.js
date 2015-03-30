7a1dbb94c930bf0f52e6b35d5e1be483ec153e3e
Albert Cui <aqc2109@columbia.edu>
2015-03-30 02:52:17 -0400

    add playerTrendsTables.js, stop stacking matchup tables

---
+++ public/js/playerTrendsTables.js
@@ -0,0 +1,75 @@
+var $ = jQuery = require('jquery');
+var utility = require('./utility');
+var formatSeconds = utility.formatSeconds;
+module.exports = function playerTables() {
+    var accuracy = $('#accuracy').dataTable({
+        "searching": true,
+        "paging": true,
+        "order": [
+            [2, "desc"]
+        ],
+        "drawCallback": function() {
+            tooltips();
+            formatHtml();
+        },
+        "columnDefs": [{
+            "targets": [0],
+            "orderData": [1]
+            }, {
+            "targets": [1],
+            visible: false
+            }]
+    });
+    var builds = $('#builds').dataTable({
+        //"searching": false,
+        "paging": true,
+        "order": [
+            [2, "asc"]
+        ],
+        "drawCallback": function() {
+            tooltips();
+            formatHtml();
+        },
+        "columns": [{}, {}, {
+            render: function(data, type) {
+                if (type === "display") {
+                    return formatSeconds(data);
+                }
+                return data;
+            }
+        }, {}],
+        "columnDefs": [{
+            "targets": [0],
+            "orderData": [1]
+            }, {
+            "targets": [1],
+            visible: false
+            }]
+    });
+    var teammates = $('#teammates').dataTable({
+        //"searching": false,
+        "paging": true,
+        "order": [
+            [1, "desc"]
+        ]
+    });
+    var heroes = $('#heroes').dataTable({
+        //"searching": false,
+        "paging": true,
+        "order": [
+            [2, "desc"]
+        ],
+        "drawCallback": function() {
+            tooltips();
+            formatHtml();
+        },
+        "columnDefs": [{
+            "targets": [0],
+            "orderData": [1]
+        }, {
+            "targets": [1],
+            visible: false
+        }]
+    });
+    return [accuracy, builds, teammates, heroes];
+};
