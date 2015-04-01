4d9ef8e019bf3e3e46c1dd12d79cc53c036271b9
Mei-Vern Then <mt2837@columbia.edu>
2015-03-31 23:29:37 -0400

    Added scroll-to-top fab button, changed main fab to just toggle other buttons on hover (on homepage).

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,27 +11,31 @@
 $(document).ready(function(){
 	$('.fab').hover(
 	function() {
-		$('.fab-button').attr("icon", "expand-less");
 
-		var e = $('<a href="#philosophy"></a>');
+		var e = $('<a href="#top"></a>');
 		e.wrapInner('<paper-fab mini class="appended-fab">');
-		var el = $('<core-tooltip noarrow label="TL;DR" position="left">').wrapInner(e);
-		var tldr = $('<div class="fab-buttons"></di>').append(el);
+		var el = $('<core-tooltip noarrow label="Scroll up!" position="left">').wrapInner(e);
+		var up = $('<div class="fab-buttons"></di>').append(el);
 
-		var features = $(tldr.clone());
-		var cheese = $(tldr.clone());
+		var tldr = $(up.clone());
+		var features = $(up.clone());
+		var cheese = $(up.clone());
 		var main = $('<div class="fab-buttons" id="fab-button-extension"></div>');
 
 		style="color: blue;"
 		
-		$(tldr).find( "paper-fab" ).attr("id","tldr").attr("icon","filter-list").attr("style", "background-color:red;");
-		$(features).find( "paper-fab" ).attr("id","features").attr("icon","star").attr("style", "background-color:green;");
+		$(up).find( "paper-fab" ).attr("id","up").attr("icon","expand-less").attr("style", "background-color:rgb(23, 190, 207);");
+		$(tldr).find( "paper-fab" ).attr("id","tldr").attr("icon","filter-list").attr("style", "background-color:rgb(255, 127, 14);");
+		$(tldr).find("a").attr("href","#philosophy");
+		$(tldr).find("core-tooltip").attr("label","TL;DR");
+		$(features).find( "paper-fab" ).attr("id","features").attr("icon","star").attr("style", "background-color:rgb(44, 160, 44);");
 		$(features).find("a").attr("href","#match_statistics");
 		$(features).find("core-tooltip").attr("label","Features");
-		$(cheese).find( "paper-fab" ).attr("id","cheese").attr("icon","redeem").attr("style", "background-color:#eea236;");
+		$(cheese).find( "paper-fab" ).attr("id","cheese").attr("icon","redeem").attr("style", "background-color:rgb(214, 39, 40);");
 		$(cheese).find("a").attr("href","#cheeses");
 		$(cheese).find("core-tooltip").attr("label","We need your help!");
 		
+		main.append($(up));
 		main.append($(tldr));
 		main.append($(features));
 		main.append($(cheese));
