d0e57402684859671b0e062f2329c48052085c57
Bitnami <howardc93@gmail.com>
2015-04-01 13:39:24 -0400

    building sets in parsemanager

--- parseManager.js
+++ parseManager.js
@@ -3,42 +3,48 @@ var r = require('./redis');
 var redis = r.client;
 var jobs = r.jobs;
 var cluster = require('cluster');
+var buildSets = require('./tasks/buildSets');
 start();
 
 function start() {
     if (cluster.isMaster) {
         console.log("[PARSEMANAGER] starting master");
-        redis.get("retrievers", function(err, result) {
-            if (err || !result) {
-                console.log("no retrievers in redis!");
-                return start();
-            }
-            var parsers = JSON.parse(result);
-            var urls = {};
-            //length of this array is capacity
-            var capacity = parsers.length;
-            // Fork workers.
-            for (var i = 0; i < capacity; i++) {
-                cluster.fork({
-                    PARSER_URL: parsers[i]
-                });
-            }
-            // handle unwanted worker exits
-            cluster.on("exit", function(worker, code) {
-                if (code != 0) {
-                    console.log("Worker crashed! Spawning a replacement.");
-                    //fork a new worker with the same url as the one that crashed
-                    //lookup by worker id
+        //build sets to ensure parsers are up to date
+        buildSets(function() {
+            redis.get("retrievers", function(err, result) {
+                if (err || !result) {
+                    console.log("no parsers in redis!");
+                    return setTimeout(function() {
+                        return start();
+                    }, 10000);
+                }
+                var parsers = JSON.parse(result);
+                var urls = {};
+                //length of this array is capacity
+                var capacity = parsers.length;
+                // Fork workers.
+                for (var i = 0; i < capacity; i++) {
                     cluster.fork({
-                        PARSER_URL: urls[worker.id]
+                        PARSER_URL: parsers[i]
                     });
                 }
-            });
-            Object.keys(cluster.workers).forEach(function(id) {
-                cluster.workers[id].on('message', function(msg) {
-                    console.log(msg);
-                    //a new worker is running, keep track of what url it's using
-                    urls[msg.id] = msg.url;
+                // handle unwanted worker exits
+                cluster.on("exit", function(worker, code) {
+                    if (code != 0) {
+                        console.log("Worker crashed! Spawning a replacement.");
+                        //fork a new worker with the same url as the one that crashed
+                        //lookup by worker id
+                        cluster.fork({
+                            PARSER_URL: urls[worker.id]
+                        });
+                    }
+                });
+                Object.keys(cluster.workers).forEach(function(id) {
+                    cluster.workers[id].on('message', function(msg) {
+                        console.log(msg);
+                        //a new worker is running, keep track of what url it's using
+                        urls[msg.id] = msg.url;
+                    });
                 });
             });
         });
