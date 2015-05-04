696e1f1119947683b071d84a81cd74a12ffb1e13
Bitnami <howardc93@gmail.com>
2015-05-04 07:21:48 -0400

    added buildSets to build step

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -y install git redis-server mongodb maven default-jdk nodejs-legacy npm
+sudo apt-get -y install redis-server mongodb maven default-jdk nodejs-legacy npm
 sudo npm install -g n && sudo n stable
