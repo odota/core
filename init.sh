4f63cdc756381da7fcc62c8c25a70d8d8cd80108
Bitnami <howardc93@gmail.com>
2015-05-05 03:55:18 -0400

    add build tools to init

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -y install redis-server mongodb maven default-jdk nodejs-legacy
+sudo apt-get -y install make g++ build-essential redis-server mongodb maven default-jdk nodejs-legacy
 sudo npm install -g n && sudo n stable
 sudo npm install -g npm
