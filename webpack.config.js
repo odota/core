f4ebd446825a862fea53e59969acf9ef37345a35
NickHH <nbh2@rice.edu>
2015-05-21 21:39:09 +0000

    added picks/bans for captains mode

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,8 @@ module.exports = {
 new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
-            "window.jQuery": "jquery"
+            "window.jQuery": "jquery",
+            "window.$": "jquery"
         })
     ]
 };
