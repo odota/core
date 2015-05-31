d931f75d79a9df8c6113d55aa737fd2cd4e66f40
Bitnami <howardc93@gmail.com>
2015-05-30 20:56:29 -0400

    removed buildSets from initial build

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var config = require('./config');
 var role = config.ROLE || config.FOREMAN_WORKER_NAME.split(".")[0];
 console.log(role);
 //constants are currently built pre-run and written to file
-//buildSets currently built pre-run and saved to redis, worker re-runs every 3 minutes, includes getRetriever, getParser, which could be separated
 //web requires constants
 //worker requires constants (fullhistory needs to iterate through heroes)
 //parseManager requires constants (processparse needs to map combat log names to hero ids)
-//scanner requires buildSets in order to avoid leaking players
-//parseManager requires getRetrievers to get replay url
-//parseManager requires getParsers, since we need to set concurrency before starting
+//buildSets currently built by worker, re-runs every 3 minutes, includes getRetriever, getParser, which could be separated
+//scanner requires buildSets in order to avoid leaking players, retries until available
+//parseManager requires getRetrievers to get replay url, retries until available
+//parseManager requires getParsers, since we need to set concurrency before starting, retries until available
 //retriever, parser, proxy are independent
 require("./" + role + ".js");
