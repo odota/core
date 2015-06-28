59cde3922b659ef103be6954efa0b08ca3fc5d7e
Bitnami <howardc93@gmail.com>
2015-06-28 00:32:14 -0400

    added date sort

--- public/js/ratingsChart.js
+++ public/js/ratingsChart.js
@@ -1,6 +1,10 @@
 module.exports = function ratingsChart(ratings) {
     $("#solo").append(ratings[0] ? ratings[0].soloCompetitiveRank : "N/A");
     $("#party").append(ratings[0] ? ratings[0].competitiveRank : "N/A");
+    //sort ratings by time
+    ratings.sort(function(a, b) {
+        return a.time - b.time;
+    });
     var times = ratings.map(function(r) {
         return new Date(r.time);
     });
