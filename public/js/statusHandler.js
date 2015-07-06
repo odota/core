be829209132635e1ae1707d997efd0975077dba3
Bitnami <howardc93@gmail.com>
2015-07-05 22:51:46 -0400

    streamlined footer, changed advanced match count display

--- public/js/statusHandler.js
+++ public/js/statusHandler.js
@@ -1,15 +1,15 @@
 module.exports = function statusHandler(data) {
     console.log(data);
-        for (var prop in data) {
-            if (typeof data[prop] === "object") {
-                $("#" + prop + " tbody").empty();
-                for (var i = 0; i < data[prop].length; i++) {
-                    $("<tr>" + "<td><a href='/matches/" + data[prop][i].match_id + "'>" + data[prop][i].match_id + "</a></td>" + "<td class='fromNow' data-time='" + (data[prop][i].start_time + data[prop][i].duration) + "'></td>" + "</tr>").appendTo($("#" + prop + " tbody"));
-                }
-                formatHtml();
+    for (var prop in data) {
+        if (typeof data[prop] === "object") {
+            $("#" + prop + " tbody").empty();
+            for (var i = 0; i < data[prop].length; i++) {
+                $("<tr>" + "<td><a href='/matches/" + data[prop][i].match_id + "'>" + data[prop][i].match_id + "</a></td>" + "<td>" + data[prop][i].match_seq_num + "</td>" + "<td class='fromNow' data-time='" + (data[prop][i].start_time + data[prop][i].duration) + "'></td>" + "</tr>").appendTo($("#" + prop + " tbody"));
             }
-            else {
-                $("#" + prop).html(data[prop]);
-            }
+            formatHtml();
         }
+        else {
+            $("#" + prop).html(data[prop]);
+        }
+    }
 }
