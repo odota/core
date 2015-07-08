71a25e1ed1c106ffe414652a4d3cba746a19a9e1
Bitnami <howardc93@gmail.com>
2015-07-08 18:33:38 -0400

    added load testing tool

---
+++ tasks/loadTest.js
@@ -0,0 +1,16 @@
+var db = require('../db');
+var async = require('async');
+var request = require('request');
+
+db.players.distinct("account_id",function(err, results){
+    console.log(results.length);
+    async.eachLimit(results, 20, function(account_id, cb){
+        console.time(account_id);
+        request("http://localhost:5000/players/"+account_id, function(err, resp, body){
+            console.timeEnd(account_id);
+            cb(err);
+        });
+    }, function(err){
+        console.log(err);
+    })
+})
