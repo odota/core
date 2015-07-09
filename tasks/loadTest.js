17027147f9071ce1e87b495677308b60c5dd3ded
Bitnami <howardc93@gmail.com>
2015-07-09 19:34:29 -0400

    stop words added

--- tasks/loadTest.js
+++ tasks/loadTest.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 db.players.distinct("account_id",function(err, results){
     console.log(results.length);
-    async.eachLimit(results, 20, function(account_id, cb){
+    async.eachLimit(results, 40, function(account_id, cb){
         console.time(account_id);
         request("http://localhost:5000/players/"+account_id, function(err, resp, body){
             console.timeEnd(account_id);
