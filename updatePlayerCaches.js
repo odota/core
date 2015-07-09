1bdba22342cc282a587fe15e12ff502eb5a4e70c
Bitnami <howardc93@gmail.com>
2015-07-09 03:17:42 -0400

    fixed bug when updating caches

--- updatePlayerCaches.js
+++ updatePlayerCaches.js
@@ -20,23 +20,29 @@ module.exports = function updatePlayerCaches(match, options, cb) {
         var reParse = docs.length && docs[0].parsed_data && options.type === "parsed";
         //console.log("reInsert: %s, reParse: %s", reInsert, reParse);
         if (reInsert || reParse) {
+            //we already aggregated this match
             return cb(err);
         }
         else {
             //insert players into db and update player caches
             async.each(match.players, function(p, cb) {
-                    db.players.findOne({
+                    db.players.update({
                         account_id: p.account_id
-                    }, function(err, player) {
+                    }, {
+                        $set: {
+                            account_id: p.account_id,
+                        }
+                    }, {
+                        upsert: true
+                    }, function(err) {
                         if (err) {
                             return cb(err);
                         }
-                        //if player cache doesn't exist, skip
-                        //if insignificant, skip
-                        //if this is a re-inserted match, skip
                         redis.get("player:" + p.account_id, function(err, result) {
-                            player.cache = result && !err ? JSON.parse(result) : null;
-                            if (player.cache && isSignificant(constants, match)) {
+                            //if player cache doesn't exist, skip
+                            //if insignificant, skip
+                            var cache = result && !err ? JSON.parse(result) : null;
+                            if (cache && isSignificant(constants, match)) {
                                 //m.players[0] should be this player
                                 //m.all_players should be all players
                                 //duplicate this data into a copy to avoid corrupting original match object
@@ -46,19 +52,9 @@ module.exports = function updatePlayerCaches(match, options, cb) {
                                 //some data fields require computeMatchData in order to aggregate correctly
                                 computeMatchData(match_copy);
                                 //do aggregations on fields based on type
-                                player.cache.aggData = aggregator([match_copy], options.type, player.cache.aggData);
-                                redis.set("player:" + p.account_id, JSON.stringify(player.cache));
+                                cache.aggData = aggregator([match_copy], options.type, cache.aggData);
+                                redis.set("player:" + p.account_id, JSON.stringify(cache));
                             }
-                        });
-                        db.players.update({
-                            account_id: p.account_id
-                        }, {
-                            $set: {
-                                account_id: p.account_id,
-                            }
-                        }, {
-                            upsert: true
-                        }, function(err) {
                             cb(err);
                         });
                     });
@@ -67,4 +63,4 @@ module.exports = function updatePlayerCaches(match, options, cb) {
                 cb);
         }
     });
-}
+};
