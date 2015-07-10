a300d9b2bbab7f933d94e346cfb17f059646c6c8
Bitnami <howardc93@gmail.com>
2015-07-10 15:36:53 -0400

    added error handling to proxy

--- proxy.js
+++ proxy.js
@@ -11,6 +11,10 @@ app.listen(process.env.PORT);
 */
 //mirrors steam api
 var httpProxy = require('http-proxy');
-httpProxy.createProxyServer({
+var proxy = httpProxy.createProxyServer({
     target: 'http://api.steampowered.com'
 }).listen(process.env.PORT);
+proxy.on('error', function (err, req, res) {
+  console.log(err);
+  process.exit(1);
+});
