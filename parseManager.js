b014225df31b4bdcf5069b0c1b6506beb8ab6b01
Bitnami <howardc93@gmail.com>
2015-07-11 19:10:13 -0400

    enforcing exclusivity in parsemanager core use

--- parseManager.js
+++ parseManager.js
@@ -17,37 +17,59 @@ function start() {
         var capacity = parsers.length;
         if (cluster.isMaster && config.NODE_ENV !== "test") {
             console.log("[PARSEMANAGER] starting master");
+            var urls = {};
+            cluster.on('fork', function(worker) {
+                worker.on('message', function(msg) {
+                    console.log(msg);
+                    //a new worker is running, keep track of what url it's using
+                    urls[msg.pid] = msg.url;
+                    console.log(urls);
+                });
+            });
             for (var i = 0; i < capacity; i++) {
-                cluster.fork();
+                //fork a worker for each available parse core
+                cluster.fork({
+                    PARSER_URL: parsers[i]
+                });
             }
+            //TODO if we submitted a job with filename it must be parsed by localhost (on master)!
             cluster.on("exit", function(worker, code) {
                 if (code !== 0) {
-                    console.log("Worker crashed! Spawning a replacement.");
-                    cluster.fork();
+                    console.log("Worker crashed! Spawning a replacement of worker %s", worker.process.pid);
+                    //give this new worker the parser url of the one that crashed
+                    cluster.fork({
+                        PARSER_URL: urls[worker.process.pid]
+                    });
                 }
             });
         }
         else {
-            console.log("[PARSEMANAGER] starting worker");
+            console.log("[PARSEMANAGER] starting worker with pid %s", process.pid);
+            process.send({
+                pid: process.pid,
+                url: process.env.PARSER_URL
+            });
             //process requests
-            //TODO if we submitted a job via kue (with filename) it must be parsed by localhost!
             jobs.process('request_parse', function(job, cb) {
-                job.parser_url = parsers[0];
-                processParse(job, cb);
+                getParserEnv(job, function() {
+                    processParse(job, cb);
+                });
             });
             //process regular parses
             jobs.process('parse', function(job, cb) {
                 console.log("starting job: %s", job.id);
-                //RR or randomly select a parser for each job
-                getParserRandom(job, function() {
+                getParserEnv(job, function() {
                     processParse(job, cb);
                 });
             });
         }
 
+        function getParserEnv(job, cb) {
+            job.parser_url = process.env.PARSER_URL;
+            cb();
+        }
+
         function getParserRandom(job, cb) {
-            //TODO enable retrieving latest parser list at selection time
-            //TODO restrict each worker to use a specific parser url to prevent collision
             job.parser_url = parsers[Math.floor(Math.random() * parsers.length)];
             cb();
         }
