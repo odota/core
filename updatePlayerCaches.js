ac4f5e916838a337c08f2326341babd4da25918e
Bitnami <howardc93@gmail.com>
2015-07-15 07:03:15 -0400

    fixed tests

--- updatePlayerCaches.js
+++ updatePlayerCaches.js
@@ -28,67 +28,60 @@ module.exports = function updatePlayerCaches(match, options, cb) {
         //determine if we're reparsing this match
         var reParse = doc && doc.parsed_data && options.type === "parsed";
         //console.log("reInsert: %s, reParse: %s", reInsert, reParse);
-        if (reInsert || reParse) {
-            //we already aggregated this match
-            return cb(err);
-        }
-        else {
-            //insert players into db and update player caches
-            async.each(match.players, function(p, cb) {
-                    redis.get("player:" + p.account_id, function(err, result) {
-                        //if player cache doesn't exist, skip
-                        //if insignificant, skip
-                        var cache = result && !err ? JSON.parse(zlib.inflateSync(new Buffer(result, 'base64'))) : null;
-                        if (cache) {
-                            //do aggregations only if significant
-                            if (isSignificant(constants, match)) {
-                                //m.players[0] should be this player
-                                //m.all_players should be all players
-                                //duplicate this data into a copy to avoid corrupting original match object
-                                var match_copy = JSON.parse(JSON.stringify(match));
-                                match_copy.all_players = match.players.slice(0);
-                                match_copy.players = [p];
-                                //some data fields require computeMatchData in order to aggregate correctly
-                                computeMatchData(match_copy);
-                                //do aggregations on fields based on type
-                                cache.aggData = aggregator([match_copy], options.type, cache.aggData);
-                            }
-                            //add match to array
-                            var ids = {};
-                            //deduplicate matches by id
-                            cache.data.forEach(function(m) {
-                                ids[m.match_id] = m;
-                            });
-                            //update this match with latest state (parse_status/skill may have changed)
-                            ids[match_copy.match_id] = reduceMatch(match_copy);
-                            cache.data = [];
-                            for (var key in ids) {
-                                cache.data.push(ids[key]);
-                            }
-                            redis.setex("player:" + p.account_id, 60 * 60 * 24 * 7, zlib.deflateSync(JSON.stringify(cache)).toString('base64'));
+        async.each(match.players, function(p, cb) {
+                redis.get("player:" + p.account_id, function(err, result) {
+                    //if player cache doesn't exist, skip
+                    //if insignificant, skip
+                    var cache = result && !err ? JSON.parse(zlib.inflateSync(new Buffer(result, 'base64'))) : null;
+                    if (cache) {
+                        //do aggregations only if significant and we didn't do them already
+                        if (isSignificant(constants, match) && !reInsert && !reParse) {
+                            //m.players[0] should be this player
+                            //m.all_players should be all players
+                            //duplicate this data into a copy to avoid corrupting original match object
+                            var match_copy = JSON.parse(JSON.stringify(match));
+                            match_copy.all_players = match.players.slice(0);
+                            match_copy.players = [p];
+                            //some data fields require computeMatchData in order to aggregate correctly
+                            computeMatchData(match_copy);
+                            //do aggregations on fields based on type
+                            cache.aggData = aggregator([match_copy], options.type, cache.aggData);
                         }
-                        /*
-                        //temporarily disable inserting new players into db
-                        db.players.update({
-                            account_id: p.account_id
-                        }, {
-                            $set: {
-                                account_id: p.account_id,
-                            }
-                        }, {
-                            upsert: true
-                        }, function(err) {
-                            if (err) {
-                                return cb(err);
-                            }
-                            cb(err);
+                        //add match to array
+                        var ids = {};
+                        //deduplicate matches by id
+                        cache.data.forEach(function(m) {
+                            ids[m.match_id] = m;
                         });
-                        */
+                        //update this match with latest state (parse_status/skill may have changed)
+                        ids[match_copy.match_id] = reduceMatch(match_copy);
+                        cache.data = [];
+                        for (var key in ids) {
+                            cache.data.push(ids[key]);
+                        }
+                        redis.setex("player:" + p.account_id, 60 * 60 * 24 * 7, zlib.deflateSync(JSON.stringify(cache)).toString('base64'));
+                    }
+                    /*
+                    //temporarily disable inserting new players into db
+                    db.players.update({
+                        account_id: p.account_id
+                    }, {
+                        $set: {
+                            account_id: p.account_id,
+                        }
+                    }, {
+                        upsert: true
+                    }, function(err) {
+                        if (err) {
+                            return cb(err);
+                        }
                         cb(err);
                     });
-                },
-                //done with all 10 players
-                cb);
-        }
+                    */
+                    cb(err);
+                });
+            },
+            //done with all 10 players
+            cb);
     });
 };
