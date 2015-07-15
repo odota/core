e17e2ac8ec1455dbbdd6a5add2448cef136d528d
Bitnami <howardc93@gmail.com>
2015-07-15 07:34:39 -0400

    fixed caching bug

--- updatePlayerCaches.js
+++ updatePlayerCaches.js
@@ -34,19 +34,7 @@ module.exports = function updatePlayerCaches(match, options, cb) {
                     //if insignificant, skip
                     var cache = result && !err ? JSON.parse(zlib.inflateSync(new Buffer(result, 'base64'))) : null;
                     if (cache) {
-                        //do aggregations only if significant and we didn't do them already
-                        if (isSignificant(constants, match) && !reInsert && !reParse) {
-                            //m.players[0] should be this player
-                            //m.all_players should be all players
-                            //duplicate this data into a copy to avoid corrupting original match object
-                            var match_copy = JSON.parse(JSON.stringify(match));
-                            match_copy.all_players = match.players.slice(0);
-                            match_copy.players = [p];
-                            //some data fields require computeMatchData in order to aggregate correctly
-                            computeMatchData(match_copy);
-                            //do aggregations on fields based on type
-                            cache.aggData = aggregator([match_copy], options.type, cache.aggData);
-                        }
+                        var match_copy = JSON.parse(JSON.stringify(match));
                         //add match to array
                         var ids = {};
                         //deduplicate matches by id
@@ -58,6 +46,18 @@ module.exports = function updatePlayerCaches(match, options, cb) {
                         cache.data = [];
                         for (var key in ids) {
                             cache.data.push(ids[key]);
+                        }
+                        //do aggregations only if significant and we didn't do them already
+                        if (isSignificant(constants, match) && !reInsert && !reParse) {
+                            //m.players[0] should be this player
+                            //m.all_players should be all players
+                            //duplicate this data into a copy to avoid corrupting original match object
+                            match_copy.all_players = match.players.slice(0);
+                            match_copy.players = [p];
+                            //some data fields require computeMatchData in order to aggregate correctly
+                            computeMatchData(match_copy);
+                            //do aggregations on fields based on type
+                            cache.aggData = aggregator([match_copy], options.type, cache.aggData);
                         }
                         redis.setex("player:" + p.account_id, 60 * 60 * 24 * 7, zlib.deflateSync(JSON.stringify(cache)).toString('base64'));
                     }
