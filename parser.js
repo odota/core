045c32ee7e126d628767730996d7711dc17d936c
Bitnami <howardc93@gmail.com>
2015-07-23 09:53:43 -0400

    removed nulling

--- parser.js
+++ parser.js
@@ -73,8 +73,6 @@ app.get('/', function(req, res, next) {
     d.on('error', function(err) {
         parser.kill();
         bz.kill();
-        parser = null;
-        bz = null;
         outStream.end(JSON.stringify({
             "type": "error",
             "key": err
