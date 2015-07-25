6b23d9f381fd550cf9d9aefd718cf3092214dfde
Bitnami <howardc93@gmail.com>
2015-07-25 09:17:11 -0400

    check for err existence

--- processParse.js
+++ processParse.js
@@ -412,7 +412,7 @@ function runParse(job, ctx, cb) {
         else {
             console.log("error occurred, can't post-process");
         }
-        return cb(err.message || err.code || err, parsed_data);
+        return cb(err ? err.message || err.code : err, parsed_data);
     }
 
     function handleStream(e) {
