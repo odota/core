b36e8cc22dc9fa347f19b51d1629b7087dba0887
Bitnami <howardc93@gmail.com>
2015-08-09 00:43:44 -0400

    simplified getreplayurl

--- processParse.js
+++ processParse.js
@@ -1,35 +1,27 @@
-var db = require('./db');
-var fs = require('fs');
 var getReplayUrl = require('./getReplayUrl');
 var request = require('request');
-var domain = require('domain');
-var constants = require('./constants.json');
-var utility = require('./utility');
 var updatePlayerCaches = require('./updatePlayerCaches');
-var r = require('./redis');
-var redis = r.client;
 var moment = require('moment');
-var config = require('./config');
 module.exports = function processParse(job, ctx, cb) {
     var match_id = job.data.payload.match_id;
     var match = job.data.payload;
     console.time("parse " + match_id);
+    if (match.start_time < moment().subtract(7, 'days').format('X') && !match.fileName && !(match.leagueid > 0)) {
+        //expired, can't parse even if we have url, but parseable if we have a filename
+        //TODO non-valve urls don't expire, we can try using them
+        //TODO jobs with filename (submitted via kue) must have the file on the parse node!
+        //TODO do we want to write parse_status:1 to db?  we should not overwrite existing parse_status:2
+        console.log("replay too old, no filename and url expired");
+        console.timeEnd("parse " + match_id);
+        return cb();
+    }
     //get the replay url, update db
     getReplayUrl(match, function(err) {
         if (err) {
             return cb(err);
         }
-        //match object should now contain replay url, and should also be persisted to db
-        if (match.start_time < moment().subtract(7, 'days').format('X') && !match.fileName) {
-            //expired, can't parse even if we have url, but parseable if we have a filename
-            //TODO jobs with filename (submitted via kue) must be parsed by localhost (on master)!
-            //TODO improve current request test: we have no url in db and replay is expired on socket request, so that request fails, but our current test doesn't verify the parse succeeded
-            //TODO do we want to write parse_status:1 to db?  we should not overwrite existing parse_status:2
-            console.log("replay too old, url expired");
-            console.timeEnd("parse " + match_id);
-            return cb(err);
-        }
         else {
+            //match object should now contain replay url, and url should be persisted to db
             console.log("[PARSER] parsing from %s", job.data.payload.url || job.data.payload.fileName);
             var url = job.data.payload.url;
             var fileName = job.data.payload.fileName;
@@ -49,17 +41,13 @@ module.exports = function processParse(job, ctx, cb) {
                 match.match_id = match_id || parsed_data.match_id;
                 match.parsed_data = parsed_data;
                 match.parse_status = 2;
-                return updateDb();
-            });
-        }
-
-        function updateDb() {
-            //run aggregations on parsed data fields
-            updatePlayerCaches(match, {
-                type: "parsed"
-            }, function(err) {
-                console.timeEnd("parse " + match_id);
-                return cb(err);
+                //run aggregations on parsed data fields
+                updatePlayerCaches(match, {
+                    type: "parsed"
+                }, function(err) {
+                    console.timeEnd("parse " + match_id);
+                    return cb(err);
+                });
             });
         }
     });
