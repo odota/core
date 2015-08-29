24df433fef3f7ff506ea98859ae7ddda95480851
Bitnami <howardc93@gmail.com>
2015-08-29 16:46:25 -0400

    moved rapier code

--- js_parser/test_js_parser.sh
+++
@@ -1,3 +0,0 @@
-node `dirname "$0"`/example/example.js < testfiles/1698148651_source2.dem
-#node `dirname "$0"`/example/example.js < testfiles/1734886116_source2.dem
-#node `dirname "$0"`/example/example.js < testfiles/1731962898_source2.dem   
