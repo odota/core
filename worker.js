be46135325e23569a5fac8217cae32b265055dd7
Bitnami <howardc93@gmail.com>
2015-09-14 03:13:32 -0400

    fixes #641

--- worker.js
+++ worker.js
@@ -6,63 +6,15 @@ var updateNames = require('./tasks/updateNames');
 var buildSets = require('./buildSets');
 var utility = require('./utility');
 var invokeInterval = utility.invokeInterval;
-var domain = require('domain');
-var async = require('async');
 var numCPUs = require('os').cpus().length;
-//don't need these handlers when kue supports job ttl in 0.9?
-//ttl fails jobs rather than requeuing them
+console.log("[WORKER] starting worker");
+invokeInterval(buildSets, 60 * 1000);
 jobs.watchStuckJobs();
-process.on('SIGTERM', function() {
-    clearActiveJobs(function(err) {
-        process.exit(err || 1);
-    });
-});
-process.on('SIGINT', function() {
-    clearActiveJobs(function(err) {
-        process.exit(err || 1);
-    });
-});
-process.once('SIGUSR2', function() {
-    clearActiveJobs(function(err) {
-        console.log(err);
-        process.kill(process.pid, 'SIGUSR2');
-    });
-});
-var d = domain.create();
-d.on('error', function(err) {
-    console.log(err.stack);
-    clearActiveJobs(function(err2) {
-        process.exit(1);
-    });
-});
-d.run(function() {
-    console.log("[WORKER] starting worker");
-    //process requests (api call, waits for parse to complete)
-    jobs.process('request', numCPUs, processApi);
-    invokeInterval(buildSets, 60 * 1000);
-    //updatenames queues an api request
-    //probably should have name updating occur in a separate service
-    //jobs.process('api', processApi);
-    //invokeInterval(updateNames, 60 * 1000);
-    //invokeInterval(constants, 15 * 60 * 1000);
-});
-
-function clearActiveJobs(cb) {
-    jobs.active(function(err, ids) {
-        if (err) {
-            return cb(err);
-        }
-        async.mapSeries(ids, function(id, cb) {
-            kue.Job.get(id, function(err, job) {
-                if (job) {
-                    console.log("requeued job %s", id);
-                    job.inactive();
-                }
-                cb(err);
-            });
-        }, function(err) {
-            console.log("cleared active jobs");
-            cb(err);
-        });
-    });
-}
+//process requests (api call, waits for parse to complete)
+jobs.process('request', numCPUs, processApi);
+utility.cleanup(jobs, kue, 'request');
+//updatenames queues an api request
+//probably should have name updating occur in a separate service
+//jobs.process('api', processApi);
+//invokeInterval(updateNames, 60 * 1000);
+//invokeInterval(constants, 15 * 60 * 1000);
