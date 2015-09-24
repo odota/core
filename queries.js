a9bab7c9a752f2dcd4890a892ff5d787bd541d3e
Bitnami <howardc93@gmail.com>
2015-09-24 16:02:13 -0400

    reduce projection for fillplayernames

--- queries.js
+++ queries.js
@@ -23,7 +23,10 @@ function fillPlayerNames(players, cb) {
         }
     }, {
         fields: {
-            "cache": 0
+            "account_id": 1,
+            "last_visited": 1,
+            "personaname": 1,
+            "avatar": 1
         }
     }, function(err, docs) {
         if (err) {
