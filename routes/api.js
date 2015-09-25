6541d12ce3408167e5f8bff509baacd7807862e9
Bitnami <howardc93@gmail.com>
2015-09-24 20:57:22 -0400

    retire api/matches

--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,10 @@
 var express = require('express');
 var api = express.Router();
 var constants = require('../constants');
-var advQuery = require('../advquery');
-var preprocessQuery = require('../preprocessQuery');
 api.get('/items', function(req, res) {
     res.json(constants.items[req.query.name]);
 });
 api.get('/abilities', function(req, res) {
     res.json(constants.abilities[req.query.name]);
 });
-api.get('/matches', function(req, res, next) {
-    var draw = Number(req.query.draw);
-    var select = req.query.select || {};
-    var limit = Number(req.query.limit);
-    var js_agg = req.query.js_agg || {};
-    //support sort hash, or order+columns
-    var js_sort = req.query.js_sort || makeSort(req.query.order, req.query.columns) || {};
-    var js_limit = Number(req.query.length) || 1;
-    var js_skip = Number(req.query.start) || 0;
-    preprocessQuery(select);
-    advQuery({
-        select: select,
-        project: null,
-        limit: Math.min(limit, 100) || 100,
-        js_agg: js_agg,
-        js_limit: js_limit,
-        js_skip: js_skip,
-        //js_sort: js_sort
-    }, function(err, result) {
-        if (err) {
-            return next(err);
-        }
-        result = {
-            draw: draw,
-            aggData: result.aggData,
-            recordsTotal: result.unfiltered_count,
-            recordsFiltered: result.data.length,
-            data: result.page
-        };
-        res.json(result);
-    });
-});
-
-function makeSort(order, columns) {
-    //Makes sort from a datatables call
-    var sort;
-    if (order && columns) {
-        sort = {};
-        order.forEach(function(s) {
-            var c = columns[Number(s.column)];
-            if (c) {
-                sort[c.data] = s.dir === 'desc' ? -1 : 1;
-            }
-        });
-    }
-    return sort;
-}
 module.exports = api;
