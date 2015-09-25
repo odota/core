44f21610ba750c7ead803fd0dc150058b8405bf4
Bitnami <howardc93@gmail.com>
2015-09-25 10:51:22 -0400

    more

--- migrations/sql.js
+++ migrations/sql.js
@@ -23,6 +23,7 @@ mongodb.get('players').find({}).each(function(p) {
 //matches.players to player_matches
 //subset of columns from matches to matches
 //subset of columns from players to players
+//do the radiant gold adv/xp adv migration while we're at it
 //CODECHANGE
 //rename parsed_data.players.gold, lh, xp -> (gold_t, lh_t, xp_t)
 //rename parsed_data.players.kills -> killed
