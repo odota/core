0d0dcf1d96d60c923e86fb59cdc65d8ea6ec5777
Howard Chung <howardc93@gmail.com>
2015-10-02 21:16:02 -0700

    Update updatePlayerCaches.js

--- updatePlayerCaches.js
+++ updatePlayerCaches.js
@@ -29,7 +29,8 @@ module.exports = function updatePlayerCaches(match, options, cb) {
             //we didn't add skill data because we didn't have this match in db, return immediately
             return cb(err);
         }
-        async.each(match.players || options.players, function(p, cb) {
+        var m = JSON.parse(JSON.stringify(match));
+        async.each(m.players || options.players, function(p, cb) {
                 //full cache
                 /*
                 var match_copy = JSON.parse(JSON.stringify(match));
@@ -65,7 +66,6 @@ module.exports = function updatePlayerCaches(match, options, cb) {
                     //if player cache doesn't exist, skip
                     var cache = result ? JSON.parse(zlib.inflateSync(new Buffer(result, 'base64'))) : null;
                     if (cache) {
-                        var m = JSON.parse(JSON.stringify(match));
                         if (options.type !== "skill") {
                             //m.players[0] should be this player
                             //m.all_players should be all players
