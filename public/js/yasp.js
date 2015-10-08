ebd4111df083b71b87cf367ec848a24bf5299786
Bitnami <howardc93@gmail.com>
2015-10-08 07:33:31 -0400

    moved some more stuff to base_layout

--- public/js/yasp.js
+++ public/js/yasp.js
@@ -1,17 +1,12 @@
 require('./ga.js');
+require('../../node_modules/font-awesome/css/font-awesome.css');
+require('../../node_modules/dota2-minimap-hero-sprites/assets/stylesheets/dota2minimapheroes.css');
 //require('../../node_modules/bootstrap/dist/css/bootstrap.css');
 //bundling darkly doesn't work right now due to webpack not properly resolving font paths
 //require('../../node_modules/bootswatch/darkly/bootstrap.css');
-require('bootstrap');
-require('wordcloud');
-window.h337 = require('../../node_modules/heatmap.js/build/heatmap.js');
-window.moment = require('moment');
-window.numeral = require('numeral');
-require('../../node_modules/font-awesome/css/font-awesome.css');
-require('../../node_modules/dota2-minimap-hero-sprites/assets/stylesheets/dota2minimapheroes.css');
 //require('jquery');
-//require('../../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
 //require('../../node_modules/datatables/media/js/jquery.dataTables.js');
+//require('../../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
 //require('../../node_modules/datatables-bootstrap3-plugin/media/js/datatables-bootstrap3.js');
 //require('../../node_modules/selectize/dist/css/selectize.bootstrap3.css');
 //require('selectize');
@@ -21,6 +16,11 @@ require('../../node_modules/dota2-minimap-hero-sprites/assets/stylesheets/dota2m
 //window.c3 = require('c3');
 //require('../../node_modules/cal-heatmap/cal-heatmap.css');
 //window.CalHeatMap = require('cal-heatmap');
+//require('bootstrap');
+//require('wordcloud');
+//window.h337 = require('../../node_modules/heatmap.js/build/heatmap.js');
+//window.moment = require('moment');
+//window.numeral = require('numeral');
 //require("./playerMatches.js")
 //require("./tooltips.js")
 //require("./formatHtml.js")
