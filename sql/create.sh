41c21c74a1b45b4d428ca8906d6776da39f66ad3
Anders Höckersten <anders@hockersten.se>
2015-10-13 19:42:58 +0200

    Use yasp db in production and yasp_test in test

---
+++ sql/create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo -u postgres dropdb yasp
+sudo -u postgres dropuser yasp
+sudo -u postgres createuser yasp
+sudo -u postgres psql -c "ALTER USER yasp WITH PASSWORD 'yasp';"
+sudo -u postgres createdb yasp --owner yasp
+cat sql/create_tables.sql | sudo -u postgres psql -d postgresql://yasp:yasp@localhost/yasp -f -
