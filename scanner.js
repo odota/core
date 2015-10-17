7d57e99033d804e3812dc107bf01e93547125c19
Howard Chung <howardc93@gmail.com>
2015-10-17 03:11:13 -0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -82,7 +82,7 @@ function scanApi(seq_num) {
                         //skipped, but only if not already queued
                         match.parse_status = 3;
                     }
-                    if (p.account_id in ratingPlayers && match.lobby_type === 7) {
+                    if (p.account_id in userPlayers && match.lobby_type === 7) {
                         //could possibly pick up MMR change for matches we don't add, this is probably ok
                         queueReq("mmr", {
                             match_id: match.match_id,
