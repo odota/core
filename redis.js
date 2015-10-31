3e0abd6b5ef3f0614dc7133d10984792b8219581
Bitnami <howardc93@gmail.com>
2015-10-31 05:15:22 -0400

    exit on redis error

--- redis.js
+++ redis.js
@@ -4,6 +4,7 @@ var client = redis.createClient(config.REDIS_URL, {
     detect_buffers: true
 });
 client.on('error', function(err) {
-    throw err;
+    console.error(err);
+    process.exit(1);
 });
 module.exports = client;
