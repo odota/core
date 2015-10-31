e2bf8ccec80a95c2a912ac8cc48fdace1a259d3b
Bitnami <howardc93@gmail.com>
2015-10-31 00:18:05 -0400

    added debugging to parser index

--- workParser.js
+++ workParser.js
@@ -19,6 +19,7 @@ if (cluster.isMaster) {
     });
     app.use(bodyParser.json());
     app.get('/', function(req, res) {
+        console.error("got visit to index");
         res.json({
             capacity: capacity,
             version: utility.getParseSchema().version
