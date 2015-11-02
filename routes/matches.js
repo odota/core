d1c55e84cc26b52b399cb321c922ba5c8a8c1239
Howard Chung <howardc93@gmail.com>
2015-11-02 13:41:47 -0800

    Update matches.js

--- routes/matches.js
+++ routes/matches.js
@@ -8,6 +8,9 @@ var renderMatch = compute.renderMatch;
 var constants = require('../constants.js');
 var matchPages = constants.match_pages;
 module.exports = function(db, redis) {
+    if (req.query.json) {
+        return res.status(500).json({error: "currently disabled"});
+    }
     matches.get('/:match_id/:info?', function(req, res, next) {
         console.time("match page");
         prepareMatch(req.params.match_id, function(err, match) {
@@ -17,8 +20,7 @@ module.exports = function(db, redis) {
             console.timeEnd("match page");
             var info = matchPages[req.params.info] ? req.params.info : "index";
             if (req.query.json) {
-                return res.status(500).json({error: "currently disabled"});
-                //return res.json(match);
+                return res.json(match);
             }
             res.render("match/match_" + info, {
                 route: info,
