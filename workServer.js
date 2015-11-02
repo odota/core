40b10ab5bf586de9237bb0ee99978db3413566f0
Bitnami <howardc93@gmail.com>
2015-11-02 02:29:47 -0500

    added debugging line

--- workServer.js
+++ workServer.js
@@ -24,6 +24,7 @@ buildSets(db, redis, function(err) {
 function start() {
     var pool_size = 200;
     queue.parse.process(pool_size, function(job, cb) {
+        console.log('loaded job %s into pool', job.jobId);
         //save the callback for this job
         job.cb = cb;
         job.ee = new EventEmitter();
