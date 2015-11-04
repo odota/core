e6bb402c58b5b0a2f63d31c58269d6b233a12824
Bitnami <howardc93@gmail.com>
2015-11-04 03:31:11 -0500

    some new queries

---
+++ sql/rowsize.sql
@@ -0,0 +1,2 @@
+SELECT octet_length(t.*::text) FROM matches AS t WHERE match_id = 1908876110;
+SELECT octet_length(t.*::text) FROM player_matches AS t WHERE match_id = 1908876110;
---
+++ sql/topratings.sql
@@ -0,0 +1,1 @@
+select * from (select distinct on(account_id) * from player_ratings) unique_account_id order by solo_competitive_rank desc nulls last;
