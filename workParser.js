b00468bdc26103c79bd4d3e8f553f2da68b509b0
Bitnami <howardc93@gmail.com>
2015-11-06 03:10:27 -0500

    removed work request timeout

--- workParser.js
+++ workParser.js
@@ -72,7 +72,6 @@ function getJob() {
     request({
         url: remote,
         json: true,
-        timeout: 30000
     }, function(err, resp, job) {
         if (err) {
             //wait interval, then get another job
