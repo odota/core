3a67d955b91d84b95ac640d5e64afa79ce0930e3
Howard Chung <howardc93@gmail.com>
2015-11-15 04:16:17 -0800

    Update fullhistory.js

--- runner/fullhistory.js
+++ runner/fullhistory.js
@@ -1,6 +1,6 @@
 var db = require('../db');
 var async = require('async');
-var queue = require('../queue').fullhistory;
+var queue = require('../queue');
 var queueReq = require('../utility').queueReq;
 /**
  * Get all players who have visited and don't have full history, and queue for full history
