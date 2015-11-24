fedad9eba9cccbaadbd08a722f44996304b81eab
Bitnami <howardc93@gmail.com>
2015-11-23 21:37:20 -0500

    null job after get and post

--- workServer.js
+++ workServer.js
@@ -114,6 +114,7 @@ function start() {
             delete active_jobs[job.jobId];
             clearTimeout(job.expire);
             job.cb(err);
+            job = null;
         };
         console.log('server sent jobid %s', job.jobId);
         return res.json({
@@ -136,7 +137,8 @@ function start() {
         console.log('received submitted work');
         if (active_jobs[req.body.jobId]) {
             var job = active_jobs[req.body.jobId];
-            job.submitWork(req.body);
+            job.submitWork(req.body)
+            job = null;
             return res.json({
                 error: null
             });
