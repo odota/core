2cde764639e89fba98cec822d9a494b0a60f0093
Albert Cui <aqc2109@columbia.edu>
2015-11-24 02:08:00 -0500

    start adding subsscriptions

--- routes/donate.js
+++ routes/donate.js
@@ -4,9 +4,11 @@ var paypal = require('paypal-rest-sdk');
 var config = require('../config');
 var paypal_id = config.PAYPAL_ID;
 var paypal_secret = config.PAYPAL_SECRET;
-var PAYMENT_SESSIONS = ["cheeseAmount", "cheeseTotal", "payerId", "paymentId"];
+var PAYMENT_SESSIONS = ["cheeseAmount", "cheeseTotal", "payerId", "paymentId", "paymentToken"];
 var root_url = config.ROOT_URL;
 var moment = require('moment');
+var url = require('url');
+
 paypal.configure({
     'mode': config.NODE_ENV === "production" ? 'live' : 'sandbox', //sandbox or live
     'client_id': paypal_id,
@@ -22,7 +24,99 @@ module.exports = function(db, redis) {
         });
     }).post(function(req, res, next) {
         var num = req.body.num;
-        if (!isNaN(num)) {
+        var subscription = req.body.subscription;
+        
+        if (subscription === "on" && !isNaN(num)) {
+            var billingPlanAttributes = {
+                "description": "Cheese Subscription to YASP",
+                "merchant_preferences": {
+                    "auto_bill_amount": "yes",
+                    "cancel_url": root_url + "/cancel",
+                    "initial_fail_amount_action": "continue",
+                    "max_fail_attempts": "1",
+                    "return_url": root_url + "/confirm-subscription",
+                    "setup_fee": {
+                        "currency": "USD",
+                        "value": "0"
+                    }
+                },
+                "name": "Monthly Cheese Sub",
+                "payment_definitions": [
+                    {
+                        "amount": {
+                            "currency": "USD",
+                            "value": num,
+                        },
+                        "cycles": "0",
+                        "frequency": "MONTH",
+                        "frequency_interval": "1",
+                        "name": "Monthly Cheese",
+                        "type": "REGULAR"
+                    }
+                ],
+                "type": "INFINITE"
+            };
+            
+            // Create billing plan
+            paypal.billingPlan.create(billingPlanAttributes, function(err, billingPlan) {
+                if (err) {
+                    return next(err);
+                }
+                
+                var billingPlanUpdateAttributes = [
+                    {
+                        "op": "replace",
+                        "path": "/",
+                        "value": {
+                            "state": "ACTIVE"
+                        }
+                    }
+                ];
+            
+                // Active the billing plan
+                paypal.billingPlan.update(billingPlan.id, billingPlanUpdateAttributes, function(err, response) {
+                    if (err) {
+                        return next(err);
+                    }
+                    
+                    // Paypal seems to be stupid and won't let me use isostring even though it says it
+                    // expects it in their documentation.
+                    var billingAgreementAttributes = {
+                        "name": "YASP Cheese Agreement",
+                        "description": "Agreement for a Monthly Cheese Subscription",
+                        "start_date": moment().add(1, 'h').format("YYYY-MM-DD[T]HH:mm:ss[Z]"),
+                        "plan": {
+                            "id": billingPlan.id
+                        },
+                        "payer": {
+                            "payment_method": "paypal"
+                        }
+                    };
+
+                    // Create the billing agreement
+                    paypal.billingAgreement.create(billingAgreementAttributes, function (err, billingAgreement) {
+                        if (err) {
+                            return next(err);
+                        }
+                        
+                        for (var index = 0; index < billingAgreement.links.length; index++) {
+                            if (billingAgreement.links[index].rel === 'approval_url') {
+                                var approval_url = billingAgreement.links[index].href;
+                                console.log("For approving subscription via Paypal, first redirect user to");
+                                console.log(approval_url);
+                                req.session.paymentToken = url.parse(approval_url, true).query.token;
+                                req.session.cheeseAmount = num;
+                                res.redirect(approval_url);
+                            }
+                        }
+                    });
+                })
+                
+            })
+            
+            
+        } else if (!isNaN(num)) {
+            
             var payment = {
                 "intent": "sale",
                 "payer": {
@@ -38,25 +132,27 @@ module.exports = function(db, redis) {
                         "currency": "USD"
                     },
                     "description": "Buying CHEESE x" + num
-        }]
+                }]
             };
+            
             paypal.payment.create(payment, function(err, payment) {
                 if (err) {
                     return next(err);
                 }
-                else {
-                    req.session.paymentId = payment.id;
-                    req.session.cheeseAmount = num;
-                    var redirectUrl;
-                    for (var i = 0; i < payment.links.length; i++) {
-                        var link = payment.links[i];
-                        if (link.method === 'REDIRECT') {
-                            redirectUrl = link.href;
-                        }
+                
+                req.session.paymentId = payment.id;
+                req.session.cheeseAmount = num;
+                var redirectUrl;
+                for (var i = 0; i < payment.links.length; i++) {
+                    var link = payment.links[i];
+                    if (link.method === 'REDIRECT') {
+                        redirectUrl = link.href;
                     }
-                    res.redirect(redirectUrl);
                 }
+                res.redirect(redirectUrl);
             });
+        } else {
+            next("Did not get a number for cheese amount.");
         }
     });
     donate.route('/confirm').get(function(req, res, next) {
@@ -110,6 +206,18 @@ module.exports = function(db, redis) {
             }
         });
     });
+    donate.route('/confirm-subscription').get(function(req, res, next) {
+        var cheeseAmount = req.session.cheeseAmount;
+        if (cheeseAmount) {
+            res.render("confirm-subscription", {
+                cheeseAmount: cheeseAmount
+            });    
+        } else {
+            clearPaymentSessions(req);
+            res.render("cancel");
+        }
+        
+    })
     donate.route('/thanks').get(function(req, res) {
         var cheeseCount = req.session.cheeseAmount;
         var cheeseTotal = req.user.cheese;
