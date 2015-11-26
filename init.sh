e5e5538194e4ef4320eacfbb1352a622cd7237fb
Bitnami <howardc93@gmail.com>
2015-11-26 15:57:53 -0500

    edit init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,2 @@
-sudo add-apt-repository ppa:chris-lea/redis-server
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -y install make g++ build-essential redis-server postgresql-9.5 maven default-jdk nodejs-legacy npm curl git
-sudo npm install -g n && sudo n latest
-sudo npm install -g npm
-sudo npm run create
+#set up openshift (oc and kubectl binaries)
+wget -qO- https://github.com/openshift/origin/releases/download/v1.1/openshift-origin-v1.1-ac7a99a-linux-amd64.tar.gz | tar -zxv -C /usr/local/bin
