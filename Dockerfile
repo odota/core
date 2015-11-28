5828e9f07615d2c306fd3afff9ab559765856dbd
Bitnami <howardc93@gmail.com>
2015-11-28 17:55:03 -0500

    update alternatives

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,7 @@ RUN echo "" > /root/.bashrc && \
     nvm use $NODE_VERSION && \
     npm install -g npm
 
-ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
+RUN update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/bin/java
 
 # Just add package.json to get the NPM install cached.
 ADD package.json /usr/src/yasp/
--- public/js/yasp.js
+++ public/js/yasp.js
@@ -1,32 +1,33 @@
-//require('./ga.js');
-//require('../../node_modules/font-awesome/css/font-awesome.css');
-//require('../../node_modules/dota2-minimap-hero-sprites/assets/stylesheets/dota2minimapheroes.css');
+require('./ga.js');
+require('../../node_modules/font-awesome/css/font-awesome.css');
+require('../../node_modules/dota2-minimap-hero-sprites/assets/stylesheets/dota2minimapheroes.css');
 //require('../../node_modules/bootstrap/dist/css/bootstrap.css');
-//bundling darkly doesn't work right now due to webpack not properly resolving font paths
-//require('../../node_modules/bootswatch/darkly/bootstrap.css');
-//require('jquery');
-//require('../../node_modules/datatables/media/js/jquery.dataTables.js');
-//require('../../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
-//require('../../node_modules/datatables-bootstrap3-plugin/media/js/datatables-bootstrap3.js');
-//require('../../node_modules/selectize/dist/css/selectize.bootstrap3.css');
-//require('selectize');
-//require('../../node_modules/qTip2/dist/jquery.qtip.css');
-//require('qTip2');
-//require('../../node_modules/c3/c3.css');
-//window.c3 = require('c3');
-//require('../../node_modules/cal-heatmap/cal-heatmap.css');
-//window.CalHeatMap = require('cal-heatmap');
-//require('bootstrap');
-//require('wordcloud');
-//window.h337 = require('../../node_modules/heatmap.js/build/heatmap.js');
-//window.moment = require('moment');
-//window.numeral = require('numeral');
-//require("./tooltips.js")
-//require("./formatHtml.js")
-//require("./buildMap.js")
-//require('./createHistogram.js')
-//require('./createCalHeatmap.js')
-//require('./ratingsChart.js')
-//require('./generateCharts.js')
-//require('./util.js')
-//require('./resize.js')
+require('../../node_modules/bootswatch/darkly/bootstrap.css');
+require('jquery');
+require('../../node_modules/datatables/media/js/jquery.dataTables.js');
+require('../../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
+require('../../node_modules/datatables-bootstrap3-plugin/media/js/datatables-bootstrap3.js');
+require('../../node_modules/selectize/dist/css/selectize.bootstrap3.css');
+require('selectize');
+require('../../node_modules/qTip2/dist/jquery.qtip.css');
+require('qTip2');
+require('../../node_modules/c3/c3.css');
+window.c3 = require('c3');
+require('../../node_modules/cal-heatmap/cal-heatmap.css');
+window.CalHeatMap = require('cal-heatmap');
+require('bootstrap');
+require('wordcloud');
+window.h337 = require('../../node_modules/heatmap.js/build/heatmap.js');
+window.moment = require('moment');
+window.numeral = require('numeral');
+require("./tooltips.js")
+require("./formatHtml.js")
+require("./buildMap.js")
+require('./createHistogram.js')
+require('./createCalHeatmap.js')
+require('./ratingsChart.js')
+require('./generateCharts.js')
+require('./util.js')
+require('./resize.js')
+require('../css/flaticon.css')
+require('../css/yasp.css')
