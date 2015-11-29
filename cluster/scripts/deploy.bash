e8d6dd4ae8116b814d38cc9f3a17ae30b478c838
Bitnami <howardc93@gmail.com>
2015-11-29 16:57:43 -0500

    change some configs

--- cluster/scripts/deploy.bash
+++ cluster/scripts/deploy.bash
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-TAG=
-if [ -n "$TRAVIS_COMMIT" ]; then
-  echo "Using travis commit ID as tag..."
-  TAG=$TRAVIS_COMMIT
+if [ -n "$DOCKER_EMAIL" ]; then
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  TAG=
+  if [ -n "$TRAVIS_COMMIT" ]; then
+    echo "Using travis commit ID as tag..."
+    TAG=$TRAVIS_COMMIT
+  fi
+  if [ -z "$TAG" ]; then
+    echo "Unable to determine tag to push to."
+  fi
+  echo "Pushing to yasp/yasp:${TAG}"
+  docker tag yasp/yasp:latest yasp/yasp:${TAG}
+  docker push yasp/yasp:${TAG}
+  docker push yasp/yasp:latest
 fi
-if [ -z "$TAG" ]; then
-  echo "Unable to determine tag to push to."
-fi
-echo "Pushing to yasp/yasp:${TAG}"
-docker tag yasp/yasp:latest yasp/yasp:${TAG}
-docker push yasp/yasp:${TAG}
-docker push yasp/yasp:latest
 
 if [ -n "$DEPLOY_WEBHOOK_URL" ]; then
   echo "Hitting deploy webhook URL..."
   curl $DEPLOY_WEBHOOK_URL/$DEPLOY_WEBHOOK_SECRET/$TRAVIS_BUILD_ID > /dev/null
 fi
 
-if [ -n "$KUBERNETES_HOST" ]; then
+if [ -n "$KUBERNETES_TOKEN" ]; then
   export PATH="$PATH:$TRAVIS_BUILD_DIR/test/testfiles"
   echo $PATH
-  npm run deploy
+  kubectl get rc -o name --selector tier=backend | cut -d '/' -f2 | xargs kubectl rolling-update --image=yasp/yasp:$TRAVIS_COMMIT --token=$KUBERNETES_TOKEN --server=$KUBERNETES_HOST:443
+else
+  kubectl get rc -o name --selector tier=backend | cut -d '/' -f2 | xargs kubectl rolling-update --image=yasp/yasp:`git rev-parse HEAD`
 fi
+
--- init-kubernetes.sh
+++ init-kubernetes.sh
@@ -10,9 +10,9 @@ source ./cluster/setup/gce.env
 #set up kubernetes cluster
 wget -q -O - https://get.k8s.io | bash
 
-#put secrets in .env (KEY=VALUE, one per line)
+#put secrets in prod.env (KEY=VALUE, one per line)
 #write secrets/config to kubernetes secret resource
-bash ./cluster/scripts/create-secrets.sh < .env | kubectl create -f -
+bash ./cluster/scripts/create-secrets.sh < prod.env | kubectl create -f -
 bash ./cluster/scripts/create-postgres-config.sh | kubectl create -f -
 bash ./cluster/scripts/create-redis-config.sh | kubectl create -f -
 
@@ -30,3 +30,6 @@ kubectl create -f ./cluster/yasp
 redis-cli config set requirepass yasp
 #set up postgres password
 psql -c "ALTER USER yasp with password yasp"
+
+#to update the cluster
+#npm run deploy
