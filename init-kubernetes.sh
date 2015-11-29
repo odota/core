2ed922d19df9c345f7faa3478df2599056cf93c3
Bitnami <howardc93@gmail.com>
2015-11-29 00:36:58 -0500

    update script

--- init-kubernetes.sh
+++ init-kubernetes.sh
@@ -13,31 +13,20 @@ source ./cluster/setup/gce.env
 #set up kubernetes cluster
 wget -q -O - https://get.k8s.io | bash
 
-#set up openshift on cluster
-bash ./cluster/config/openshift-origin/create.sh
-
-#login to openshift
-oc login http://os.yasp.co
-#create a new project
-oc new-project yasp
-
-#suppress secret warning
-oc secrets add serviceaccount/default secrets/api-keys
-
 #put secrets in .env (KEY=VALUE, one per line)
 #set up secrets
-bash ./cluster/scripts/create-secrets.sh < .env | oc create -f -
+bash ./cluster/scripts/create-secrets.sh < .env | kubectl create -f -
+bash ./cluster/scripts/create-postgres-config.sh < .env | kubectl create -f -
+bash ./cluster/scripts/create-redis-config.sh < .env | kubectl create -f -
 
 #add yasp services to cluster, redis, postgres
-oc create -f ./cluster/config/yasp
+kubectl create -f ./cluster/yasp
 
-#put redis data/config in GCE disk "disk-redis"
-#put postgres data/config in GCE disk "disk-postgres"
+#put redis data in GCE disk "disk-redis"
+#put postgres data in GCE disk "disk-postgres"
 
-#connect to master kubernetes node, then:
-
+#set up remote access from a kubernetes node
 #set up redis password
 redis-cli config set requirepass yasp
-
 #set up postgres password
 psql -c "ALTER USER yasp with password yasp"
