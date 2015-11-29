93fefd4c6eaf8f8415d49a97f447e78f80aadf73
Bitnami <howardc93@gmail.com>
2015-11-29 02:54:25 -0500

    change deployment

--- cluster/scripts/create-secrets.sh
+++ cluster/scripts/create-secrets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ENCODED=$(cat | base64 -w0)
-sed -e "s#{{secret_data}}#${ENCODED}#g" ../setup/secret-template.yaml
+sed -e "s#{{secret_data}}#${ENCODED}#g" ./cluster/setup/secret-template.yaml
