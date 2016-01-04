e864305e671fd94fc96829aa68109e9b73435b29
Bitnami <howardc93@gmail.com>
2016-01-03 20:04:38 -0500

    fix deploy

--- deploy.js
+++ deploy.js
@@ -2,26 +2,78 @@ var pm2 = require('pm2');
 var async = require('async');
 var config = require('./config');
 var args = process.argv.slice(2);
-var services = require("./deploy.json");
+var services = {
+    "apps": [
+        {
+            "script": "skill.js",
+            "role": "core"
+      },
+        {
+            "script": "mmr.js",
+            "role": "core"
+      },
+        {
+            "script": "parser.js",
+            "role": "core",
+            "exec_mode": "cluster",
+            "instances": 0
+      },
+        {
+            "script": "scanner.js",
+            "role": "core"
+      },
+        {
+            "script": "worker.js",
+            "role": "core"
+      },
+        {
+            "script": "fullhistory.js",
+            "role": "core",
+            "exec_mode": "cluster",
+            "instances": 0
+      },
+        {
+            "script": "retriever.js",
+            "role": "retriever"
+      },
+        {
+            "script": "proxy.js",
+            "role": "proxy"
+      },
+        {
+            "script": "web.js",
+            "role": "core",
+            "exec_mode": "cluster",
+            "instances": 0
+      }]
+};
 var apps = services.apps;
-if (config.ROLE === "retriever" || config.ROLE == "proxy") {
+if (config.ROLE === "retriever" || config.ROLE == "proxy")
+{
     //don't use pm2 for these node types
     require('./' + config.ROLE + ".js");
 }
-else {
-    pm2.connect(function() {
-        async.each(apps, function(app, cb) {
-            if (args[0] === "all" || app.role === config.ROLE || app.role === args[0]) {
-                if (app.script === "fullhistory.js") {
+else
+{
+    pm2.connect(function()
+    {
+        async.each(apps, function(app, cb)
+        {
+            if (args[0] === "all" || app.role === config.ROLE || app.role === args[0])
+            {
+                if (app.script === "fullhistory.js")
+                {
                     //scale fh worker based on number of steam proxies
                     app.instances = config.STEAM_API_HOST.split(",").length;
                 }
                 pm2.start(app, cb);
             }
-            else {
+            else
+            {
                 cb();
             }
-        }, function() {
+        }, function()
+        {
             pm2.disconnect();
             process.exit(0);
         });
