f562097880ba13c09db198a39fabf2335dbc2ff6
Bitnami <howardc93@gmail.com>
2016-01-04 07:26:53 -0500

    improve status histograms

--- init-kubernetes.sh
+++ init-kubernetes.sh
@@ -15,7 +15,7 @@ wget -q -O - https://get.k8s.io | bash
 #put redis data in GCE disk "disk-redis"
 gcloud compute disks create "disk-redis" --size "50" --zone "us-central1-f" --type "pd-ssd"
 #put postgres data in GCE disk "disk-postgres"
-gcloud compute disks create "disk-postgres" --size "50" --zone "us-central1-f" --type "pd-ssd"
+gcloud compute disks create "disk-postgres" --size "2000" --zone "us-central1-f" --type "pd-ssd"
 
 #create namespace
 kubectl create -f ./cluster/setup/namespace.yaml
@@ -30,19 +30,14 @@ bash ./cluster/scripts/create-redis-config.sh | kubectl create -f -
 kubectl create -f ./cluster/yasp
 
 #set up db on postgres node
-kubectl exec postgres-ltm1a "bash" -it
+kubectl exec -it postgres-ltm1a "bash"
 su postgres 
 bash
 createuser yasp
 psql -c "ALTER USER yasp WITH PASSWORD 'yasp';"
 createdb yasp --owner yasp
-psql postgres://yasp:yasp@postgres/yasp
-#create_tables.sql
+cat "sql/create_tables.sql" | kubectl exec postgres-cxo7r -i -- psql postgresql://yasp:yasp@postgres/yasp
 
-#copy old data
-rsync -rav --info=progress2 /var/lib/redis/ /newdisk/var/lib/redis
-rsync -rav --info=progress2 /var/lib/postgresql/9.4/main/ /var/lib/postgres/data/pgdata
- 
 #secure remote connections to redis/postgres
 #set up redis password
 #redis-cli config set requirepass yasp
@@ -51,3 +46,10 @@ rsync -rav --info=progress2 /var/lib/postgresql/9.4/main/ /var/lib/postgres/data
 
 #to update the cluster
 #npm run deploy
+
+#backup/restore
+pg_dump -d postgres://yasp:yasp@localhost/yasp -f yasp.sql --format=c
+#pg_restore -d postgres://yasp:yasp@localhost/yasp yasp.sql
+cat "yasp.sql" | kubectl exec postgres-cxo7r -i -- pg_restore -d postgres://yasp:yasp@localhost/yasp
+#mount disk-redis to /newdisk
+cp /var/lib/redis/dump.rdb /newdisk/dump.rdb'
--- status.js
+++ status.js
@@ -1,70 +1,97 @@
 var async = require('async');
 var moment = require('moment');
-module.exports = function getStatus(db, redis, queue, cb) {
+module.exports = function getStatus(db, redis, queue, cb)
+{
     console.time('status');
-    async.series({
-        matches: function(cb) {
+    async.series(
+    {
+        matches: function(cb)
+        {
             //db.from('matches').count().asCallback(function(err, count) {
-            db.raw("SELECT reltuples::bigint AS count FROM pg_class where relname='matches';").asCallback(function(err, count) {
+            db.raw("SELECT reltuples::bigint AS count FROM pg_class where relname='matches';").asCallback(function(err, count)
+            {
                 extractCount(err, count, cb);
             });
         },
-        players: function(cb) {
+        players: function(cb)
+        {
             //db.from('players').count().asCallback(function(err, count) {
-            db.raw("SELECT reltuples::bigint AS count FROM pg_class where relname='players';").asCallback(function(err, count) {
+            db.raw("SELECT reltuples::bigint AS count FROM pg_class where relname='players';").asCallback(function(err, count)
+            {
                 extractCount(err, count, cb);
             });
         },
-        user_players: function(cb) {
-            db.from('players').count().whereNotNull('last_login').asCallback(function(err, count) {
+        user_players: function(cb)
+        {
+            db.from('players').count().whereNotNull('last_login').asCallback(function(err, count)
+            {
                 extractCount(err, count, cb);
             });
         },
-        full_history_players: function(cb) {
-            db.from('players').count().whereNotNull('full_history_time').asCallback(function(err, count) {
+        full_history_players: function(cb)
+        {
+            db.from('players').count().whereNotNull('full_history_time').asCallback(function(err, count)
+            {
                 extractCount(err, count, cb);
             });
         },
-        tracked_players: function(cb) {
-            redis.get("trackedPlayers", function(err, res) {
+        tracked_players: function(cb)
+        {
+            redis.get("trackedPlayers", function(err, res)
+            {
                 res = res ? Object.keys(JSON.parse(res)).length : 0;
                 cb(err, res);
             });
         },
-        donated_players: function(cb) {
-            redis.get("donators", function(err, res) {
+        donated_players: function(cb)
+        {
+            redis.get("donators", function(err, res)
+            {
                 res = res ? Object.keys(JSON.parse(res)).length : 0;
                 cb(err, res);
             });
         },
-        cached_players: function(cb) {
-            redis.keys("player:*", function(err, result) {
+        cached_players: function(cb)
+        {
+            redis.keys("player:*", function(err, result)
+            {
                 cb(err, result.length);
             });
         },
-        error_500: function(cb) {
+        error_500: function(cb)
+        {
             redis.zcard("error_500", cb);
         },
-        matches_last_day: function(cb) {
+        matches_last_day: function(cb)
+        {
             redis.zcard("added_match", cb);
         },
-        last_added: function(cb) {
+        last_added: function(cb)
+        {
             db.from('matches').select(['match_id', 'duration', 'start_time']).orderBy('match_id', 'desc').limit(10).asCallback(cb);
         },
-        last_parsed: function(cb) {
+        last_parsed: function(cb)
+        {
             db.from('matches').select(['match_id', 'duration', 'start_time']).where('version', '>', 0).orderBy('match_id', 'desc').limit(10).asCallback(cb);
         },
-        parser: function(cb) {
-            redis.keys("parser:*", function(err, result) {
-                if (err) {
+        parser: function(cb)
+        {
+            redis.keys("parser:*", function(err, result)
+            {
+                if (err)
+                {
                     return cb(err);
                 }
-                async.map(result, function(zset, cb) {
-                    redis.zcard(zset, function(err, cnt) {
-                        if (err) {
+                async.map(result, function(zset, cb)
+                {
+                    redis.zcard(zset, function(err, cnt)
+                    {
+                        if (err)
+                        {
                             return cb(err);
                         }
-                        return cb(err, {
+                        return cb(err,
+                        {
                             hostname: zset.substring("parser:".length),
                             count: cnt
                         });
@@ -72,17 +99,24 @@ module.exports = function getStatus(db, redis, queue, cb) {
                 }, cb);
             });
         },
-        retriever: function(cb) {
-            redis.keys("retriever:*", function(err, result) {
-                if (err) {
+        retriever: function(cb)
+        {
+            redis.keys("retriever:*", function(err, result)
+            {
+                if (err)
+                {
                     return cb(err);
                 }
-                async.map(result, function(zset, cb) {
-                    redis.zcard(zset, function(err, cnt) {
-                        if (err) {
+                async.map(result, function(zset, cb)
+                {
+                    redis.zcard(zset, function(err, cnt)
+                    {
+                        if (err)
+                        {
                             return cb(err);
                         }
-                        return cb(err, {
+                        return cb(err,
+                        {
                             hostname: zset.substring("retriever:".length),
                             count: cnt
                         });
@@ -90,55 +124,88 @@ module.exports = function getStatus(db, redis, queue, cb) {
                 }, cb);
             });
         },
-        queue: function(cb) {
+        queue: function(cb)
+        {
             console.time('queue');
             //object with properties as queue types, each mapped to json object mapping state to count
-            async.map(Object.keys(queue), getQueueCounts, function(err, result) {
+            async.map(Object.keys(queue), getQueueCounts, function(err, result)
+            {
                 var obj = {};
-                result.forEach(function(r, i) {
+                result.forEach(function(r, i)
+                {
                     obj[Object.keys(queue)[i]] = r;
                 });
                 console.timeEnd('queue');
                 cb(err, obj);
             });
 
-            function getQueueCounts(type, cb) {
-                async.series({
-                    "wait": function(cb) {
+            function getQueueCounts(type, cb)
+            {
+                async.series(
+                {
+                    "wait": function(cb)
+                    {
                         redis.llen(queue[type].toKey("wait"), cb);
                     },
-                    "act": function(cb) {
+                    "act": function(cb)
+                    {
                         redis.llen(queue[type].toKey("active"), cb);
                     },
-                    "del": function(cb) {
+                    "del": function(cb)
+                    {
                         redis.zcard(queue[type].toKey("delayed"), cb);
                     },
-                    "comp": function(cb) {
+                    "comp": function(cb)
+                    {
                         redis.scard(queue[type].toKey("completed"), cb);
                     },
-                    "fail": function(cb) {
+                    "fail": function(cb)
+                    {
                         redis.scard(queue[type].toKey("failed"), cb);
                     }
                 }, cb);
             }
         },
-        load_times: function(cb) {
-            redis.lrange("load_times", 0, -1, cb);
+        load_times: function(cb)
+        {
+            redis.lrange("load_times", 0, -1, function(err, arr)
+            {
+                cb(err, generateCounts(arr, 5000));
+            });
         },
-        parse_delay: function(cb) {
-            redis.lrange("parse_delay", 0, -1, cb);
+        parse_delay: function(cb)
+        {
+            redis.lrange("parse_delay", 0, -1, function(err, arr)
+            {
+                cb(err, generateCounts(arr, 86400 * 1000));
+            });
         }
-    }, function(err, results) {
+    }, function(err, results)
+    {
         console.timeEnd('status');
         cb(err, results);
     });
 
-    function extractCount(err, count, cb) {
-        if (err) {
+    function generateCounts(arr, cap)
+    {
+        var res = {};
+        arr.forEach(function(e)
+        {
+            e = Math.min(e, cap);
+            res[e] = res[e] ? res[e] + 1 : 1;
+        });
+        return res;
+    }
+
+    function extractCount(err, count, cb)
+    {
+        if (err)
+        {
             return cb(err);
         }
         // We need the property "rows" for "matches" and "players". Others just need count
-        if (count.hasOwnProperty("rows")) {
+        if (count.hasOwnProperty("rows"))
+        {
             count = count.rows;
         }
         //psql counts are returned as [{count:'string'}].  If we want to do math with them we need to numberify them
