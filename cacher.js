8f4d591390293c172b77db63205de9cb001bf2d0
Bitnami <howardc93@gmail.com>
2016-01-03 20:00:50 -0500

    add debug traces

--- cacher.js
+++ cacher.js
@@ -3,6 +3,15 @@ var playerCache = require('./playerCache');
 var updateCache = playerCache.updateCache;
 queue.cache.process(1, processCache);
 
-function processCache(job, cb) {
-    updateCache(job.payload, cb);
+function processCache(job, cb)
+{
+    console.log('match: %s, account: %s', job.payload.match_id, job.payload.account_id);
+    updateCache(job.payload, function(err)
+    {
+        if (err)
+        {
+            console.error(err);
+        }
+        return cb(err);
+    });
 }
