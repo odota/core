0625adbc2bcb1ee7fe656e912cb66479a0815fb8
Bitnami <howardc93@gmail.com>
2016-01-06 21:22:34 -0500

    fix hostname call

--- parser.js
+++ parser.js
@@ -59,7 +59,7 @@ queue.parse.process(function(job, cb)
                     console.log(err);
                     return cb(err);
                 }
-                var hostname = os.hostname;
+                var hostname = os.hostname();
                 redis.zadd("parser:" + hostname, moment().format('X'), match.match_id);
                 redis.lpush("parse_delay", new Date() - (match.start_time + match.duration) * 1000);
                 redis.ltrim("parse_delay", 0, 10000);
