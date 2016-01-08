65dff2ef97bc7c49babd47b539846670b6ea7032
Bitnami <howardc93@gmail.com>
2016-01-08 08:31:59 -0500

    add postgres install source

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #STANDALONE
 sudo add-apt-repository ppa:openjdk-r/ppa
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get -y install make g++ build-essential redis-server postgresql-9.5 openjdk-8-jdk nodejs-legacy npm curl git maven cassandra
