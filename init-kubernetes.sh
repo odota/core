db2befa239c379bd97b76e2f42b7178efe950807
Bitnami <howardc93@gmail.com>
2016-02-02 02:08:54 -0500

    update kube script

--- init-kubernetes.sh
+++ init-kubernetes.sh
@@ -6,22 +6,35 @@ gcloud init
 
 gcloud components install kubectl
 
-#source cluster config to env
-source ./cluster/setup/gce.env
+export KUBERNETES_PROVIDER=gce
+export KUBE_GCE_ZONE=us-central1-b
+export MASTER_SIZE=n1-highmem-8
+export NODE_SIZE=n1-highcpu-2
+export NODE_DISK_SIZE=10GB
+export PREEMPTIBLE_NODE=true
+export KUBE_GCE_NETWORK=k8s
 
-#set up kubernetes cluster
-wget -q -O - https://get.k8s.io | bash
+export ENABLE_CLUSTER_MONITORING=googleinfluxdb
+export ENABLE_NODE_AUTOSCALER=true
+export ENABLE_DAEMONSETS=true
+export ENABLE_DEPLOYMENTS=true
 
-#copy k8s-yasp-minion-template, use hm, make new reliable instance(s) for dbs, preemptible for cassandra?
+export KUBE_UP_AUTOMATIC_CLEANUP=true
+
+curl -L https://github.com/kubernetes/kubernetes/releases/download/v1.2.0-alpha.6/kubernetes.tar.gz | tar xvz
+
+bash ./kubernetes/cluster/kube-up.sh
 
 #persistent disks
 gcloud compute disks create "disk-redis" --size "50" --zone "us-central1-b" --type "pd-ssd"
 gcloud compute disks create "disk-postgres" --size "2000" --zone "us-central1-b" --type "pd-ssd"
+#use persistent volumes/claims for cassandra storage?
+#gcloud compute disks create "disk-cassandra-1" --size "200" --zone "us-central1-b" --type "pd-ssd"
 
 #create namespace
 kubectl create -f ./cluster/setup/namespace.yaml
 
-kubectl config set-context peaceful-parity-87002_k8s-yasp --namespace=yasp
+kubectl config set-context peaceful-parity-87002_kubernetes --namespace=yasp
 
 #put secrets in prod.env (KEY=VALUE, one per line)
 #write secrets/config to kubernetes secret resource
@@ -40,30 +53,24 @@ su postgres
 bash
 createuser yasp
 psql -c "ALTER USER yasp WITH PASSWORD 'yasp';"
-psql -c "CREATE EXTENSION pg_trgm;"
 createdb yasp --owner yasp
+psql yasp -c "CREATE EXTENSION pg_trgm;"
 #exit remote shell
 cat "sql/create_tables.sql" | kubectl exec postgres-q4s59 -i -- psql postgresql://yasp:yasp@postgres/yasp
 
-#secure remote connections to redis/postgres
-#set up redis password
-#redis-cli config set requirepass yasp
-#set up postgres password
-#psql -c "ALTER USER yasp with password yasp"
-
-#to update the cluster
-#npm run deploy
-
 #backup/restore
-pg_dump -d postgres://yasp:yasp@localhost/yasp -f - --format=c -t players | kubectl exec postgres-i1f5k -i -- pg_restore -d postgres://yasp:yasp@localhost/yasp
-pg_dump -d postgres://yasp:yasp@localhost/yasp -f - --format=c -T players | kubectl exec postgres-rairo -i -- pg_restore -d postgres://yasp:yasp@localhost/yasp
-cat yasp.sql | kubectl exec postgres-rairo -i -- pg_restore -d postgres://yasp:yasp@localhost/yasp
-#pg_dump -d postgres://yasp:yasp@localhost/yasp -f - --format=c | kubectl exec postgres-i1f5k -i -- pg_restore -d postgres://yasp:yasp@localhost/yasp --clean --create
+pg_dump -d postgres://yasp:yasp@localhost/yasp -f - --format=c | kubectl exec postgres-rairo -i -- pg_restore -d postgres://yasp:yasp@localhost/yasp --verbose
 #mount disk-redis to /newdisk
 cp /var/lib/redis/dump.rdb /newdisk/dump.rdb
 
-#set nodeselector on monitoring-influxdb-grafana-v2 to reliable instance
-
 #teardown cluster
-source ./cluster/setup/gce.env
+#old prefix for teardown!
+export INSTANCE_PREFIX=k8s-yasp
+export KUBE_GCE_NETWORK=k8s
 bash ./kubernetes/cluster/kube-down.sh
+
+#deploy latest yasp to cluster with rolling update
+#npm run deploy
+
+#update kubernetes on cluster
+#bash ./kubernetes/cluster/gce/upgrade.sh
