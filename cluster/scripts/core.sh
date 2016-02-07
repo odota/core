4ab20fece83fac01dd575941299a847bfd14d5bd
Bitnami <howardc93@gmail.com>
2016-02-07 05:21:54 -0500

    add thp diable

--- cluster/scripts/core.sh
+++ cluster/scripts/core.sh
@@ -4,6 +4,7 @@ sudo mkdir -p /var/lib/redis
 sudo mount -o discard,defaults /dev/sdb /var/lib/redis
 sudo mkdir -p /var/lib/postgresql/data
 sudo mount -o discard,defaults /dev/sdc /var/lib/postgresql/data
+sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
 sudo docker run -d --name yasp --restart=always --net=host yasp/yasp:latest "node deploy.js && sleep infinity"
 sudo docker run -d --name redis --restart=always -v /yasp/cluster/setup/redis.conf:/etc/redis/redis.conf -v /var/lib/redis:/var/lib/redis/ --net=host redis:3 -- redis-server /etc/redis/redis.conf
 sudo docker run -d --name postgres --restart=always -u postgres -e "PGDATA=/var/lib/postgresql/data/pgdata" -v /var/lib/postgresql/data:/var/lib/postgresql/data -v /yasp/cluster/setup/pg_hba.conf:/etc/postgresql/pg_hba.conf -v /yasp/cluster/setup/postgresql.conf:/etc/postgresql/postgresql.conf --net=host postgres:9.5 -- postgres --config_file=/etc/postgresql/postgresql.conf
