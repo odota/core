90b3a0519bad3ffaa26dbd5e09835043bbbc02db
Bitnami <howardc93@gmail.com>
2016-02-11 00:57:14 -0500

    use temp table in ranking query

--- sql/ranking.sql
+++ sql/ranking.sql
@@ -8,36 +8,32 @@ where account_id = ?
 
 --begin transaction
 --delete table if exists
+START TRANSACTION;
 DROP TABLE IF EXISTS hero_rankings;
---generate score table
-CREATE TABLE hero_rankings AS
-SELECT wins.account_id, hero_id, wins.count as wins, solo_competitive_rank, wins.count*pow(1.0005, solo_competitive_rank) as score
-FROM 
-(SELECT player_matches.account_id, hero_id, count(hero_id) from player_matches 
-JOIN 
-(SELECT match_id, radiant_win, lobby_type from matches) matches
-ON player_matches.match_id = matches.match_id
-WHERE (player_slot < 64) = radiant_win
---AND lobby_type = 7 --ranked only?
-group by player_matches.account_id, hero_id) wins
-JOIN 
+
+CREATE TEMPORARY TABLE curr_ratings ON COMMIT DROP AS
 (SELECT pr.account_id, solo_competitive_rank from player_ratings pr
 JOIN 
 (select account_id, max(time) as maxtime from player_ratings GROUP by account_id) grouped
 ON pr.account_id = grouped.account_id
 AND pr.time = grouped.maxtime
-WHERE pr.time > now() - INTERVAL '1 days' --rating isn't stale
-) curr_ratings
-ON wins.account_id = curr_ratings.account_id
+--WHERE pr.time > now() - INTERVAL '7 days' --rating isn't stale
+);
+
+--generate score table
+CREATE TABLE hero_rankings AS
+SELECT player_matches.account_id, hero_id, count(hero_id) as wins, max(solo_competitive_rank), count(hero_id)*pow(1.0005, max(solo_competitive_rank)) as score
+FROM
+curr_ratings
+JOIN player_matches
+ON player_matches.account_id = curr_ratings.account_id
+JOIN matches
+ON player_matches.match_id = matches.match_id
+WHERE (player_slot < 64) = radiant_win
+--AND lobby_type = 7 --ranked only?
+GROUP BY player_matches.account_id, hero_id;
 --WHERE hero_id = 53--debug
-ORDER BY hero_id, score desc;
+--ORDER BY hero_id, score desc;
 --index by hero_id, score
 --index by account_id
---end transaction
-
-SELECT * from player_ratings pr
-JOIN 
-(select account_id, max(time) as maxtime from player_ratings GROUP by account_id) grouped
-ON pr.account_id = grouped.account_id
-AND pr.time = grouped.maxtime
---WHERE time > now() - INTERVAL '1 days';
+COMMIT;
