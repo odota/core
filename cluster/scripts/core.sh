5893c096f0583ab88642744e877d49c918da33e0
Bitnami <howardc93@gmail.com>
2016-02-11 02:23:02 -0500

    deployment tweaks

--- cluster/scripts/core.sh
+++ cluster/scripts/core.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf yasp
 git clone https://github.com/yasp-dota/yasp
+
 sudo mkdir -p /var/lib/redis
 sudo mount -o discard,defaults /dev/sdb /var/lib/redis
 sudo mkdir -p /var/lib/postgresql/data
@@ -12,6 +13,15 @@ sudo docker rm -f yasp
 sudo docker rm -f redis
 sudo docker rm -f postgres
 
-sudo docker run -d --name yasp --restart=always --net=host yasp/yasp:latest "node deploy.js && sleep infinity"
+sudo docker run -d --name yasp --restart=always --net=host yasp/yasp:latest "sleep infinity"
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start web.js -i 0
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start worker.js -i 1
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start scanner.js -i 1
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start skill.js -i 1
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start mmr.js -i 1
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start fullhistory.js -i 4
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start cacher.js -i 2
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start requests.js -i 1
+sudo docker exec yasp ./node_modules/pm2/bin/pm2 start profiler.js -i 1
 sudo docker run -d --name redis --restart=always -v /yasp/cluster/setup/redis.conf:/etc/redis/redis.conf -v /var/lib/redis:/var/lib/redis/ --net=host redis:3 -- redis-server /etc/redis/redis.conf
 sudo docker run -d --name postgres --restart=always -u postgres -e "PGDATA=/var/lib/postgresql/data/pgdata" -v /var/lib/postgresql/data:/var/lib/postgresql/data -v /yasp/cluster/setup/pg_hba.conf:/etc/postgresql/pg_hba.conf -v /yasp/cluster/setup/postgresql.conf:/etc/postgresql/postgresql.conf --net=host postgres:9.5 -- postgres --config_file=/etc/postgresql/postgresql.conf
