0aff6aa26c6ea5a993d33b8c992e1a5feea47fc1
Bitnami <howardc93@gmail.com>
2016-02-10 23:20:06 -0500

    more work on ranking query

--- sql/ranking.sql
+++ sql/ranking.sql
@@ -1,6 +1,6 @@
 --per hero rankings for player
 /*
-select hero_id, account_id, percentile from hero_rankings
+select hero_id, percentile from hero_rankings
 JOIN (select hero_id, percentile(score) from hero_rankings hr where hero_id = hr.hero_id) pct
 ON hero_id = pct.hero_id
 where account_id = ?
@@ -8,22 +8,36 @@ where account_id = ?
 
 --begin transaction
 --delete table if exists
+DROP TABLE IF EXISTS hero_rankings;
 --generate score table
-SELECT wins.account_id, hero_id, wins.count as wins, solo_competitive_rank, wins.count*solo_competitive_rank as score
+CREATE TABLE hero_rankings AS
+SELECT wins.account_id, hero_id, wins.count as wins, solo_competitive_rank, wins.count*pow(1.0005, solo_competitive_rank) as score
 FROM 
 (SELECT player_matches.account_id, hero_id, count(hero_id) from player_matches 
 JOIN 
-(SELECT match_id, radiant_win, lobby_type from matches order by match_id desc limit 100000) matches
+(SELECT match_id, radiant_win, lobby_type from matches) matches
 ON player_matches.match_id = matches.match_id
 WHERE (player_slot < 64) = radiant_win
 --AND lobby_type = 7 --ranked only?
 group by player_matches.account_id, hero_id) wins
-JOIN player_ratings pr
-ON wins.account_id = pr.account_id--only players who have ratings
-WHERE time = (SELECT MAX(time) from player_ratings pr2 WHERE wins.account_id = pr2.account_id)
---AND hero_id = 53--debug
-AND wins.count >= 10--minimum win count on hero to reduce rows?
-ORDER BY score desc
+JOIN 
+(SELECT pr.account_id, solo_competitive_rank from player_ratings pr
+JOIN 
+(select account_id, max(time) as maxtime from player_ratings GROUP by account_id) grouped
+ON pr.account_id = grouped.account_id
+AND pr.time = grouped.maxtime
+WHERE pr.time > now() - INTERVAL '1 days' --rating isn't stale
+) curr_ratings
+ON wins.account_id = curr_ratings.account_id
+--WHERE hero_id = 53--debug
+ORDER BY hero_id, score desc;
 --index by hero_id, score
 --index by account_id
 --end transaction
+
+SELECT * from player_ratings pr
+JOIN 
+(select account_id, max(time) as maxtime from player_ratings GROUP by account_id) grouped
+ON pr.account_id = grouped.account_id
+AND pr.time = grouped.maxtime
+--WHERE time > now() - INTERVAL '1 days';
