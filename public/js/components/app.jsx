22d1e4d985764ad92a44d57b6cbb16982c129607
Bitnami <howardc93@gmail.com>
2016-02-22 01:24:37 -0500

    remove base component

--- public/js/components/app.jsx
+++ public/js/components/app.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import store from '../store'
-import Base from './base'
+import NavBar from './navbar'
+import Container from './container'
+import Footer from './footer'
 
 let reactElement = document.getElementById('react');
 
 render(
   <Provider store={store}>
-    <Base />
+    <div>
+      <NavBar />
+      <Container />
+      <Footer />
+    </div>
   </Provider>,
   reactElement
 )
