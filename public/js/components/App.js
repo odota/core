8b66ac048efa5d9fd2538978191896588c5d2581
Albert Cui <aqc2109@columbia.edu>
2016-04-08 05:34:03 +0000

    added a sidebar

--- public/js/components/App.js
+++ public/js/components/App.js
@@ -3,13 +3,26 @@ import { Link, browserHistory } from 'react-router'
 import NavBar from './NavBar';
 import Container from './Container';
 import Footer from './Footer';
+import AppBar from 'material-ui/lib/app-bar';
+import Navigation from './Navigation'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 export default function App({ children }) {
   return (
-    <div className='container'>
-      <NavBar />
-      <Container />
-      <Footer />
+    <div>
+      <div className='container'>
+        <AppBar/>
+        <Navigation />
+        //<NavBar />
+        <Container />
+        <Footer />
+      </div>
     </div>
   )
 }
---
+++ public/js/components/Navigation.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import LeftNav from 'material-ui/lib/left-nav';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import World from 'material-ui/lib/svg-icons/social/public';
+import RaisedButton from 'material-ui/lib/raised-button';
+import {Motion, spring} from 'react-motion';
+
+export default class Navigation extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        expand: false,
+        width: 54
+    };
+    
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
+  }
+
+  onMouseOver() {
+    this.setState({expand: true});
+  }
+  
+  onMouseOut() {
+    this.setState({expand: false});
+  }
+  
+
+  render() {
+    return (
+        <div>
+          <Motion style={{x: spring(this.state.expand ? 200 : 52)}}>
+            {({x}) =>
+              <LeftNav
+                open={true}
+                width={x}
+                containerStyle={{
+                  top: '64px'
+                }}
+                style={{
+                  transition: 'width'
+                }}
+              >
+                <List
+                  onMouseEnter={this.onMouseOver}
+                  onMouseLeave={this.onMouseOut}
+                >
+                  <ListItem 
+                    primaryText="Global" 
+                    leftIcon={<World />}
+                    initiallyOpen={true}
+                    primaryTogglesNestedList={true}
+                    nestedItems={[
+                      <ListItem
+                        key={1}
+                        primaryText="MM Stats"
+                      />,
+                      <ListItem
+                        key={2}
+                        primaryText="Distributions"
+                      />
+                    ]}
+                  />
+                </List>
+              </LeftNav>
+            }
+          </Motion>
+        </div>
+    );
+  }
+}
