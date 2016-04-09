c4cd9c37869ed6b64d69e06c337ee33624eb8db8
Howard Chung <howardc93@gmail.com>
2016-04-09 12:29:58 -0700

    css before js

--- public/js/yasp.js
+++ public/js/yasp.js
@@ -1,30 +1,30 @@
-require('./ga.js');
 require('../../node_modules/font-awesome/css/font-awesome.css');
 require('../../node_modules/dota2-minimap-hero-sprites/assets/stylesheets/dota2minimapheroes.css');
 require('../../node_modules/bootstrap/dist/css/bootstrap.css');
 require('../../node_modules/bootswatch/darkly/bootstrap.css');
+require('../../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
+require('../../node_modules/selectize/dist/css/selectize.bootstrap3.css');
+require('../../node_modules/qtip2/dist/jquery.qtip.css');
+require('../../node_modules/c3/c3.css');
+require('../../node_modules/cal-heatmap/cal-heatmap.css');
+require('../../node_modules/flag-icon-css/css/flag-icon.css');
+require('../css/flaticon.css')
+require('../css/yasp.css')
+require('./ga.js');
 require('jquery');
 //require('datatables.net');
 //require('datatables.net-bs');
 require('../../node_modules/datatables/media/js/jquery.dataTables.js');
-require('../../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
 require('../../node_modules/datatables-bootstrap3-plugin/media/js/datatables-bootstrap3.js');
-require('../../node_modules/selectize/dist/css/selectize.bootstrap3.css');
 require('selectize');
-require('../../node_modules/qtip2/dist/jquery.qtip.css');
 require('qtip2');
-require('../../node_modules/c3/c3.css');
 window.c3 = require('c3');
-require('../../node_modules/cal-heatmap/cal-heatmap.css');
 window.CalHeatMap = require('cal-heatmap');
 require('bootstrap');
 require('wordcloud');
 window.h337 = require('../../node_modules/heatmap.js/heatmap.js');
 window.moment = require('moment');
 window.numeral = require('numeral');
-// window.Resumable = require('resumablejs');
-//window.Plotly = require('plotly.js');
-require('../../node_modules/flag-icon-css/css/flag-icon.css');
 require("./tooltips.js")
 require("./formatHtml.js")
 require("./buildMap.js")
@@ -35,8 +35,6 @@ require('./generateCharts.js')
 require('./util.js')
 require('./resize.js')
 require('./requestForm.js')
-require('../css/flaticon.css')
-require('../css/yasp.css')
 window.constants = {
     player_colors: require('../../json/player_colors.json')
 };
