b077d4ee33c633ca60ddde5b6b6150ace6c33f63
Bitnami <howardc93@gmail.com>
2016-04-28 12:46:38 -0400

    update constants

--- monitor.js
+++ monitor.js
@@ -149,7 +149,7 @@ var health = {
             return cb(err,
             {
                 metric: result.rows[0].pg_database_size,
-                threshold: 4800000000000
+                threshold: 5000000000000
             });
         });
     }
