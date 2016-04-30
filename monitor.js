7d57127e39df110d32a548803e90a49c83cb3c09
Howard Chung <howardc93@gmail.com>
2016-04-29 22:50:02 -0700

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -149,7 +149,7 @@ var health = {
             return cb(err,
             {
                 metric: result.rows[0].pg_database_size,
-                threshold: 5000000000000
+                threshold: 5200000000000
             });
         });
     }
