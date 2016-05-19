5addde3863e85efb7b77b16a3f91cc74a242cccb
Bitnami <howardc93@gmail.com>
2016-05-18 23:19:25 -0400

    support multiple cassandra contact points

--- store/cassandra.js
+++ store/cassandra.js
@@ -4,11 +4,15 @@
 var cass = require('cassandra-driver');
 var config = require('../config');
 var url = require('url');
-var u = url.parse(config.CASSANDRA_URL);
+var spl = config.CASSANDRA_URL.split(',');
+var cps = spl.map(function(u)
+{
+    return url.parse(u).host;
+});
 console.error('connecting %s', config.CASSANDRA_URL);
 var cassandra = new cass.Client(
 {
-    contactPoints: [u.host],
-    keyspace: u.path.substring(1)
+    contactPoints: cps,
+    keyspace: url.parse(spl[0]).path.substring(1),
 });
 module.exports = cassandra;
