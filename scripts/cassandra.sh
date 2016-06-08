b7e0918c0d51d3b17ee473fd1ca9d00b88db937f
Ubuntu <howardc93@gmail.com>
2016-06-08 07:13:37 +0000

    add a sudo

--- scripts/cassandra.sh
+++ scripts/cassandra.sh
@@ -5,5 +5,5 @@ sudo mkdir -p /var/lib/cassandra
 sudo mount -o discard,defaults /dev/disk/by-id/google-persistent-disk-1 /var/lib/cassandra
 
 #remove seed if initial node
-docker run --name cassandra --restart=always -d --net=host -v /var/lib/cassandra:/var/lib/cassandra -e CASSANDRA_SEEDS=cassandra-1 cassandra:3
+sudo docker run --name cassandra --restart=always -d --net=host -v /var/lib/cassandra:/var/lib/cassandra -e CASSANDRA_SEEDS=cassandra-1 cassandra:3
 sudo docker start cassandra
