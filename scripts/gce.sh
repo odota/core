7735eb85cca11c8bac3480c654b727a547c70fb5
Ubuntu <howardc93@gmail.com>
2016-06-08 05:27:59 +0000

    edit gce script

--- scripts/gce.sh
+++ scripts/gce.sh
@@ -14,10 +14,10 @@ gcloud compute instances create redis-1 --machine-type n1-highmem-4 --image cont
 gcloud compute instances add-metadata redis-1 --metadata-from-file startup-script=./scripts/redis.sh
 
 #cassandra
-gcloud compute --project "peaceful-parity-87002" disks create "disk-cassandra-3" --size "2000" --zone "us-central1-b" --type "pd-standard"
-gcloud compute instances delete -q cassandra-3
-gcloud compute instances create cassandra-3 --machine-type n1-highmem-4 --image container-vm --boot-disk-size 10GB --boot-disk-type pd-ssd --disk "name=disk-cassandra-3,device-name=persistent-disk-1" 
-gcloud compute instances add-metadata cassandra-3 --metadata-from-file startup-script=./scripts/cassandra.sh
+gcloud compute --project "peaceful-parity-87002" disks create "disk-cassandra-4" --size "2000" --zone "us-central1-b" --type "pd-standard"
+gcloud compute instances delete -q cassandra-4
+gcloud compute instances create cassandra-4 --machine-type n1-highmem-4 --image container-vm --boot-disk-size 10GB --boot-disk-type pd-ssd --disk "name=disk-cassandra-3,device-name=persistent-disk-1" 
+gcloud compute instances add-metadata cassandra-4 --metadata-from-file startup-script=./scripts/cassandra.sh
 
 #web, health check, loadbalancer
 gcloud compute forwarding-rules delete -q lb-rule
@@ -58,7 +58,7 @@ gcloud compute instance-groups managed set-autoscaling "parser-group-1" --cool-d
 #task nodes
 gcloud compute instance-groups managed delete -q task-group-1
 gcloud compute instance-templates delete -q task-1
-gcloud compute instance-templates create task-1 --preemptible --machine-type n1-highcpu-8 --image container-vm --boot-disk-size 50GB --boot-disk-type pd-ssd --metadata startup-script='#!/bin/bash
+gcloud compute instance-templates create task-1 --preemptible --machine-type n1-highcpu-16 --image container-vm --boot-disk-size 50GB --boot-disk-type pd-ssd --metadata startup-script='#!/bin/bash
 sudo docker run -d --name task -e "POSTGRES_URL=postgres://yasp:yasp@10.240.0.5:5432/yasp" --restart=always --net=host yasp/yasp:latest sh -c "curl -H \"Metadata-Flavor: Google\" -L http://metadata.google.internal/computeMetadata/v1/project/attributes/env > /usr/src/yasp/.env && node dev/postgresToCassandra.js 0 2500000000"
 '
 gcloud compute instance-groups managed create "task-group-1" --base-instance-name "task-group-1" --template "task-1" --size "1"
