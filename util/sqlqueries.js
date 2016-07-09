03b54b346e75152164347d6dc6da58cb90fa2608
Ubuntu <howardc93@gmail.com>
2016-07-09 03:14:50 +0000

    add some queries

--- util/sqlqueries.js
+++ util/sqlqueries.js
@@ -1,52 +1,79 @@
 module.exports = {
     "players":
     {
-        "name": 'Players',
+        "name": "Players",
         "sql": `
 SELECT * from notable_players;
             `,
     },
-    /*
     "players_most_damage":
     {
-        "name": 'Players, most damage dealt',
+        "name": "Players, most damage dealt",
         "sql": `
-SELECT pm.account_id, name, sum(value) as sum
-FROM match_logs ml
-JOIN player_matches pm
-ON ml.sourcename_slot = pm.player_slot
-AND ml.match_id = pm.match_id
+SELECT pm.account_id, name, sum(hero_damage) as sum
+FROM player_matches pm
 JOIN matches m
-ON m.match_id = ml.match_id
+ON m.match_id = pm.match_id
 JOIN notable_players np
 ON pm.account_id = np.account_id
-WHERE type = 'DOTA_COMBATLOG_DAMAGE'
 GROUP BY pm.account_id, name
-ORDER BY sum desc;
+ORDER BY sum desc NULLS LAST
+LIMIT 100;
             `,
     },
-    "players_most_damage_taken":
+    /*
+        "players_most_damage_taken":
+        {
+            "name": "Players, most damage taken",
+            "sql": `
+    SELECT pm.account_id, name, sum(value) as sum
+    FROM match_logs ml
+    JOIN player_matches pm
+    ON ml.targetname_slot = pm.player_slot
+    AND ml.match_id = pm.match_id
+    JOIN matches m
+    ON m.match_id = ml.match_id
+    JOIN notable_players np
+    ON pm.account_id = np.account_id
+    WHERE type = "DOTA_COMBATLOG_DAMAGE"
+    GROUP BY pm.account_id, name
+    ORDER BY sum desc;
+                `,
+        },
+        */
+    "players_most_healing":
     {
-        "name": 'Players, most damage taken',
+        "name": "Players, most healing",
         "sql": `
-SELECT pm.account_id, name, sum(value) as sum
-FROM match_logs ml
-JOIN player_matches pm
-ON ml.targetname_slot = pm.player_slot
-AND ml.match_id = pm.match_id
+SELECT pm.account_id, name, sum(hero_healing) as sum
+FROM player_matches pm
 JOIN matches m
-ON m.match_id = ml.match_id
+ON m.match_id = pm.match_id
 JOIN notable_players np
 ON pm.account_id = np.account_id
-WHERE type = 'DOTA_COMBATLOG_DAMAGE'
 GROUP BY pm.account_id, name
-ORDER BY sum desc;
-            `,
+ORDER BY sum desc NULLS LAST
+LIMIT 100;
+        `,
     },
-    */
+    "players_most_tower_damage":
+    {
+        "name": "Players, most tower damage",
+        "sql": `
+SELECT pm.account_id, name, sum(tower_damage) as sum
+FROM player_matches pm
+JOIN matches m
+ON m.match_id = pm.match_id
+JOIN notable_players np
+ON pm.account_id = np.account_id
+GROUP BY pm.account_id, name
+ORDER BY sum desc NULLS LAST
+LIMIT 100;
+        `,
+    },
     "players_most_lh10":
     {
-        "name": 'Players, most LH@10',
+        "name": "Players, most LH@10",
         "sql": `
 SELECT lh_t[10], np.name, m.match_id, le.name as leaguename
 FROM matches m
@@ -61,28 +88,9 @@ LIMIT 100;
             `,
     },
     /*
-        "players_most_healing":
-        {
-            "name": 'Players, most healing',
-            "sql": `
-    SELECT pm.account_id, name, sum(value) as sum
-    FROM match_logs ml
-    JOIN player_matches pm
-    ON ml.targetname_slot = pm.player_slot
-    AND ml.match_id = pm.match_id
-    JOIN matches m
-    ON m.match_id = ml.match_id
-    JOIN notable_players np
-    ON pm.account_id = np.account_id
-    WHERE type = 'DOTA_COMBATLOG_HEAL'
-    GROUP BY pm.account_id, name
-    ORDER BY sum desc;
-            `,
-        },
-        */
     "players_fastest_manta":
     {
-        "name": 'Players, fastest Manta',
+        "name": "Players, fastest Manta",
         "sql": `
 SELECT time, valuename, pm.account_id, name, m.match_id, leagueid
 FROM match_logs ml
@@ -93,27 +101,66 @@ ON ml.match_id = pm.match_id
 AND ml.targetname_slot = pm.player_slot
 JOIN notable_players np
 ON pm.account_id = np.account_id
-WHERE type = 'DOTA_COMBATLOG_PURCHASE'
-AND valuename = 'item_manta'
+WHERE type = "DOTA_COMBATLOG_PURCHASE"
+AND valuename = "item_manta"
 ORDER BY time ASC
 LIMIT 100;
         `,
     },
+    */
     "players_most_games":
     {
-        "name": 'Players, most pro games',
+        "name": "Players, most pro games",
         "sql": `
-SELECT pm.account_id, np.name, count(*)
+SELECT pm.account_id, np.name,
+count(*),
+sum(case when ((pm.player_slot < 128) = m.radiant_win) then 1 else 0 end) wins
 FROM player_matches pm
 JOIN notable_players np
 ON pm.account_id = np.account_id
+JOIN matches m
+ON pm.match_id = m.match_id
 GROUP BY pm.account_id, np.name
 ORDER BY count DESC;
         `,
     },
+    "players_most_games_abaddon":
+    {
+        "name": "Players, most pro games on Abaddon",
+        "sql": `
+SELECT pm.account_id, np.name,
+count(*),
+sum(case when ((pm.player_slot < 128) = m.radiant_win) then 1 else 0 end) wins
+FROM player_matches pm
+JOIN notable_players np
+ON pm.account_id = np.account_id
+JOIN matches m
+ON pm.match_id = m.match_id
+WHERE pm.hero_id = 1
+GROUP BY pm.account_id, np.name
+ORDER BY count DESC;
+            `,
+    },
+    "players_most_games_furion":
+    {
+        "name": "Players, most pro games on Nature's Prophet",
+        "sql": `
+SELECT pm.account_id, np.name,
+count(*),
+sum(case when ((pm.player_slot < 128) = m.radiant_win) then 1 else 0 end) wins
+FROM player_matches pm
+JOIN notable_players np
+ON pm.account_id = np.account_id
+JOIN matches m
+ON pm.match_id = m.match_id
+WHERE pm.hero_id = 53
+GROUP BY pm.account_id, np.name
+ORDER BY count DESC;
+            `,
+    },
     "heroes_most_picked_banned":
     {
-        "name": 'Heroes, most picked/banned',
+        "name": "Heroes, most picked/banned",
         "sql": `
 SELECT pb.hero_id,
 sum(case when ((pm.player_slot < 128) = m.radiant_win) then 1 else 0 end) wins, 
@@ -131,7 +178,7 @@ ORDER BY picks DESC;
     },
     "matches_most_recent":
     {
-        "name": 'Matches, most recent',
+        "name": "Matches, most recent",
         "sql": `
 SELECT match_id, t1.name AS radiant_team_name, t2.name AS dire_team_name, le.name, start_time, duration
 FROM matches ma
@@ -153,7 +200,7 @@ LIMIT 100;
         "sql": `
 SELECT *
 FROM information_schema.columns
-WHERE table_schema = 'public';
+WHERE table_schema = "public";
         `,
     },
     */
