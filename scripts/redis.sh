38bcdf44f4e3c9fe4565c6df1293e7eb3d562eb5
Howard Chung <howardc93@gmail.com>
2016-08-03 19:11:55 -0700

    Update redis.sh

--- scripts/redis.sh
+++ scripts/redis.sh
@@ -5,5 +5,5 @@ sudo mkdir -p /var/lib/redis
 sudo mount -o discard,defaults /dev/disk/by-id/google-persistent-disk-1 /var/lib/redis
 sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
-sudo docker run -d --name redis --restart=always -v /redis.conf:/usr/local/etc/redis/redis.conf -v /var/lib/redis:/data --net=host redis:3 /usr/local/bin/redis-server /usr/local/etc/redis/redis.conf
+sudo docker run -d --name redis --restart=always -v /redis.conf:/usr/local/etc/redis/redis.conf -v /var/lib/redis:/var/lib/redis --net=host redis:3 /usr/local/bin/redis-server /usr/local/etc/redis/redis.conf
 sudo docker start redis
