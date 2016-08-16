d1c159f80268f3f51d19fd12c4299cd73587e911
Howard Chung <howardc93@gmail.com>
2016-08-15 21:24:41 -0700

    Update backupscanner.js

--- svc/backupscanner.js
+++ svc/backupscanner.js
@@ -8,7 +8,7 @@ const cassandra = require('../store/cassandra');
 const queries = require('../store/queries');
 const insertMatch = queries.insertMatch;
 const config = require('../config');
-const PARALLELISM = config.STEAM_API_HOST.split(',').length * 10;
+const PARALLELISM = config.STEAM_API_HOST.split(',').length * 8;
 const delay = 1000;
 start();
 
