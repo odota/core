2e4b208e22e253645148390bf456b216268acecb
Ubuntu <howardc93@gmail.com>
2016-09-05 07:13:58 +0000

    remove parser push

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-#push image to docker hub
 if [ -n "$DOCKER_USERNAME" ]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  TAG=
-  if [ -n "$TRAVIS_COMMIT" ]; then
-    echo "Using travis commit ID as tag..."
-    TAG=$TRAVIS_COMMIT
-  fi
-  if [ -z "$TAG" ]; then
-    echo "Unable to determine tag to push to."
-  fi
-  docker tag yasp/yasp:latest yasp/yasp:${TAG}
-  docker push yasp/yasp:${TAG}
+  docker tag yasp/yasp:latest yasp/yasp:${TRAVIS_COMMIT}
+  docker push yasp/yasp:${TRAVIS_COMMIT}
   docker push yasp/yasp:latest
-  docker push yasp/parser:latest
 fi
