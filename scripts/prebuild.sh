afe41b4aa83141b28fb1a550712bc291ad858ec1
micael.bergeron <micael.bergeron@solutionstlm.com>
2016-09-07 13:18:13 -0400

    cassandra memory limit for non docker-compose env.

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 sudo service postgresql stop
-sudo docker run -d --privileged --name cassandra --net=host -e "CASSANDRA_LISTEN_ADDRESS=127.0.0.1" cassandra:3
+sudo docker run -d --privileged --name cassandra --net=host \
+    -e "CASSANDRA_LISTEN_ADDRESS=127.0.0.1" \
+    -e "MAX_HEAP_SIZE=128M" \
+    -e "HEAP_NEWSIZE=24M" \
+    cassandra:3
 sudo docker run -d --privileged --name postgres --net=host postgres:9.5
 sudo docker run -d --privileged --name redis --net=host redis:3
 sudo docker run -d --name parser --net=host yasp/parser
