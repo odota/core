3ce9966f6cfed23f90ad13b62c4290f7c0787bec
Howard Chung <howardc93@gmail.com>
2016-09-17 19:04:23 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo docker-compose up -d
-sudo docker exec -i odota-core sh -c 'npm run test'
+#sudo docker-compose up -d
+sudo service postgresql stop		
+sudo docker run -d --privileged --name cassandra --net=host -e "CASSANDRA_LISTEN_ADDRESS=127.0.0.1" cassandra:3		
+sudo docker run -d --privileged --name postgres --net=host postgres:9.5		
+sudo docker run -d --privileged --name redis --net=host redis:3		
+sudo docker run -d --name parser --net=host odota/parser		
+sudo docker build -t "odota/core" . 
+sudo docker run -i odota/core sh -c 'npm run test'
