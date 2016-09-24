0facc813a067a5ac8fb53190c074c46e88b35323
Ubuntu <howardc93@gmail.com>
2016-09-24 04:52:17 +0000

    update working directory

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const group = args[0] || process.env.GROUP;
 const cp = require('child_process');
 if (process.env.PROVIDER === 'gce')
 {
-  cp.execSync('curl -H "Metadata-Flavor: Google" -L http://metadata.google.internal/computeMetadata/v1/project/attributes/env > /usr/src/yasp/.env');
+  cp.execSync('curl -H "Metadata-Flavor: Google" -L http://metadata.google.internal/computeMetadata/v1/project/attributes/env > /usr/src/.env');
 }
 if (process.env.ROLE)
 {
