ca848ca2a807de8c5dfb6f04c9fd6bf5a1004446
Ubuntu <howardc93@gmail.com>
2016-09-29 07:03:47 +0000

    formatting

--- store/buildStatus.js
+++ store/buildStatus.js
@@ -5,8 +5,7 @@ const config = require('../config');
 const queue = require('./queue');
 const async = require('async');
 const moment = require('moment');
-module.exports = function buildStatus(db, redis, cb)
-{
+module.exports = function buildStatus(db, redis, cb) {
   console.time('status');
   redis.zremrangebyscore('added_match', 0, moment().subtract(1, 'day').format('X'));
   redis.zremrangebyscore('error_500', 0, moment().subtract(1, 'day').format('X'));
@@ -17,114 +16,92 @@ module.exports = function buildStatus(db, redis, cb)
   }).forEach((retkey) => {
     redis.zremrangebyscore(retkey, 0, moment().subtract(1, 'day').format('X'));
   });
-  async.series(
-    {
-      user_players(cb)
-        {
-        redis.zcard('visitors', cb);
-      },
-      tracked_players(cb)
-        {
-        redis.zcard('tracked', cb);
-      },
-      error_500(cb)
-        {
-        redis.zcard('error_500', cb);
-      },
-      matches_last_day(cb)
-        {
-        redis.zcard('added_match', cb);
-      },
-      matches_last_hour(cb)
-        {
-        redis.zcount('added_match', moment().subtract(1, 'hour').format('X'), '+inf', cb);
-      },
-      api_hits(cb)
-        {
-        redis.zcard('api_hits', cb);
-      },
-      last_added(cb)
-        {
-        redis.lrange('matches_last_added', 0, -1, (err, result) => {
-          return cb(err, result.map((r) => {
-            return JSON.parse(r);
-          }));
-        });
-      },
-      last_parsed(cb)
-        {
-        redis.lrange('matches_last_parsed', 0, -1, (err, result) => {
-          return cb(err, result.map((r) => {
-            return JSON.parse(r);
-          }));
-        });
-      },
-      parser(cb)
-        {
-        async.map(['parser'], (zset, cb) => {
-          redis.zcard(zset, (err, cnt) => {
-            if (err)
-                    {
-              return cb(err);
-            }
-            return cb(err,
-              {
-                hostname: zset,
-                count: cnt,
-              });
-          });
-        }, cb);
-      },
-      retriever(cb)
-        {
-        async.map(config.RETRIEVER_HOST.split(',').map((r) => {
-          return 'retriever:' + r.split('.')[0];
-        }), (zset, cb) => {
-          redis.zcard(zset, (err, cnt) => {
-            if (err)
-                    {
-              return cb(err);
-            }
-            return cb(err,
-              {
-                hostname: zset.substring('retriever:'.length),
-                count: cnt,
-              });
-          });
-        }, cb);
-      },
-      queue(cb)
-        {
-            // generate object with properties as queue types, each mapped to json object mapping state to count
-        queue.getCounts(redis, cb);
-      },
-      load_times(cb)
-        {
-        redis.lrange('load_times', 0, -1, (err, arr) => {
-          cb(err, generateCounts(arr, 1000));
-        });
-      },
-      health(cb)
-        {
-        redis.hgetall('health', (err, result) => {
-          if (err)
-                {
+  async.series({
+    user_players(cb) {
+      redis.zcard('visitors', cb);
+    },
+    tracked_players(cb) {
+      redis.zcard('tracked', cb);
+    },
+    error_500(cb) {
+      redis.zcard('error_500', cb);
+    },
+    matches_last_day(cb) {
+      redis.zcard('added_match', cb);
+    },
+    matches_last_hour(cb) {
+      redis.zcount('added_match', moment().subtract(1, 'hour').format('X'), '+inf', cb);
+    },
+    api_hits(cb) {
+      redis.zcard('api_hits', cb);
+    },
+    last_added(cb) {
+      redis.lrange('matches_last_added', 0, -1, (err, result) => {
+        return cb(err, result.map((r) => {
+          return JSON.parse(r);
+        }));
+      });
+    },
+    last_parsed(cb) {
+      redis.lrange('matches_last_parsed', 0, -1, (err, result) => {
+        return cb(err, result.map((r) => {
+          return JSON.parse(r);
+        }));
+      });
+    },
+    parser(cb) {
+      async.map(['parser'], (zset, cb) => {
+        redis.zcard(zset, (err, cnt) => {
+          if (err) {
             return cb(err);
           }
-          for (const key in result)
-                {
-            result[key] = JSON.parse(result[key]);
-          }
-          cb(err, result ||
-                {});
+          return cb(err, {
+            hostname: zset,
+            count: cnt,
+          });
         });
-      },
-    }, (err, results) => {
+      }, cb);
+    },
+    retriever(cb) {
+      async.map(config.RETRIEVER_HOST.split(',').map((r) => {
+        return 'retriever:' + r;
+      }), (zset, cb) => {
+        redis.zcard(zset, (err, cnt) => {
+          if (err) {
+            return cb(err);
+          }
+          return cb(err, {
+            hostname: zset,
+            count: cnt,
+          });
+        });
+      }, cb);
+    },
+    queue(cb) {
+      // generate object with properties as queue types, each mapped to json object mapping state to count
+      queue.getCounts(redis, cb);
+    },
+    load_times(cb) {
+      redis.lrange('load_times', 0, -1, (err, arr) => {
+        cb(err, generateCounts(arr, 1000));
+      });
+    },
+    health(cb) {
+      redis.hgetall('health', (err, result) => {
+        if (err) {
+          return cb(err);
+        }
+        for (const key in result) {
+          result[key] = JSON.parse(result[key]);
+        }
+        cb(err, result || {});
+      });
+    },
+  }, (err, results) => {
     cb(err, results);
   });
 
-  function generateCounts(arr, cap)
-    {
+  function generateCounts(arr, cap) {
     const res = {};
     arr.forEach((e) => {
       e = Math.min(e, cap);
--- util/getGCData.js
+++ util/getGCData.js
@@ -9,65 +9,54 @@ const moment = require('moment');
 const getData = utility.getData;
 const queries = require('../store/queries');
 const buildReplayUrl = utility.buildReplayUrl;
-module.exports = function getGCData(db, redis, match, cb)
-{
-  db.first().from('match_gcdata').where(
-    {
-      match_id: match.match_id,
-    }).asCallback((err, gcdata) => {
-      if (err)
-        {
-        return cb(err);
-      }
-      if (gcdata && gcdata.replay_salt)
-        {
-        console.log('found cached replay url for %s', match.match_id);
-        match.url = buildReplayUrl(gcdata.match_id, gcdata.cluster, gcdata.replay_salt);
-        return cb(err);
-      }
-      else
-        {
-        const retrievers = retrieverConfig.split(',').map((r) => {
-          return 'http://' + r + '?key=' + secret;
-        });
-        const result = retrievers;
-            // make array of retriever urls and use a random one on each retry
-        const urls = result.map((r) => {
-          return r + '&match_id=' + match.match_id;
-        });
-        getData(urls, (err, body, metadata) => {
-          if (err || !body || !body.match || !body.match.replay_salt)
-                {
-                    // non-retryable error
-            return cb('invalid body or error');
-          }
-                // count retriever calls
-          redis.zadd('retriever:' + metadata.hostname.split('.')[0], moment().format('X'), match.match_id);
-          match.url = buildReplayUrl(match.match_id, body.match.cluster, body.match.replay_salt);
-          const parties = {};
-          if (body.match.players)
-                {
-            body.match.players.forEach((p) => {
-              if (p.party_id)
-                        {
-                parties[p.player_slot] = p.party_id.low;
-              }
-            });
-          }
-                // Persist GC data to database
-          queries.upsert(db, 'match_gcdata',
-            {
-              match_id: match.match_id,
-              cluster: body.match.cluster,
-              replay_salt: body.match.replay_salt,
-              series_id: body.match.series_id,
-              series_type: body.match.series_type,
-              parties,
-            },
-            {
-              match_id: body.match.match_id,
-            }, cb);
-        });
-      }
-    });
+module.exports = function getGCData(db, redis, match, cb) {
+  db.first().from('match_gcdata').where({
+    match_id: match.match_id,
+  }).asCallback((err, gcdata) => {
+    if (err) {
+      return cb(err);
+    }
+    if (gcdata && gcdata.replay_salt) {
+      console.log('found cached replay url for %s', match.match_id);
+      match.url = buildReplayUrl(gcdata.match_id, gcdata.cluster, gcdata.replay_salt);
+      return cb(err);
+    } else {
+      const retrievers = retrieverConfig.split(',').map((r) => {
+        return 'http://' + r + '?key=' + secret;
+      });
+      const result = retrievers;
+      // make array of retriever urls and use a random one on each retry
+      const urls = result.map((r) => {
+        return r + '&match_id=' + match.match_id;
+      });
+      getData(urls, (err, body, metadata) => {
+        if (err || !body || !body.match || !body.match.replay_salt) {
+          // non-retryable error
+          return cb('invalid body or error');
+        }
+        // count retriever calls
+        redis.zadd('retriever:' + metadata.hostname, moment().format('X'), match.match_id);
+        match.url = buildReplayUrl(match.match_id, body.match.cluster, body.match.replay_salt);
+        const parties = {};
+        if (body.match.players) {
+          body.match.players.forEach((p) => {
+            if (p.party_id) {
+              parties[p.player_slot] = p.party_id.low;
+            }
+          });
+        }
+        // Persist GC data to database
+        queries.upsert(db, 'match_gcdata', {
+          match_id: match.match_id,
+          cluster: body.match.cluster,
+          replay_salt: body.match.replay_salt,
+          series_id: body.match.series_id,
+          series_type: body.match.series_type,
+          parties,
+        }, {
+          match_id: body.match.match_id,
+        }, cb);
+      });
+    }
+  });
 };
