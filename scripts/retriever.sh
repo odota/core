0a9b06dd17218b077b5c8d1fca153875ee103c0a
Ubuntu <howardc93@gmail.com>
2016-09-30 11:59:26 +0000

    new retriever script

--- scripts/retriever.sh
+++ scripts/retriever.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# Setup
 curl -sSL https://get.docker.com/ | sh
 sudo docker run -d --name=retriever --restart=always --net=host --log-opt max-size=1g -e RETRIEVER_PORT=80 -e PROVIDER=gce -e ROLE=retriever odota/core:latest sh -c "npm start"
+
+# If already initialized
 sudo docker start retriever
-# Reboot the instance after a timeout
-sleep 900 && gcloud compute instances delete $(hostname) -q &
+sudo docker logs -f retriever && gcloud compute instances delete $(hostname) -q &
