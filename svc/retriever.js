e920847d92c43a738b3f6574ec573038dce0005d
Albert Cui <albertcui@users.noreply.github.com>
2016-10-16 14:13:53 -0700

    Fix howard's incompetence
    * startIndex < 0 causes retriever to hang as there are no accounts to log into.
    * Change number of accounts to 15 to reduce collision chance.

--- svc/retriever.js
+++ svc/retriever.js
@@ -24,8 +24,8 @@ let users = config.STEAM_USER.split(',');
 let passes = config.STEAM_PASS.split(',');
 if (config.PROVIDER === 'gce' && config.STEAM_ACCOUNT_DATA) {
   const accountData = cp.execSync(`curl '${config.STEAM_ACCOUNT_DATA}'`).toString().split(/\r\n|\r|\n/g);
-  const accountsToUse = 30;
-  const startIndex = Math.floor((Math.random() * accountData.length - accountsToUse));
+  const accountsToUse = 15;
+  const startIndex = Math.floor((Math.random() * (accountData.length - accountsToUse)));
   console.log('total registered accounts: %s, startIndex: %s', accountData.length, startIndex);
   const accountDataToUse = accountData.slice(startIndex, startIndex + accountsToUse);
   users = accountDataToUse.map(a => a.split('\t')[0]);
