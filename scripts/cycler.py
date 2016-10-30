58a05a897f5d321bf342b57b95890908f8cd523c
Ubuntu <howardc93@gmail.com>
2016-10-30 20:14:48 +0000

    update cycler

--- scripts/cycler.py
+++ scripts/cycler.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 
+from itertools import cycle
 import subprocess
 import time
 
-# For completeness this should also create the backend and the HTTP load balancer
-while True:
+def start():
+  # For completeness this should also create the backend, HTTP load balancer, template, and network
   # Get the available zones
   zones = subprocess.check_output("gcloud compute zones list --format='value(NAME)'", shell=True)
   zoneList = zones.strip().split('\n')
-  # sort by zone ID
-  zoneList = sorted(zoneList, key=lambda x: x[-1])
+  # sort by zone letter (last character)
+  # zoneList = sorted(zoneList, key=lambda x: x[-1])
+  print zoneList
   for i, zone in enumerate(zoneList):
     backendname = "retriever"
-    templatename = "retriever-2"
+    templatename = "retriever-1"
     instancegroupname = "retriever-group-" + zone
     print i, zone, instancegroupname
     # Create the instance group
@@ -21,12 +23,29 @@ while True:
     subprocess.call("gcloud compute instance-groups managed set-instance-template {} --quiet --zone={} --template={}".format(instancegroupname, zone, templatename), shell=True)
     # Add it to backend
     subprocess.call("gcloud compute backend-services add-backend {} --quiet --instance-group={} --instance-group-zone={}".format(backendname, instancegroupname, zone), shell=True)
-    # Scale the instance group if it's the correct bucket
-    bucketsize = 86400 // len(zoneList)
-    unixhour = time.time() // bucketsize
-    bucket = unixhour % len(zoneList)
-    size = 24 if i == bucket else 0
-    print unixhour, bucket, size
-    subprocess.call("gcloud compute instance-groups managed resize {} --quiet --zone={} --size={}".format(instancegroupname, zone, size), shell=True)
-  # Wait half a bucket and redo
-  time.sleep(bucketsize // 2)
+  pool = cycle(zoneList)
+  while True:
+    # Consider all instances in each instance group connected in a chain
+    # Every iteration, slide the current window one slot
+    # Create the new instance in the next group first, then delete an instance in the current group
+    zone = next(pool)
+    instancegroupname = "retriever-group-" + zone
+    currentsize = int(subprocess.check_output("gcloud compute instance-groups managed describe {} --quiet --zone={} --format='value(targetSize)'".format(instancegroupname, zone), shell=True))
+    if currentsize > 0:
+      nextzone = next(pool)
+      nextinstancegroupname = "retriever-group-" + nextzone
+      nextsize = int(subprocess.check_output("gcloud compute instance-groups managed describe {} --quiet --zone={} --format='value(targetSize)'".format(nextinstancegroupname, nextzone), shell=True))
+      # Scale up the next zone
+      subprocess.call("gcloud compute instance-groups managed resize {} --quiet --zone={} --size={}".format(nextinstancegroupname, nextzone, nextsize + 1), shell=True)
+      # Scale down the current zone
+      subprocess.call("gcloud compute instance-groups managed resize {} --quiet --zone={} --size={}".format(instancegroupname, zone, currentsize - 1), shell=True)
+      # We want to cycle fast enough that each instance lives for approximately 15 minutes
+      time.sleep(900 // (currentsize + nextsize))
+      
+while True:
+  try:
+    start()
+  except:
+    pass
+  else:
+    break
--- scripts/retriever.sh
+++ scripts/retriever.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Setup
+# Docker install not needed if using gci image
 curl -sSL https://get.docker.com/ | sh
 sudo docker run -d --name=retriever --restart=always --net=host --log-opt max-size=1g -e RETRIEVER_PORT=80 -e PROVIDER=gce -e ROLE=retriever odota/core:latest sh -c "npm start"
 
 # If already initialized
 sudo docker start retriever
-sudo docker logs -f retriever && gcloud compute instances delete $(hostname) -q
+sudo docker logs -f retriever
