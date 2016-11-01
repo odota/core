13d58136c742979caa97a4d2b506b9459530116f
Howard Chung <howardc93@gmail.com>
2016-11-01 01:54:40 -0700

    Update gcdata.js

--- svc/gcdata.js
+++ svc/gcdata.js
@@ -5,7 +5,7 @@ const getGcData = require('../util/getGcData');
 const queue = require('../store/queue');
 const db = require('../store/db');
 const redis = require('../store/redis');
-queue.runQueue('gcQueue', 30, processGcData);
+queue.runQueue('gcQueue', 1, processGcData);
 
 function processGcData(job, cb) {
   getGcData(db, redis, job, cb);
