ace4e5ca540a3b72fbe6098cd744df4017104e9f
Ubuntu <howardc93@gmail.com>
2016-11-12 11:45:22 +0000

    lint backupscanner

--- svc/backupscanner.js
+++ svc/backupscanner.js
@@ -1,16 +1,85 @@
 const async = require('async');
 const utility = require('../util/utility');
-const generateJob = utility.generateJob;
-const getData = utility.getData;
 const redis = require('../store/redis');
 const queries = require('../store/queries');
-const insertMatch = queries.insertMatch;
 const config = require('../config');
-const api_keys = config.STEAM_API_KEY.split(',');
-const api_hosts = config.STEAM_API_HOST.split(',');
-const parallelism = Math.min(api_hosts.length * 1, api_keys.length);
+
+const generateJob = utility.generateJob;
+const getData = utility.getData;
+const insertMatch = queries.insertMatch;
+const apiKeys = config.STEAM_API_KEY.split(',');
+const apiHosts = config.STEAM_API_HOST.split(',');
+const parallelism = Math.min(apiHosts.length * 1, apiKeys.length);
 const delay = 1000;
-start();
+
+function processMatch(matchId, cb) {
+  // Check if exists
+  redis.get(`scanner_insert:${matchId}`, (err, res) => {
+    if (err) {
+      return cb(err);
+    }
+    if (res) {
+      return cb();
+    }
+    const job = generateJob('api_details', {
+      match_id: matchId,
+    });
+    const url = job.url;
+    return getData({
+      url,
+      delay,
+    }, (err, body) => {
+      if (err) {
+        throw err;
+      }
+      if (!body.result) {
+        return cb();
+      }
+      const match = body.result;
+      return insertMatch(match, {
+        type: 'api',
+        origin: 'scanner',
+        skipCounts: false,
+        skipAbilityUpgrades: false,
+      }, (err) => {
+        if (!err) {
+          redis.set(`scanner_insert:${match.match_id}`, 1);
+        }
+        cb(err);
+      });
+    });
+  });
+}
+
+function processPlayer(accountId, cb) {
+  const ajob = generateJob('api_history', {
+    account_id: accountId,
+  });
+  getData({
+    url: ajob.url,
+    delay,
+  }, (err, body) => {
+    if (err) {
+      console.error(err);
+    }
+    if (!body || !body.result || !body.result.matches) {
+      // Skip this player on this iteration
+      return cb();
+    }
+    return redis.get('match_seq_num', (err, res) => {
+      if (err) {
+        console.error(err);
+      }
+      // Get matches with recent seqnums
+      const matches = body.result.matches.filter(m =>
+        m.match_seq_num > Number(res)
+      ).map(m =>
+        m.match_id
+      );
+      async.eachLimit(matches, 1, processMatch, cb);
+    });
+  });
+}
 
 function start(err) {
   if (err) {
@@ -24,73 +93,4 @@ function start(err) {
   });
 }
 
-function processPlayer(account_id, cb) {
-  const ajob = generateJob('api_history', {
-    account_id,
-  });
-  getData({
-    url: ajob.url,
-    delay,
-  }, (err, body) => {
-    if (err) {
-      console.error(err);
-    }
-    if (!body || !body.result || !body.result.matches) {
-      // Skip this player on this iteration
-      return cb();
-    }
-    redis.get('match_seq_num', (err, res) => {
-      if (err) {
-        console.error(err);
-      }
-      // Get matches with recent seqnums
-      const matches = body.result.matches.filter(m =>
-         m.match_seq_num > Number(res)
-      ).map(m =>
-         m.match_id
-      );
-      async.eachLimit(matches, 1, processMatch, cb);
-    });
-  });
-}
-
-function processMatch(match_id, cb) {
-  // Check if exists
-  redis.get(`scanner_insert:${match_id}`, (err, res) => {
-    if (err) {
-      return cb(err);
-    }
-    if (res) {
-      return cb();
-    } else {
-      const job = generateJob('api_details', {
-        match_id,
-      });
-      const url = job.url;
-      getData({
-        url,
-        delay,
-      }, (err, body) => {
-        if (err) {
-          throw err;
-        }
-        if (!body.result) {
-          return cb();
-        } else {
-          const match = body.result;
-          insertMatch(match, {
-            type: 'api',
-            origin: 'scanner',
-            skipCounts: false,
-            skipAbilityUpgrades: false,
-          }, (err) => {
-            if (!err) {
-              redis.set(`scanner_insert:${match.match_id}`, 1);
-            }
-            cb(err);
-          });
-        }
-      });
-    }
-  });
-}
+start();
