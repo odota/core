1eba5cfa8229842ca04e9ebe9e868c80a92dc88e
Ubuntu <howardc93@gmail.com>
2016-11-15 07:29:43 +0000

    extract monitor functions

--- svc/monitor.js
+++ svc/monitor.js
@@ -32,81 +32,90 @@ function invokeInterval(func) {
   }());
 }
 
+function steamApi(cb) {
+  request(`${'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key='}${apiKey}`, (err, resp, body) => {
+    if (err || resp.statusCode !== 200) {
+      return cb('bad http response');
+    }
+    try {
+      const fail = err || resp.statusCode !== 200 || JSON.parse(body).result.status !== 1;
+      return cb(fail, {
+        metric: Number(fail),
+        threshold: 1,
+      });
+    } catch (e) {
+      return cb('malformed http response');
+    }
+  });
+}
+
+function seqNumDelay(cb) {
+  utility.getData(utility.generateJob('api_history', {}).url, (err, body) => {
+    if (err) {
+      return cb('failed to get current sequence number');
+    }
+    // get match_seq_num, compare with real seqnum
+    const currSeqNum = body.result.matches[0].match_seq_num;
+    return redis.get('match_seq_num', (err, num) => {
+      if (err) {
+        return cb(err);
+      }
+      num = Number(num);
+      const metric = currSeqNum - num;
+      return cb(err, {
+        metric,
+        threshold: 10000,
+      });
+    });
+  });
+}
+
+function postgresUsage(cb) {
+  db.raw('select pg_database_size(\'yasp\')').asCallback((err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    return cb(err, {
+      metric: result.rows[0].pg_database_size,
+      threshold: 2 * Math.pow(10, 11),
+    });
+  });
+}
+
+function cassandraUsage(cb) {
+  cassandra.execute('select mean_partition_size, partitions_count from system.size_estimates where keyspace_name = \'yasp\'', (err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    let size = 0;
+    result.rows.forEach((r) => {
+      size += r.mean_partition_size * r.partitions_count * 0.5 * 4;
+    });
+    return cb(err, {
+      metric: size,
+      threshold: 8 * Math.pow(10, 12),
+    });
+  });
+}
+
+function redisUsage(cb) {
+  redis.info((err) => {
+    if (err) {
+      return cb(err);
+    }
+    // console.log(info);
+    return cb(err, {
+      metric: redis.server_info.used_memory,
+      threshold: 13 * Math.pow(10, 9),
+    });
+  });
+}
 const health = {
-  steam_api: function steamApi(cb) {
-    request(`${'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key='}${apiKey}`, (err, resp, body) => {
-      if (err || resp.statusCode !== 200) {
-        return cb('bad http response');
-      }
-      try {
-        const fail = err || resp.statusCode !== 200 || JSON.parse(body).result.status !== 1;
-        return cb(fail, {
-          metric: Number(fail),
-          threshold: 1,
-        });
-      } catch (e) {
-        return cb('malformed http response');
-      }
-    });
-  },
-  seq_num_delay: function seqNumDelay(cb) {
-    utility.getData(utility.generateJob('api_history', {}).url, (err, body) => {
-      if (err) {
-        return cb('failed to get current sequence number');
-      }
-      // get match_seq_num, compare with real seqnum
-      const currSeqNum = body.result.matches[0].match_seq_num;
-      return redis.get('match_seq_num', (err, num) => {
-        if (err) {
-          return cb(err);
-        }
-        num = Number(num);
-        const metric = currSeqNum - num;
-        return cb(err, {
-          metric,
-          threshold: 10000,
-        });
-      });
-    });
-  },
-  redis_usage: function redisUsage(cb) {
-    redis.info((err) => {
-      if (err) {
-        return cb(err);
-      }
-      // console.log(info);
-      return cb(err, {
-        metric: redis.server_info.used_memory,
-        threshold: 13 * Math.pow(10, 9),
-      });
-    });
-  },
-  postgres_usage: function postgresUsage(cb) {
-    db.raw('select pg_database_size(\'yasp\')').asCallback((err, result) => {
-      if (err) {
-        return cb(err);
-      }
-      return cb(err, {
-        metric: result.rows[0].pg_database_size,
-        threshold: 2 * Math.pow(10, 11),
-      });
-    });
-  },
-  cassandra_usage: function cassandraUsage(cb) {
-    cassandra.execute('select mean_partition_size, partitions_count from system.size_estimates where keyspace_name = \'yasp\'', (err, result) => {
-      if (err) {
-        return cb(err);
-      }
-      let size = 0;
-      result.rows.forEach((r) => {
-        size += r.mean_partition_size * r.partitions_count * 0.5 * 4;
-      });
-      return cb(err, {
-        metric: size,
-        threshold: 8 * Math.pow(10, 12),
-      });
-    });
-  },
+  steamApi,
+  seqNumDelay,
+  postgresUsage,
+  cassandraUsage,
+  redisUsage,
 };
 Object.keys(health).forEach((key) => {
   invokeInterval(health[key]);
