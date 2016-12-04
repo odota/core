eaf62572f137e4ac634d8cb74d2f057d0bb758f5
Howard Chung <howardc93@gmail.com>
2016-12-04 03:15:05 +0000

    add user-agent string

--- svc/parser.js
+++ svc/parser.js
@@ -175,6 +175,9 @@ function runParse(match, job, cb) {
   const download = request({
     url,
     encoding: null,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    },
   });
   const timeout = setTimeout(() => {
     download.abort();
