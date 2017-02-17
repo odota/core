cd8e1dce0a04797b1aa6f8777230c73c95a707a3
Howard Chung <howardc93@gmail.com>
2017-02-16 20:08:21 -0800

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,7 @@
 ALTER USER postgres PASSWORD 'postgres';
 CREATE ROLE readonly PASSWORD 'readonly';
-REVOKE CREATE ON SCHEMA public FROM readonly;
+REVOKE CREATE ON SCHEMA public FROM public;
+GRANT CREATE ON SCHEMA public TO postgres;
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM readonly;
 GRANT SELECT ON matches TO readonly;
 GRANT SELECT ON player_matches TO readonly;
