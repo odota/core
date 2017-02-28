8c68395d193fb0d7215610fd6b5abf37a5dae454
Howard Chung <howardc93@gmail.com>
2017-02-28 06:55:03 +0000

    increase matchrequestdelay

--- svc/retriever.js
+++ svc/retriever.js
@@ -102,7 +102,7 @@ function getGcMatchData(idx, matchId, cb) {
   }
   const timeout = setTimeout(() => {
     timeouts += 1;
-    matchRequestDelay += 100;
+    matchRequestDelay += 200;
   }, timeoutMs);
   return Dota2.requestMatchDetails(Number(matchId), (err, matchData) => {
     matchSuccesses += 1;
