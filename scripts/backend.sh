77984287f07ffcd7408a999f6125bab6b9b85ebc
Howard Chung <howardc93@gmail.com>
2017-06-11 00:11:00 +0000

    set max log size on backend node

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -sSL https://get.docker.com/ | sh
-sudo docker run -d --name backend --restart=always --net=host -e PROVIDER=gce -e GROUP=backend odota/core:latest sh -c "npm start"
+sudo docker run -d --name backend --restart=always --net=host --log-opt max-size=1g -e PROVIDER=gce -e GROUP=backend odota/core:latest sh -c "npm start"
 sudo docker start backend
