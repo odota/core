c0bd135924a647735648a0ede13d09ecffac2635
Howard Chung <howardc93@gmail.com>
2017-08-19 17:37:49 +0000

    optimize mmr queries

--- sql/country_mmr.sql
+++ sql/country_mmr.sql
@@ -1,13 +1,8 @@
 SELECT loccountrycode, 
        count(*), 
-       round(avg(solo_competitive_rank)) as avg
-FROM   players 
-       JOIN player_ratings 
-         ON player_ratings.account_id = players.account_id
-WHERE  loccountrycode IS NOT NULL 
-       AND solo_competitive_rank IS NOT NULL
-       AND time = (SELECT Max(time) 
-                   FROM   player_ratings pr 
-                   WHERE  players.account_id = pr.account_id)
+       round(avg(solo_competitive_rank.rating)) as avg
+FROM   players
+JOIN solo_competitive_rank using(account_id)
+WHERE  loccountrycode IS NOT NULL
 GROUP  BY loccountrycode 
 ORDER  BY avg DESC; 
--- sql/mmr.sql
+++ sql/mmr.sql
@@ -1,13 +1,4 @@
---number of players in bins of 100
-SELECT solo_competitive_rank / 100 AS bin, solo_competitive_rank / 100 * 100 as bin_name, count(*)
-FROM player_ratings pr
-INNER JOIN
-    (SELECT account_id, MAX(time) AS MaxTime
-    FROM player_ratings
-    GROUP BY account_id) grouped
-ON pr.account_id = grouped.account_id
-AND pr.time = grouped.MaxTime
-WHERE solo_competitive_rank IS NOT NULL
-AND solo_competitive_rank > 0
+SELECT solo_competitive_rank.rating / 100 AS bin, solo_competitive_rank.rating / 100 * 100 as bin_name, count(*)
+FROM solo_competitive_rank
 GROUP BY bin
 ORDER BY bin;
