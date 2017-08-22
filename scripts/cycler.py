4c4b1b3c7d45f26c6ae9ef99a1b0cb0be7ba09f8
Howard Chung <howardc93@gmail.com>
2017-08-22 19:44:45 +0000

    increase pool size

--- scripts/cycler.py
+++ scripts/cycler.py
@@ -6,7 +6,7 @@ import time
 
 # subprocess.call("sudo gcloud components update --quiet", shell=True)
 # For completeness this should also create the backend, HTTP load balancer, template, and network
-targetsize = 20
+targetsize = 40
 backendname = "retriever"
 templatename = "retriever-3"
 
