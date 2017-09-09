5f74841101c685524a9c5aef9651e7a930ea28bd
Howard Chung <howardc93@gmail.com>
2017-09-09 19:45:30 +0000

    adjust backup ratio

--- util/getGcData.js
+++ util/getGcData.js
@@ -18,9 +18,9 @@ const insertMatch = queries.insertMatch;
 function getGcDataFromRetriever(match, cb) {
   // make array of retriever urls and use a random one on each retry
   const urls = retrieverArr.map(r => `http://${r}?key=${secret}&match_id=${match.match_id}`);
-  const retrieverCount = retrieverArr.length * 1;
+  const backupCount = retrieverArr.length * 9;
   if (config.NODE_ENV !== 'test' && match.allowBackup) {
-    for (let i = 0; i < retrieverCount; i += 1) {
+    for (let i = 0; i < backupCount; i += 1) {
       urls.push(`https://api.stratz.com/api/v1/match?matchId=${match.match_id}`);
     }
   }
