4f565ea049e64b520c2aeb11060cf7eda2eea200
Argn0 <Argn0@users.noreply.github.com>
2017-12-30 05:41:14 +0100

    3

--- svc/scenarios.js
+++ svc/scenarios.js
@@ -1,6 +1,8 @@
 const queue = require('../store/queue');
 const buildMatch = require('../store/buildMatch');
 const db = require('../store/db');
+const async = require('async');
+const utility = require('../util/utility');
 
 function processScenarios(matchID, cb) {
   buildMatch(matchID, (err, match) => {
@@ -8,40 +10,95 @@ function processScenarios(matchID, cb) {
       cb(err)
     }
     console.log(match.match_id);
+    const rows = scenarioChecks.itemTimings()
 
-    if (scenarioChecks[0](match).condition) {
-      const name = scenarioChecks[0](match).name
-      const incrementWins = scenarioChecks[0](match).won ? 1 : 0
+    async.eachSeries(rows, (row, cb) => { 
+      const values = Object.keys(row.columns).map(() =>
+      '?');
+      const query = util.format(
+        `INSERT INTO %s (%s) VALUES (%s) ON CONFLICT (scenario) DO UPDATE SET wins = team_scenarios.wins + ${row.won ? 1 : 0}, games = team_scenarios.games + 1`,
+        row.table,
+        Object.keys(row.columns).join(','),
+        values,
+      );
 
-    db.raw(`INSERT INTO team_scenarios VALUES (?, ?, ?) ON CONFLICT(scenario) DO UPDATE SET wins = team_scenarios.wins + ${incrementWins}, games = team_scenarios.games + 1`, [name, 1, 1]).asCallback(cb);
-    }
-    console.log("test");
+      db.raw(query, Object.keys(row.columns).map(key =>
+        row.columns[key])).asCallback(cb);
+    });
+/*
+    async.eachSeries(teamScenarioChecks, (func, cb) => { 
+      scenario = func(match)
+      if (scenario.condition) {
+        const {name, won} = scenario
+      db.raw(`INSERT INTO team_scenarios VALUES (?, ?, ?) ON CONFLICT(scenario) DO UPDATE SET wins = team_scenarios.wins + ${won ? 1 : 0}, games = team_scenarios.games + 1`, [name, 0, 0]).asCallback(cb);
+    }});*/
+    cb();
+   
   });
 }
 
-var scenarioChecks = [
+const scenarioChecks = [
+  function itemTimings(match){
+    const hit = [{hero: 1,item: 'bfury' ,time: 1004}]
+
+    const rows = []
+
+    hit.forEach(function (c) {
+      const player = match.players.find(h=> h.hero_id === c.hero)
+      if (player) {
+        const item = player.purchase_log.find(i => i.key === c.item)
+        if (item && item.time < c.time) {
+          const won = (player.player_slot < 5 && match.radiant_win) ||  (player.player_slot > 4 && !match.radiant_win)
+          rows.push({
+              columns:
+                {scenario: c.stringify(),
+                hero: c.hero_id,
+                item: c.item,
+                time: c.time,},
+              won,
+              table: 'scenarios'
+            })          
+        }
+      }    
+    })
+    return rows
+  }
+]
+
+/*
+const teamScenarioChecks = [
   function firstBlood(match) {
     let team;
     let won;
-    console.log(typeof match)
     const condition = match.objectives.find(function(x){return x.type === 'CHAT_MESSAGE_FIRSTBLOOD'});
     if (condition) {
       team = condition.player_slot < 5 ? true : false;
     }
-    console.log(team)
     won = match.radiant_win === team;
-    console.log(won)
     return {
       name: "First Blood",
       condition,
       won,
     }
   },
-
-];
+  function courierKill(match) {
+    let team;
+    let won;
+    const condition = match.objectives.find(function(x){return x.type === 'CHAT_MESSAGE_COURIER_LOST' && x.time < 180000});
+    if (condition) {
+      team = condition.team === 3 ? true : false;
+    }
+    won = match.radiant_win === team;
+    return {
+      name: "Courier Kill",
+      condition,
+      won,
+    }
+  },
+];*/
 
 /*
-function scenarioChecks()  {
+function teamScenarioChecks()  {
 
   return
   [
