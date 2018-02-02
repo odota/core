871313bf48e1885c5fe547152e6b09c5b59da524
Mitchell Dzurisin <madlab96@gmail.com>
2018-02-01 22:21:43 -0600

    Improved majority of object field descriptions. (#1600)
    * Improved majority of object field descriptions.
    
    * Fixed multiple spelling errors and improved descriptions for remaining fields.

--- routes/objects.js
+++ routes/objects.js
@@ -2,7 +2,7 @@ const teamObject = {
   type: 'object',
   properties: {
     team_id: {
-      description: 'team_id',
+      description: 'Team\'s identifier',
       type: 'integer',
     },
     rating: {
@@ -22,11 +22,11 @@ const teamObject = {
       type: 'integer',
     },
     name: {
-      description: 'name',
+      description: 'Team name, eg. \'Newbee\'',
       type: 'string',
     },
     tag: {
-      description: 'The team tag',
+      description: 'The team tag/abbreviation',
       type: 'string',
     },
   },
@@ -35,59 +35,59 @@ const matchObject = {
   type: 'object',
   properties: {
     match_id: {
-      description: 'match_id',
+      description: 'Used to identify individual matches, e.g. 3703866531',
       type: 'integer',
     },
     duration: {
-      description: 'duration',
+      description: 'Length of the match',
       type: 'integer',
     },
     start_time: {
-      description: 'start_time',
+      description: 'Unix timestamp of when the match began',
       type: 'integer',
     },
     radiant_team_id: {
-      description: 'radiant_team_id',
+      description: 'The Radiant\'s team_id',
       type: 'integer',
     },
     radiant_name: {
-      description: 'radiant_name',
+      description: 'The Radiant\'s team name',
       type: 'string',
     },
     dire_team_id: {
-      description: 'dire_team_id',
+      description: 'The Dire\'s team_id',
       type: 'integer',
     },
     dire_name: {
-      description: 'dire_name',
+      description: 'The Dire\'s team name',
       type: 'string',
     },
     leagueid: {
-      description: 'leagueid',
+      description: 'Identifier for the league the match took place in',
       type: 'integer',
     },
     league_name: {
-      description: 'league_name',
+      description: 'Name of league the match took place in',
       type: 'string',
     },
     series_id: {
-      description: 'series_id',
+      description: 'Identifier for the series of the match',
       type: 'integer',
     },
     series_type: {
-      description: 'series_type',
+      description: 'Type of series the match was',
       type: 'integer',
     },
     radiant_score: {
-      description: 'radiant_score',
+      description: 'Number of kills the Radiant team had when the match ended',
       type: 'integer',
     },
     dire_score: {
-      description: 'dire_score',
+      description: 'Number of kills the Dire team had when the match ended',
       type: 'integer',
     },
     radiant_win: {
-      description: 'radiant_win',
+      description: 'Whether or not the Radiant won the match',
       type: 'boolean',
     },
     radiant: {
@@ -100,29 +100,29 @@ const heroObject = {
   type: 'object',
   properties: {
     id: {
-      description: 'id',
+      description: 'Numeric identifier for the hero object',
       type: 'integer',
     },
     name: {
-      description: 'name',
+      description: 'Dota hero command name, e.g. \'npc_dota_hero_antimage\'',
       type: 'string',
     },
     localized_name: {
-      description: 'localized_name',
+      description: 'Hero name, e.g. \'Anti-Mage\'',
       type: 'string',
     },
     primary_attr: {
-      description: 'primary_attr',
+      description: 'Hero primary shorthand attribute name, e.g. \'agi\'',
       type: 'string',
     },
     attack_type: {
-      description: 'attack_type',
+      description: 'Hero attack type, either \'Melee\' or \'Ranged\'',
       type: 'string',
     },
     roles: {
       type: 'array',
       items: {
-        description: 'role',
+        description: 'A hero\'s role in the game',
         type: 'string',
       },
     },
@@ -134,87 +134,87 @@ const playerObject = {
     type: 'object',
     properties: {
       account_id: {
-        description: 'account_id',
+        description: 'Player\'s account identifier',
         type: 'integer',
       },
       steamid: {
-        description: 'steamid',
+        description: 'Player\'s steam identifier',
         type: 'string',
       },
       avatar: {
-        description: 'avatar',
+        description: 'Steam picture URL (small picture)',
         type: 'string',
       },
       avatarmedium: {
-        description: 'avatarmedium',
+        description: 'Steam picture URL (medium picture)',
         type: 'string',
       },
       avatarfull: {
-        description: 'avatarfull',
+        description: 'Steam picture URL (full picture)',
         type: 'string',
       },
       profileurl: {
-        description: 'profileurl',
+        description: 'Steam profile URL',
         type: 'string',
       },
       personaname: {
-        description: 'personaname',
+        description: 'Player\'s Steam name',
         type: 'string',
       },
       last_login: {
-        description: 'last_login',
+        description: 'Date and time of last login to OpenDota',
         type: 'dateTime',
       },
       full_history_time: {
-        description: 'full_history_time',
+        description: 'Date and time of last request to refresh player\'s match history',
         type: 'dateTime',
       },
       cheese: {
-        description: 'cheese',
+        description: 'Amount of dollars the player has donated to OpenDota',
         type: 'integer',
       },
       fh_unavailable: {
-        description: 'fh_unavailable',
+        description: 'Whether the refresh of player\' match history failed',
         type: 'boolean',
       },
       loccountrycode: {
-        description: 'loccountrycode',
+        description: 'Player\'s country identifier, e.g. US',
         type: 'string',
       },
       name: {
-        description: 'name',
+        description: 'Verified player name, e.g. \'Miracle-\'',
         type: 'string',
       },
       country_code: {
-        description: 'country_code',
+        description: 'Player\'s country code',
         type: 'string',
       },
       fantasy_role: {
-        description: 'fantasy_role',
+        description: 'Player\'s ingame role (core: 1 or support: 2)',
         type: 'integer',
       },
       team_id: {
-        description: 'team_id',
+        description: 'Player\'s team identifier',
         type: 'integer',
       },
       team_name: {
-        description: 'team_name',
+        description: 'Player\'s team name, e.g. \'Evil Geniuses\'',
         type: 'string',
       },
       team_tag: {
-        description: 'team_tag',
+        description: 'Player\'s team shorthand tag, e.g. \'EG\'',
         type: 'string',
       },
       is_locked: {
-        description: 'is_locked',
+        description: 'Whether the roster lock is active',
         type: 'boolean',
       },
       is_pro: {
-        description: 'is_pro',
+        description: 'Whether the player is professional or not',
         type: 'boolean',
       },
       locked_until: {
-        description: 'locked_until',
+        description: 'When the roster lock will end',
         type: 'integer',
       },
     },
