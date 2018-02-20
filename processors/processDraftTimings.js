08d29bddb13591c587cf3e75d769c8be21fa22b3
bobsaget4lyfe <justindixon91@gmail.com>
2018-02-20 21:30:38 +1100

    Active Team Now fixed.

--- processors/processDraftTimings.js
+++ processors/processDraftTimings.js
@@ -11,18 +11,17 @@
  * time: removed for total time taken
  * extra_time: how much of the extra time is left at the end of the draft stage
  * total_time_taken: the time taken for the draft stage
- * extra_time_taken: the amount of extra time used in the draft stage - Now done client side
  */
 
 function processDraftTimings(entries, meta) {
   const draftTimings = [];
   const heroIdToSlot = meta.hero_id_to_slot;
   let sumActiveTeam = 0;
+  let previousActiveTeam = 0;
   for (let i = 0; i < entries.length; i += 1) {
     const e = entries[i];
     const heroId = e.hero_id;
-    // Ignore All Pick matches
-    if (e.type === 'draft_timings' && e.draft_active_team !== 0) {
+    if (e.type === 'draft_timings') {
       // The active team needs to be downshifted by 1, so ignore the final observation.
       if (i < (entries.length - 1)) {
         sumActiveTeam += e.draft_active_team;
@@ -30,33 +29,37 @@ function processDraftTimings(entries, meta) {
       const currpickban = {
         order: e.draft_order,
         pick: e.pick,
-        active_team: i > 0 ? (entries[i-1].draft_active_team) : null,
+        active_team: previousActiveTeam,
         hero_id: e.hero_id,
         player_slot: e.pick === true ? heroIdToSlot[heroId] : null,
         time: e.time,
         extra_time: e.draft_active_team === 2 ? e.draft_extime0 : e.draft_extime1,
         total_time_taken: 0,
-        /* extra_time_taken: 0, */
       };
       draftTimings.push(JSON.parse(JSON.stringify(currpickban)));
+      previousActiveTeam = e.draft_active_team;
     }
   }
-  // update the team that had the first pick/ban
-  draftTimings[0].active_team = ((sumActiveTeam % 2) + 2);
-  for (let j = 0; j < draftTimings.length; j += 1) {
-    if (draftTimings[j].order === 1) {
-      draftTimings[j].total_time_taken = (meta.game_zero + draftTimings[j].time);
-    } else {
-      let index2;
-      // find the time of the end of the previous order
-      for (let i = 0; i < draftTimings.length; i += 1) {
-        const currpick = draftTimings[i];
-        if (currpick.order === (draftTimings[j].order - 1)) {
-          index2 = i;
+  // ignore Source 1 games
+  console.log(draftTimings);
+  if (typeof draftTimings[0] !== 'undefined') {
+    // update the team that had the first pick/ban
+    draftTimings[0].active_team = ((sumActiveTeam % 2) + 2);
+    for (let j = 0; j < draftTimings.length; j += 1) {
+      if (draftTimings[j].order === 1) {
+        draftTimings[j].total_time_taken = (meta.game_zero + draftTimings[j].time);
+      } else {
+        let index2;
+        // find the time of the end of the previous order
+        for (let i = 0; i < draftTimings.length; i += 1) {
+          const currpick = draftTimings[i];
+          if (currpick.order === (draftTimings[j].order - 1)) {
+            index2 = i;
+          }
         }
+        // calculate the timings
+        draftTimings[j].total_time_taken = (draftTimings[j].time - draftTimings[index2].time);
       }
-      // calculate the timings
-      draftTimings[j].total_time_taken = (draftTimings[j].time - draftTimings[index2].time);
     }
   }
   // remove the time, no need for it
