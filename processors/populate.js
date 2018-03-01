ac0f8d76b7c3a3e8d483d052f1ffa6e90ebd5c34
Argn0 <Argn0@users.noreply.github.com>
2018-03-01 17:25:43 +0100

    review 1

--- processors/populate.js
+++ processors/populate.js
@@ -85,17 +85,16 @@ function populate(e, container) {
           t[ability][target] = 1;
         }
       } else if (e.type === 'damage_targets') {
-        const ability = e.key[0] === null ? 'Autoattack' : e.key[0];
+        const ability = e.key[0];
         const target = e.key[1];
         const damage = e.value;
-        if (t[ability] && t[ability][target]) {
-          t[ability][target] += damage;
-        } else if (t[ability]) {
-          t[ability][target] = damage;
-        } else {
+        if (!t[ability]) {
           t[ability] = {};
-          t[ability][target] = damage;
         }
+        if (!t[ability][target]) {
+          t[ability][target] = 0;
+        }
+        t[ability][target] += damage;
       } else if (typeof t === 'object') {
       // add it to hash of counts
         e.value = e.value || 1;
