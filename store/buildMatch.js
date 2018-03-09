c95852e75d83c1f4c16a680becd0822c65a06260
Howard Chung <howardzchung@gmail.com>
2018-03-09 05:32:04 -0800

    Update buildMatch.js

--- store/buildMatch.js
+++ store/buildMatch.js
@@ -171,7 +171,7 @@ function buildMatch(matchId, cb) {
         return cb();
       }
       if (match.version && config.ENABLE_MATCH_CACHE) {
-        return redis.setex(key, 300, JSON.stringify(match), (err) => {
+        return redis.setex(key, 120, JSON.stringify(match), (err) => {
           if (err) {
             console.error(err);
           }
