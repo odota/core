8419edc32405bfd4dee6adb78b4e6d8ab4ec21f6
Argn0 <Argn0@users.noreply.github.com>
2018-04-10 13:27:31 +0200

    error fix and refactoring

--- util/scenariosUtil.js
+++ util/scenariosUtil.js
@@ -25,7 +25,7 @@ function buildTeamScenario(scenario, isRadiant, match) {
     scenario,
     is_radiant: isRadiant,
     region: match.region,
-    wins: match.radiant_win === isRadiant ? '1' : '0',
+    wins: match.radiant_win === isRadiant,
   }];
 }
 
@@ -35,16 +35,18 @@ const scenarioChecks = {
     function itemTimings(match) {
       const rows = [];
       match.players.forEach((player) => {
-        player.purchase_log.forEach((item) => {
-          if (dotaItems.indexOf(item.key) !== -1 && item.time <= timings[timings.length - 1]) {
-            rows.push({
-              hero_id: player.hero_id,
-              item: item.key,
-              time: timings.find(x => x >= item.time),
-              wins: playerWon(player, match) ? '1' : '0',
-            });
-          }
-        });
+        if (player.purchase_log) {
+          player.purchase_log.forEach((item) => {
+            if (dotaItems.indexOf(item.key) !== -1 && item.time <= timings[timings.length - 1]) {
+              rows.push({
+                hero_id: player.hero_id,
+                item: item.key,
+                time: timings.find(x => x >= item.time),
+                wins: playerWon(player, match),
+              });
+            }
+          });
+        }
       });
       return rows;
     },
@@ -57,7 +59,7 @@ const scenarioChecks = {
             hero_id: player.hero_id,
             lane_role: player.lane_role,
             time: gameDurationBucket.find(x => x >= match.duration),
-            wins: playerWon(player, match) ? '1' : '0',
+            wins: playerWon(player, match),
           });
         }
       });
