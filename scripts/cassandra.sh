ea5d89a557915d5b6627d96155f450a92e91f602
Howard Chung <howardzchung@gmail.com>
2018-04-12 09:09:15 +0000

    update cassandra script

--- scripts/cassandra.sh
+++ scripts/cassandra.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sSL https://get.docker.com/ | sh
-
+#curl -sSL https://get.docker.com/ | sh
 #sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-persistent-disk-1
+
 sudo mkdir -p /var/lib/cassandra
 sudo mount -o discard,defaults /dev/disk/by-id/google-persistent-disk-1 /var/lib/cassandra
 
