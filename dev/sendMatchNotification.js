d8a069306863fa755a6dafebc839e27145d88a52
Albert Cui <aqc2109@columbia.edu>
2018-06-04 04:23:56 +0000

    fix lint

--- dev/sendMatchNotification.js
+++ dev/sendMatchNotification.js
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 const rewire = require('rewire');
+
 const parser = rewire('../svc/parser.js');
 
 const sendNotifications = parser.__get__('sendNotifications');
 
 // Sends notification to all the account_ids in the pgroup object. Replace account_id with the account you're testing against.
-let job = JSON.parse('{"match_id":3402873969,"radiant_win":true,"start_time":1503712159,"duration":2887,"pgroup":{"0":{"account_id":102344608,"hero_id":1,"player_slot":0},"1":{"account_id":null,"hero_id":40,"player_slot":1}},"ability_upgrades":[]}');
+const job = JSON.parse('{"match_id":3402873969,"radiant_win":true,"start_time":1503712159,"duration":2887,"pgroup":{"0":{"account_id":102344608,"hero_id":1,"player_slot":0},"1":{"account_id":null,"hero_id":40,"player_slot":1}},"ability_upgrades":[]}');
 
 sendNotifications(job);
--- util/notifications.js
+++ util/notifications.js
@@ -6,44 +6,42 @@ firebase.initializeApp({
   credential: firebase.credential.cert({
     projectId: config.GCP_PROJECT_ID,
     clientEmail: config.GCP_CLIENT_EMAIL,
-    privateKey: config.GCP_PRIVATE_KEY
+    privateKey: config.GCP_PRIVATE_KEY,
   }),
-  projectId: config.GCP_PROJECT_ID
+  projectId: config.GCP_PROJECT_ID,
 });
 
 
 function sendNotificationViaToken(token, data, cb) {
   firebase.messaging().send({
-    data: data,
-    token: token
+    data,
+    token,
   })
-  .then((response) => {
+    .then((response) => {
     // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-    cb(true);
-  })
-  .catch((err) => {
-    console.log('Error sending message:', err);
-    cb(err)
-  });
+      console.log('Successfully sent message:', response);
+      cb(true);
+    })
+    .catch((err) => {
+      console.log('Error sending message:', err);
+      cb(err);
+    });
 }
 
 function sendNotificationViaAccountId(accountId, data, cb) {
   redis.hget('notification_users', accountId, (err, res) => {
     if (err) {
       cb(err);
+    } else if (res) {
+      console.log(res);
+      sendNotificationViaToken(res, data, cb);
     } else {
-      if (res) {
-        console.log(res);
-        sendNotificationViaToken(res, data, cb);
-      } else {
-        cb(false)
-      }
+      cb(false);
     }
-  })
+  });
 }
 
 module.exports = {
   sendNotificationViaToken,
-  sendNotificationViaAccountId
+  sendNotificationViaAccountId,
 };
