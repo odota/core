565b7c7b32d7b7e7c19e7b40ac4e529b998cfe39
Albert Cui <aqc2109@columbia.edu>
2018-06-07 02:50:42 +0000

    do not count for timeout

--- svc/web.js
+++ svc/web.js
@@ -182,7 +182,10 @@ app.use((req, res, cb) => {
     }
 
     // When called from a middleware, the mount point is not included in req.path. See Express docs.
-    if (res.statusCode !== 500 && res.statusCode !== 429 && !whitelistedPaths.includes(req.baseUrl + (req.path === '/' ? '' : req.path))) {
+    if (res.statusCode !== 500 &&
+        res.statusCode !== 429 &&
+        !whitelistedPaths.includes(req.baseUrl + (req.path === '/' ? '' : req.path)) &&
+        elapsed < 10000) {
       const multi = redis.multi();
       if (res.locals.isAPIRequest) {
         multi.hincrby('usage_count', res.locals.usageIdentifier, 1)
