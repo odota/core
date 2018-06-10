0170d37d9d513955a5fa6ff940280db641905e10
Howard Chung <howardzchung@gmail.com>
2018-06-09 17:29:56 -0700

    Update getGcData.js

--- util/getGcData.js
+++ util/getGcData.js
@@ -20,7 +20,7 @@ function getGcDataFromRetriever(match, cb) {
   if (config.NODE_ENV !== 'test' && match.allowBackup && (Math.random() * 100) < Number(config.BACKUP_RETRIEVER_PERCENT)) {
     urls = [`https://api.stratz.com/api/v1/match?matchId=${match.match_id}`];
   }
-  return getData({ url: urls, noRetry: match.noRetry }, (err, body, metadata) => {
+  return getData({ url: urls, noRetry: match.noRetry, timeout: 3000 }, (err, body, metadata) => {
     if (metadata && metadata.hostname === 'api.stratz.com') {
       // handle backup urls (don't save to DB since no party/buffs data)
       return cb(err, {
