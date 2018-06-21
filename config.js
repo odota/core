b9b81a644396a402b79b39ffcdd0d78fc77671c4
Argn0 <Argn0@users.noreply.github.com>
2018-06-21 09:05:17 +0200

    benchmark retention 1 hour

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ const defaults = {
   SCANNER_DELAY: 2000, // delay for scanner API requests (stricter rate limit)
   MMR_PARALLELISM: 10, // Number of simultaneous MMR requests to make (per retriever)
   PARSER_PARALLELISM: 1, // Number of simultaneous parse jobs to run (per parser)
-  BENCHMARK_RETENTION_MINUTES: 5, // minutes in block to retain benchmark data for percentile
+  BENCHMARK_RETENTION_MINUTES: 60, // minutes in block to retain benchmark data for percentile
   GCDATA_PERCENT: 0, // percent of inserted matches to randomly queue for GC data
   SCANNER_PERCENT: 100, // percent of matches to insert from scanner
   PUBLIC_SAMPLE_PERCENT: 10, // percent of public matches to sample in DB
