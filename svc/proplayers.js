abb3350992042a5ea1ba40c68150cfc95e14176a
Howard Chung <howardzchung@gmail.com>
2018-08-07 22:58:51 -0700

    Update proplayers.js

--- svc/proplayers.js
+++ svc/proplayers.js
@@ -12,9 +12,29 @@ function doProPlayers(cb) {
       return cb(err);
     }
     return async.each(body.player_infos, (p, cb) => {
+      // Corrections
       if ((p.account_id === 180012313 || p.account_id === 323792491)
         && p.locked_until < 1502694000) {
         p.locked_until = 1502694000;
+      }
+      if (p.account_id === 87382579 && p.team_id === 39) {
+        // Misery no longer on EG
+        p.team_id = 0;
+      }
+      if (p.account_id === 88271237) {
+        p.name = "7ckngMad";
+      }
+      if (p.account_id === 311360822 && p.team_id === 5216274) {
+        // Ana is now on OG
+        p.team_id = 2586976;
+      }
+      if (p.account_id === 94054712 && p.team_id === 5154470) {
+        // Topson is now on OG
+        p.team_id = 2586976;
+      }
+      if (p.account_id === 124936122 && p.team_id === 2643401) {
+        // Zyd now on Team Serenity
+        p.team_id = 5021898;
       }
       queries.upsert(db, 'notable_players', p, {
         account_id: p.account_id,
