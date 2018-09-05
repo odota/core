cefa1e2de8cffdb9d72eb14b736fa7247b6a766e
Howard Chung <howardzchung@gmail.com>
2018-09-05 00:32:10 -0700

    Update config.js

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ const defaults = {
   WEBSOCKET_PORT: 80, // port for live match subscription websocket service
   MAXIMUM_AGE_SCENARIOS_ROWS: 4, // maximum allowed age of scenarios rows in weeks
   MATCH_CACHE_SECONDS: 60, // number of seconds to cache matches
-  PLAYER_CACHE_SECONDS: 3600, // number of seconds to cache player aggregations
+  PLAYER_CACHE_SECONDS: 1800, // number of seconds to cache player aggregations
   SCANNER_PLAYER_PERCENT: 109, // percent of matches from scanner to insert player account IDs for (discover new player account IDs)
   ENABLE_RETRIEVER_ADVANCED_AUTH: '', // set to enable retriever two-factor and SteamGuard authentication,
   ENABLE_API_LIMIT: '', // if truthy, API calls after exceeding API_FREE_LIMIT are blocked
