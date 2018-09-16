f8609d01c10ed37da98fa7ffc39b4b92c662535d
Dustin Dawes <gu3st@dustindawes.com>
2018-09-15 23:26:04 -0700

    Created contributors file for OpenDota contributors to flag their profile if desired.  Added it to the API response for /players/:id.

---
+++ CONTRIBUTORS.js
@@ -0,0 +1,9 @@
+/*
+  This file is for contributors to OpenDota to add their DOTA friend codes (AKA SteamId3).
+
+  Currently this will cause the /players/:id endpoint to return is_contributor = true for your profile
+*/
+
+module.exports = {
+  9977887: {},
+};
