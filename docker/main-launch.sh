0309f55b5a2c42d755c9fc584b028ceee203598b
Howard Chung <howardc93@gmail.com>
2018-09-21 22:53:48 -0700

    Update main-launch.sh

--- docker/main-launch.sh
+++ docker/main-launch.sh
@@ -7,13 +7,7 @@
 # Rebuild to replace mapped directory build
 npm run build
 
-pm2 start manifest.json
-
-# Stop everything to prevent overload
-pm2 stop all
-
-# Start web server
-pm2 start web
+pm2 start manifest.json --only web
 
 # We shall now display logs indefinitely
 pm2 logs
