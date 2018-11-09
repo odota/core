9372a2e97676e6ae074eff87d2a6c991e1da21a3
Howard Chung <howardzchung@gmail.com>
2018-11-09 07:21:19 +0000

    update to redis 5

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,9 @@ set -e
 
 #sudo docker-compose up -d
 sudo service postgresql stop
-sudo docker run -d --privileged --name cassandra --net=host -e "CASSANDRA_LISTEN_ADDRESS=127.0.0.1" cassandra:3
+sudo docker run -d --privileged --name cassandra --net=host -e "CASSANDRA_LISTEN_ADDRESS=127.0.0.1" cassandra:3.5
 sudo docker run -d --privileged --name postgres --net=host postgres:9.5
-sudo docker run -d --privileged --name redis --net=host redis:4
+sudo docker run -d --privileged --name redis --net=host redis:5
 sudo docker run -d --privileged --name elasticsearch --net=host docker.elastic.co/elasticsearch/elasticsearch:6.2.4
 sudo docker run -d --name parser --net=host odota/parser
 sudo docker build -t "odota/core" . && sudo docker run --net=host -i odota/core sh -c 'npm run test'
