6a97beffb127b3c126d09ac148de6ada6dd734e0
builder_247 <olaakkonen@gmail.com>
2019-05-22 11:33:11 +0300

    Added a note about dotaconstants to API spec introduction (#1917)
    * People often ask how to interpret the data provided the API, so i thought it might be useful addition to the docs.

--- routes/spec.js
+++ routes/spec.js
@@ -71,6 +71,8 @@ const spec = {
     description: `# Introduction
 The OpenDota API provides Dota 2 related data including advanced match data extracted from match replays.
 
+You can find data that can be used to convert hero and ability IDs and other information provided by the API from the [dotaconstants](https://github.com/odota/dotaconstants) repository.
+
 **Beginning 2018-04-22, the OpenDota API is limited to 50,000 free calls per month and 60 requests/minute** We offer a Premium Tier with unlimited API calls and higher rate limits. Check out the [API page](https://www.opendota.com/api-keys) to learn more.
 `,
     version: packageJson.version,
