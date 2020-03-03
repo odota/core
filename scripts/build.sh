b0dd266cd6da6f0a0d9c4ac5a7b590c167ee25ad
Ubuntu <howardzchung@gmail.com>
2020-03-03 21:37:30 +0000

    adjust wait

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 # Exit on error
 set -e
 
-sudo docker-compose up -d
-# Give Casssandra some time to start up
-sleep 20 && sudo docker exec -it odota-core sh -c 'npm run test'
+# Give Casssandra/ES some time to start up
+sudo docker-compose up -d && sleep 30 && sudo docker exec -it odota-core sh -c 'npm run test'
+
