8e3b8c220bdc544f2ed991ab85293185f46d78d2
builder_247 <olaakkonen@gmail.com>
2020-05-06 15:09:26 +0300

    Document ability to use bearer tokens

--- routes/spec.js
+++ routes/spec.js
@@ -84,6 +84,8 @@ You can find data that can be used to convert hero and ability IDs and other inf
       description: `Use an API key to remove monthly call limits and to receive higher rate limits. [Learn more and get your API key](https://www.opendota.com/api-keys).
 
       Usage example: https://api.opendota.com/api/matches/271145478?api_key=YOUR-API-KEY
+      
+      API key can also be sent using the authorization header "Authorization: Bearer YOUR-API-KEY"
       `,
       in: 'query',
     },
