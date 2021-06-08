5645b8ff09ee88443a19ed76ffa74ec25cad6c71
Howard Chung <howardzchung@gmail.com>
2021-06-08 00:14:14 -0700

    Update postbuild.sh

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ -n "$DOCKER_USERNAME" ]; then
-  echo $DOCKER_PASSWORD | docker login --username "$DOCKER_USERNAME" --password-stdin
   docker tag odota/core:latest odota/core:${TRAVIS_COMMIT}
   docker push odota/core:${TRAVIS_COMMIT}
   docker push odota/core:latest
