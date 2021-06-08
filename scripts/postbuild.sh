14218812ce5edbeebff3af44309ccae1cd3fb3f4
Howard Chung <howardzchung@gmail.com>
2021-06-08 00:23:55 -0700

    Update postbuild.sh

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$DOCKER_USERNAME" ]; then
-  docker tag odota/core:latest odota/core:${TRAVIS_COMMIT}
-  docker push odota/core:${TRAVIS_COMMIT}
-  docker push odota/core:latest
+  sudo docker tag odota/core:latest odota/core:${TRAVIS_COMMIT}
+  sudo docker push odota/core:${TRAVIS_COMMIT}
+  sudo docker push odota/core:latest
 fi
