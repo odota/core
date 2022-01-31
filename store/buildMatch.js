843049bd0b36e424c136bfec4e78b654081f2f2e
Ubuntu <howardzchung@gmail.com>
2022-01-31 09:12:11 +0000

    repair json errors

--- store/buildMatch.js
+++ store/buildMatch.js
@@ -102,7 +102,7 @@ async function getMatch(matchId) {
     }
   } catch (e) {
     console.error(e);
-    if (e.message.startsWith('Server failure during read query') || e.message.startsWith('no players found')) {
+    if (e.message.startsWith('Server failure during read query') || e.message.startsWith('no players found') || e.message.startsWith('Unexpected end of JSON input')) {
       // Delete and request new 
       await cassandra.execute('DELETE FROM player_matches where match_id = ?', [Number(matchId)], { prepare: true });
       const match = {
