e51bd3ecf681c0fb8d0c17dd46f39229c8c43077
Ubuntu <howardzchung@gmail.com>
2022-02-08 10:45:13 +0000

    chmod

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 # Exit on error
 set -e
 
+sudo chmod 755 /var/log
+
 # Give Casssandra/ES some time to start up
 sudo docker-compose up -d && sleep 30 && sudo docker exec -it odota-core sh -c 'npm run test'
 
