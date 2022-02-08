b789dafbbe44999c3195910793ca49f6be4e0df2
Ubuntu <howardzchung@gmail.com>
2022-02-08 10:38:35 +0000

    update elasticsearch

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Exit on error
-set -e
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+set -es
 
 # Give Casssandra/ES some time to start up
 sudo docker-compose up -d && sleep 30 && sudo docker exec -it odota-core sh -c 'npm run test'
--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ -n "$DOCKER_USERNAME" ]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   sudo docker tag odota/core:latest odota/core:${TRAVIS_COMMIT}
   sudo docker push odota/core:${TRAVIS_COMMIT}
   sudo docker push odota/core:latest
