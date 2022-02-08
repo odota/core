6b6a6eaef6aa1234266a02cc404ffc6c94f63cb8
Ubuntu <howardzchung@gmail.com>
2022-02-08 14:49:52 +0000

    longer sleep

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 # Exit on error
 set -e
 
-docker-compose up -d && sleep 10 && docker exec -it odota-core sh -c 'npm run test'
+docker-compose up -d && sleep 20 && docker exec -it odota-core sh -c 'npm run test'
 
