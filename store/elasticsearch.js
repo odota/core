17c7d6327c60b22752efd3564b5b3be1f457b3f3
Ubuntu <howardzchung@gmail.com>
2022-02-08 15:07:55 +0000

    fix es config

--- store/elasticsearch.js
+++ store/elasticsearch.js
@@ -7,13 +7,8 @@ const config = require('../config');
 
 console.log('connecting %s', config.ELASTICSEARCH_URL);
 const es = new elasticsearch.Client({
-  host: config.ELASTICSEARCH_URL,
+  node: config.ELASTICSEARCH_URL,
   apiVersion: '6.8',
-  log: config.NODE_ENV === 'development' ? 'trace' : {
-    type: 'file',
-    level: 'trace',
-    path: '/var/log/elasticsearch.log',
-  },
 });
 
 const INDEX = config.NODE_ENV === 'test' ? 'dota-test' : 'dota';
