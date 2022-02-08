483bd1be28bd7cbdb12e4e1a24eef725daf42776
Ubuntu <howardzchung@gmail.com>
2022-02-08 11:56:32 +0000

    remove sudo

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,5 @@
 # Exit on error
 set -e
 
-# Give Casssandra/ES some time to start up
-sudo docker-compose up -d && sleep 30 && sudo docker exec -it odota-core sh -c 'npm run test'
+docker-compose up -d && docker exec -it odota-core sh -c 'npm run test'
 
--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$DOCKER_USERNAME" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  sudo docker tag odota/core:latest odota/core:${TRAVIS_COMMIT}
-  sudo docker push odota/core:${TRAVIS_COMMIT}
-  sudo docker push odota/core:latest
+  docker tag odota/core:latest odota/core:${TRAVIS_COMMIT}
+  docker push odota/core:${TRAVIS_COMMIT}
+  docker push odota/core:latest
 fi
