62345137abc7b060c211dd9c44a6d62bf6b129bf
Ubuntu <howardzchung@gmail.com>
2022-02-14 12:00:26 +0000

    start caching retriever responses again

--- util/getGcData.js
+++ util/getGcData.js
@@ -57,6 +57,8 @@ function handleGcData(match, body, cb) {
   });
 }
 
+redis.del('nonRetryable');
+
 function getGcDataFromRetriever(match, cb) {
   const retrieverArr = utility.getRetrieverArr(match.useGcDataArr);
   // make array of retriever urls and use a random one on each retry
@@ -64,8 +66,8 @@ function getGcDataFromRetriever(match, cb) {
   return getData({ url: urls, noRetry: match.noRetry, timeout: 5000 }, (err, body, metadata) => {
     if (err || !body || !body.match || !body.match.replay_salt || !body.match.players) {
       // non-retryable error
-      redis.lpush('nonRetryable', JSON.stringify({ matchId: match.match_id, body }));
-      redis.ltrim('nonRetryable', 0, 10000);
+      // redis.lpush('nonRetryable', JSON.stringify({ matchId: match.match_id, body }));
+      // redis.ltrim('nonRetryable', 0, 10000);
       return cb(new Error('invalid body or error'));
     }
     // Count retriever calls
@@ -73,7 +75,7 @@ function getGcDataFromRetriever(match, cb) {
     redis.zincrby('retrieverCounts', 1, metadata.hostname);
     redis.expireat('retrieverCounts', moment().startOf('hour').add(1, 'hour').format('X'));
 
-    // redis.setex(`gcdata:${match.match_id}`, 15 * 60, zlib.gzipSync(JSON.stringify(body)));
+    redis.setex(`gcdata:${match.match_id}`, 60 * 60, zlib.gzipSync(JSON.stringify(body)));
     // TODO add discovered account_ids to database and fetch account data/rank medal
     return handleGcData(match, body, cb);
   });
@@ -84,14 +86,14 @@ module.exports = function getGcData(match, cb) {
   if (!matchId || Number.isNaN(Number(matchId)) || Number(matchId) <= 0) {
     return cb(new Error('invalid match_id'));
   }
-  return getGcDataFromRetriever(match, cb);
-  // return redis.get(Buffer.from(`gcdata:${match.match_id}`), (err, body) => {
-  //   if (err) {
-  //     return cb(err);
-  //   }
-  //   if (body) {
-  //     return handleGcData(match, JSON.parse(zlib.gunzipSync(body)), cb);
-  //   }
-  //   return getGcDataFromRetriever(match, cb);
-  // });
+  //return getGcDataFromRetriever(match, cb);
+  return redis.get(Buffer.from(`gcdata:${match.match_id}`), (err, body) => {
+    if (err) {
+      return cb(err);
+    }
+    if (body) {
+      return handleGcData(match, JSON.parse(zlib.gunzipSync(body)), cb);
+    }
+    return getGcDataFromRetriever(match, cb);
+  });
 };
