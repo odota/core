3da2b21aed224f49bd7c08ff8504e4eaa70f7865
Howard Chung <howardzchung@gmail.com>
2022-08-06 03:33:50 -0700

    Update retriever.js

--- svc/retriever.js
+++ svc/retriever.js
@@ -25,10 +25,10 @@ const steamObj = {};
 const minUpTimeSeconds = 60;
 const timeoutMs = 1500;
 const accountsToUse = 9;
-const matchRequestLimit = 900;
+const matchRequestLimit = 920;
 const port = config.PORT || config.RETRIEVER_PORT;
-const matchRequestDelay = 700;
-const matchRequestDelayStep = 200;
+const matchRequestDelay = 500;
+const matchRequestDelayStep = 10;
 
 let matchRequestDelayIncr = 0;
 let lastRequestTime;
