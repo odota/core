00a9b7d5e495648f74abed50f55c389c861901d8
mdebeer <ff137@proton.me>
2023-04-28 01:02:06 +0200

    rankings result is an array, score is a number

--- routes/spec.js
+++ routes/spec.js
@@ -3278,82 +3278,85 @@ You can find data that can be used to convert hero and ability IDs and other inf
                 },
                 rankings: {
                   description: "rankings",
-                  type: "object",
-                  properties: {
-                    account_id: {
-                      description: "account_id",
-                      type: "integer",
-                    },
-                    score: {
-                      description: "score",
-                      type: "string",
-                    },
-                    steamid: {
-                      description: "steamid",
-                      type: "string",
-                      nullable: true,
-                    },
-                    avatar: {
-                      description: "avatar",
-                      type: "string",
-                      nullable: true,
-                    },
-                    avatarmedium: {
-                      description: "avatarmedium",
-                      type: "string",
-                      nullable: true,
-                    },
-                    avatarfull: {
-                      description: "avatarfull",
-                      type: "string",
-                      nullable: true,
-                    },
-                    profileurl: {
-                      description: "profileurl",
-                      type: "string",
-                      nullable: true,
-                    },
-                    personaname: {
-                      description: "personaname",
-                      type: "string",
-                      nullable: true,
-                    },
-                    last_login: {
-                      description: "last_login",
-                      type: "string",
-                      format: "date-time",
-                      nullable: true,
-                    },
-                    full_history_time: {
-                      description: "full_history_time",
-                      type: "string",
-                      format: "date-time",
-                    },
-                    cheese: {
-                      description: "cheese",
-                      type: "integer",
-                      nullable: true,
-                    },
-                    fh_unavailable: {
-                      description: "fh_unavailable",
-                      type: "boolean",
-                      nullable: true,
-                    },
-                    loccountrycode: {
-                      description: "loccountrycode",
-                      type: "string",
-                      nullable: true,
-                    },
-                    rank_tier: {
-                      description: "rank_tier",
-                      type: "integer",
-                      nullable: true,
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      account_id: {
+                        description: "account_id",
+                        type: "integer",
+                      },
+                      score: {
+                        description: "score",
+                        type: "number",
+                      },
+                      steamid: {
+                        description: "steamid",
+                        type: "string",
+                        nullable: true,
+                      },
+                      avatar: {
+                        description: "avatar",
+                        type: "string",
+                        nullable: true,
+                      },
+                      avatarmedium: {
+                        description: "avatarmedium",
+                        type: "string",
+                        nullable: true,
+                      },
+                      avatarfull: {
+                        description: "avatarfull",
+                        type: "string",
+                        nullable: true,
+                      },
+                      profileurl: {
+                        description: "profileurl",
+                        type: "string",
+                        nullable: true,
+                      },
+                      personaname: {
+                        description: "personaname",
+                        type: "string",
+                        nullable: true,
+                      },
+                      last_login: {
+                        description: "last_login",
+                        type: "string",
+                        format: "date-time",
+                        nullable: true,
+                      },
+                      full_history_time: {
+                        description: "full_history_time",
+                        type: "string",
+                        format: "date-time",
+                      },
+                      cheese: {
+                        description: "cheese",
+                        type: "integer",
+                        nullable: true,
+                      },
+                      fh_unavailable: {
+                        description: "fh_unavailable",
+                        type: "boolean",
+                        nullable: true,
+                      },
+                      loccountrycode: {
+                        description: "loccountrycode",
+                        type: "string",
+                        nullable: true,
+                      },
+                      rank_tier: {
+                        description: "rank_tier",
+                        type: "integer",
+                        nullable: true,
+                      },
                     },
                   },
                 },
               },
             },
-          },
+          }
         },
         route: () => "/rankings",
         func: (req, res, cb) => {
