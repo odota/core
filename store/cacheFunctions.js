05af5272758e91830bd167c13616c0dfb8d7ae00
ff137 <ff137@proton.me>
2023-05-06 19:42:58 +0200

    move sendDataWithCache function from spec.js

--- store/cacheFunctions.js
+++ store/cacheFunctions.js
@@ -1,6 +1,15 @@
 const redis = require("./redis");
 const config = require("../config");
 
+const write = (req, data, cb) => {
+  // console.log(`[WRITECACHE] cache:${req.key}:${req.account_id}`);
+  redis.setex(
+    `cache:${req.key}:${req.account_id}`,
+    config.PLAYER_CACHE_SECONDS,
+    data,
+    cb
+  );
+};
 const getKeys = () => ["wl", "heroes", "peers", "counts"];
 
 module.exports = {
@@ -11,14 +20,23 @@ module.exports = {
   update: (req, cb) => {
     redis.del(`cache:${req.key}:${req.account_id}`, cb);
   },
-  write: (req, data, cb) => {
-    // console.log(`[WRITECACHE] cache:${req.key}:${req.account_id}`);
-    redis.setex(
-      `cache:${req.key}:${req.account_id}`,
-      config.PLAYER_CACHE_SECONDS,
-      data,
-      cb
-    );
+  write,
+  sendDataWithCache: (req, res, data, key) => {
+    if (
+      config.ENABLE_PLAYER_CACHE &&
+      req.originalQuery &&
+      !Object.keys(req.originalQuery).length
+    ) {
+      write(
+        {
+          key,
+          account_id: req.params.account_id,
+        },
+        JSON.stringify(data),
+        () => {}
+      );
+    }
+    return res.json(data);
   },
   getKeys,
 };
