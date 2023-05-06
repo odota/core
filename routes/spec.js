d76f05ec50589aa7836b9df7bb636ce8792f17c3
ff137 <ff137@proton.me>
2023-05-06 19:49:32 +0200

    :art:

--- routes/spec.js
+++ routes/spec.js
@@ -185,71 +185,70 @@ You can find data that can be used to convert hero and ability IDs and other inf
                 schema: {
                   $ref: "#/components/schemas/PlayersResponse",
                 },
-                },
               },
             },
           },
         },
-        route: () => "/players/:account_id",
-        func: (req, res, cb) => {
-          const accountId = Number(req.params.account_id);
-          async.parallel(
-            {
-              profile(cb) {
-                queries.getPlayer(db, accountId, (err, playerData) => {
-                  if (playerData !== null && playerData !== undefined) {
-                    playerData.is_contributor = isContributor(accountId);
-                    playerData.is_subscriber = Boolean(playerData?.status);
-                  }
-                  cb(err, playerData);
-                });
-              },
-              solo_competitive_rank(cb) {
-                db.first()
-                  .from("solo_competitive_rank")
-                  .where({ account_id: accountId })
-                  .asCallback((err, row) => {
-                    cb(err, row ? row.rating : null);
-                  });
-              },
-              competitive_rank(cb) {
-                db.first()
-                  .from("competitive_rank")
-                  .where({ account_id: accountId })
-                  .asCallback((err, row) => {
-                    cb(err, row ? row.rating : null);
-                  });
-              },
-              rank_tier(cb) {
-                db.first()
-                  .from("rank_tier")
-                  .where({ account_id: accountId })
-                  .asCallback((err, row) => {
-                    cb(err, row ? row.rating : null);
-                  });
-              },
-              leaderboard_rank(cb) {
-                db.first()
-                  .from("leaderboard_rank")
-                  .where({ account_id: accountId })
-                  .asCallback((err, row) => {
-                    cb(err, row ? row.rating : null);
-                  });
-              },
-              mmr_estimate(cb) {
-                queries.getMmrEstimate(accountId, (err, est) =>
-                  cb(err, est || {})
-                );
-              },
+      },
+      route: () => "/players/:account_id",
+      func: (req, res, cb) => {
+        const accountId = Number(req.params.account_id);
+        async.parallel(
+          {
+            profile(cb) {
+              queries.getPlayer(db, accountId, (err, playerData) => {
+                if (playerData !== null && playerData !== undefined) {
+                  playerData.is_contributor = isContributor(accountId);
+                  playerData.is_subscriber = Boolean(playerData?.status);
+                }
+                cb(err, playerData);
+              });
             },
-            (err, result) => {
-              if (err) {
-                return cb(err);
-              }
-              return res.json(result);
+            solo_competitive_rank(cb) {
+              db.first()
+                .from("solo_competitive_rank")
+                .where({ account_id: accountId })
+                .asCallback((err, row) => {
+                  cb(err, row ? row.rating : null);
+                });
+            },
+            competitive_rank(cb) {
+              db.first()
+                .from("competitive_rank")
+                .where({ account_id: accountId })
+                .asCallback((err, row) => {
+                  cb(err, row ? row.rating : null);
+                });
+            },
+            rank_tier(cb) {
+              db.first()
+                .from("rank_tier")
+                .where({ account_id: accountId })
+                .asCallback((err, row) => {
+                  cb(err, row ? row.rating : null);
+                });
+            },
+            leaderboard_rank(cb) {
+              db.first()
+                .from("leaderboard_rank")
+                .where({ account_id: accountId })
+                .asCallback((err, row) => {
+                  cb(err, row ? row.rating : null);
+                });
+            },
+            mmr_estimate(cb) {
+              queries.getMmrEstimate(accountId, (err, est) =>
+                cb(err, est || {})
+              );
+            },
+          },
+          (err, result) => {
+            if (err) {
+              return cb(err);
             }
-          );
-        },
+            return res.json(result);
+          }
+        );
       },
     },
     "/players/{account_id}/wl": {
