4d82960345bb884e10f1adaa2abb28ba96f18b4f
ff137 <ff137@proton.me>
2023-05-06 21:12:59 +0200

    move BenchmarksResponse

--- routes/responses.js
+++ routes/responses.js
@@ -2499,4 +2499,132 @@ module.exports = {
       },
     },
   },
+  BenchmarksResponse: {
+    title: "BenchmarksResponse",
+    type: "object",
+    properties: {
+      hero_id: {
+        description: "The ID value of the hero played",
+        type: "integer",
+      },
+      result: {
+        description: "result",
+        type: "object",
+        properties: {
+          gold_per_min: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                percentile: {
+                  description: "percentile",
+                  type: "number",
+                },
+                value: {
+                  description: "value",
+                  type: "number",
+                },
+              },
+            },
+          },
+          xp_per_min: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                percentile: {
+                  description: "percentile",
+                  type: "number",
+                },
+                value: {
+                  description: "value",
+                  type: "number",
+                },
+              },
+            },
+          },
+          kills_per_min: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                percentile: {
+                  description: "percentile",
+                  type: "number",
+                },
+                value: {
+                  description: "value",
+                  type: "number",
+                },
+              },
+            },
+          },
+          last_hits_per_min: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                percentile: {
+                  description: "percentile",
+                  type: "number",
+                },
+                value: {
+                  description: "value",
+                  type: "number",
+                },
+              },
+            },
+          },
+          hero_damage_per_min: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                percentile: {
+                  description: "percentile",
+                  type: "number",
+                },
+                value: {
+                  description: "value",
+                  type: "number",
+                },
+              },
+            },
+          },
+          hero_healing_per_min: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                percentile: {
+                  description: "percentile",
+                  type: "number",
+                },
+                value: {
+                  description: "value",
+                  type: "number",
+                },
+              },
+            },
+          },
+          tower_damage: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                percentile: {
+                  description: "percentile",
+                  type: "number",
+                },
+                value: {
+                  description: "value",
+                  type: "integer",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 };
