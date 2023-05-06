5937336172313a60793f10e6c8de2313f277e58c
ff137 <ff137@proton.me>
2023-05-06 21:17:43 +0200

    :art:

--- routes/spec.js
+++ routes/spec.js
@@ -1993,60 +1993,6 @@ You can find data that can be used to convert hero and ability IDs and other inf
             }
           );
         },
-        responses: {
-          200: {
-            description: "Success",
-            schema: {
-              title: "HeroItemPopularityResponse",
-              type: "object",
-              properties: {
-                start_game_items: {
-                  description: "Items bought before game started",
-                  type: "object",
-                  properties: {
-                    item: {
-                      description: "Number of item bought",
-                      type: "integer",
-                    },
-                  },
-                },
-                early_game_items: {
-                  description:
-                    "Items bought in the first 10 min of the game, with cost at least 700",
-                  type: "object",
-                  properties: {
-                    item: {
-                      description: "Number of item bought",
-                      type: "integer",
-                    },
-                  },
-                },
-                mid_game_items: {
-                  description:
-                    "Items bought between 10 and 25 min of the game, with cost at least 2000",
-                  type: "object",
-                  properties: {
-                    item: {
-                      description: "Number of item bought",
-                      type: "integer",
-                    },
-                  },
-                },
-                late_game_items: {
-                  description:
-                    "Items bought at least 25 min after game started, with cost at least 4000",
-                  type: "object",
-                  properties: {
-                    item: {
-                      description: "Number of item bought",
-                      type: "integer",
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
       },
     },
     "/leagues": {
