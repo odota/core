d5c82e91d097912a31bacd9e4daafcc050ca4601
ff137 <ff137@proton.me>
2023-05-06 20:07:30 +0200

    move PlayerPeersResponse

--- routes/responses.js
+++ routes/responses.js
@@ -1400,4 +1400,83 @@ module.exports = {
       },
     },
   },
+  PlayerPeersResponse: {
+    title: "PlayerPeersResponse",
+    type: "object",
+    properties: {
+      account_id: {
+        description: "account_id",
+        type: "integer",
+      },
+      last_played: {
+        description: "last_played",
+        type: "integer",
+      },
+      win: {
+        description: "win",
+        type: "integer",
+      },
+      games: {
+        description: "games",
+        type: "integer",
+      },
+      with_win: {
+        description: "with_win",
+        type: "integer",
+      },
+      with_games: {
+        description: "with_games",
+        type: "integer",
+      },
+      against_win: {
+        description: "against_win",
+        type: "integer",
+      },
+      against_games: {
+        description: "against_games",
+        type: "integer",
+      },
+      with_gpm_sum: {
+        description: "with_gpm_sum",
+        type: "integer",
+      },
+      with_xpm_sum: {
+        description: "with_xpm_sum",
+        type: "integer",
+      },
+      personaname: {
+        description: "personaname",
+        type: "string",
+        nullable: true,
+      },
+      name: {
+        description: "name",
+        type: "string",
+        nullable: true,
+      },
+      is_contributor: {
+        description: "is_contributor",
+        type: "boolean",
+      },
+      is_subscriber: {
+        description: "is_subscriber",
+        type: "boolean",
+      },
+      last_login: {
+        description: "last_login",
+        type: "string",
+        nullable: true,
+      },
+      avatar: {
+        description: "avatar",
+        type: "string",
+        nullable: true,
+      },
+      avatarfull: {
+        description: "avatarfull",
+        type: "string",
+        nullable: true,
+      },
+    },
+  },
 };
