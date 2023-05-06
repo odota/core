a1efc2ea430588e4cab8fc9c5578a7bbfcf8e4d4
ff137 <ff137@proton.me>
2023-05-06 20:05:28 +0200

    move PlayerHeroesResponse

--- routes/responses.js
+++ routes/responses.js
@@ -1361,4 +1361,43 @@ module.exports = {
       },
     },
   },
+  PlayerHeroesResponse: {
+    title: "PlayerHeroesResponse",
+    description: "hero",
+    type: "object",
+    properties: {
+      hero_id: {
+        description: "The ID value of the hero played",
+        type: "string",
+      },
+      last_played: {
+        description: "last_played",
+        type: "integer",
+      },
+      games: {
+        description: "games",
+        type: "integer",
+      },
+      win: {
+        description: "win",
+        type: "integer",
+      },
+      with_games: {
+        description: "with_games",
+        type: "integer",
+      },
+      with_win: {
+        description: "with_win",
+        type: "integer",
+      },
+      against_games: {
+        description: "against_games",
+        type: "integer",
+      },
+      against_win: {
+        description: "against_win",
+        type: "integer",
+      },
+    },
+  },
 };
