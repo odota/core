52e436c92b7cd890afc4afc4bfabaeaeb9089220
ff137 <ff137@proton.me>
2023-05-07 10:32:00 +0200

    fix nesting depth of route/func definitions

--- routes/spec.js
+++ routes/spec.js
@@ -185,66 +185,66 @@ You can find data that can be used to convert hero and ability IDs and other inf
             },
           },
         },
-      },
-      route: () => "/players/:account_id",
-      func: (req, res, cb) => {
-        const accountId = Number(req.params.account_id);
-        async.parallel(
-          {
-            profile(cb) {
-              queries.getPlayer(db, accountId, (err, playerData) => {
-                if (playerData !== null && playerData !== undefined) {
-                  playerData.is_contributor = isContributor(accountId);
-                  playerData.is_subscriber = Boolean(playerData?.status);
-                }
-                cb(err, playerData);
-              });
-            },
-            solo_competitive_rank(cb) {
-              db.first()
-                .from("solo_competitive_rank")
-                .where({ account_id: accountId })
-                .asCallback((err, row) => {
-                  cb(err, row ? row.rating : null);
+        route: () => "/players/:account_id",
+        func: (req, res, cb) => {
+          const accountId = Number(req.params.account_id);
+          async.parallel(
+            {
+              profile(cb) {
+                queries.getPlayer(db, accountId, (err, playerData) => {
+                  if (playerData !== null && playerData !== undefined) {
+                    playerData.is_contributor = isContributor(accountId);
+                    playerData.is_subscriber = Boolean(playerData?.status);
+                  }
+                  cb(err, playerData);
                 });
+              },
+              solo_competitive_rank(cb) {
+                db.first()
+                  .from("solo_competitive_rank")
+                  .where({ account_id: accountId })
+                  .asCallback((err, row) => {
+                    cb(err, row ? row.rating : null);
+                  });
+              },
+              competitive_rank(cb) {
+                db.first()
+                  .from("competitive_rank")
+                  .where({ account_id: accountId })
+                  .asCallback((err, row) => {
+                    cb(err, row ? row.rating : null);
+                  });
+              },
+              rank_tier(cb) {
+                db.first()
+                  .from("rank_tier")
+                  .where({ account_id: accountId })
+                  .asCallback((err, row) => {
+                    cb(err, row ? row.rating : null);
+                  });
+              },
+              leaderboard_rank(cb) {
+                db.first()
+                  .from("leaderboard_rank")
+                  .where({ account_id: accountId })
+                  .asCallback((err, row) => {
+                    cb(err, row ? row.rating : null);
+                  });
+              },
+              mmr_estimate(cb) {
+                queries.getMmrEstimate(accountId, (err, est) =>
+                  cb(err, est || {})
+                );
+              },
             },
-            competitive_rank(cb) {
-              db.first()
-                .from("competitive_rank")
-                .where({ account_id: accountId })
-                .asCallback((err, row) => {
-                  cb(err, row ? row.rating : null);
-                });
-            },
-            rank_tier(cb) {
-              db.first()
-                .from("rank_tier")
-                .where({ account_id: accountId })
-                .asCallback((err, row) => {
-                  cb(err, row ? row.rating : null);
-                });
-            },
-            leaderboard_rank(cb) {
-              db.first()
-                .from("leaderboard_rank")
-                .where({ account_id: accountId })
-                .asCallback((err, row) => {
-                  cb(err, row ? row.rating : null);
-                });
-            },
-            mmr_estimate(cb) {
-              queries.getMmrEstimate(accountId, (err, est) =>
-                cb(err, est || {})
-              );
-            },
-          },
-          (err, result) => {
-            if (err) {
-              return cb(err);
+            (err, result) => {
+              if (err) {
+                return cb(err);
+              }
+              return res.json(result);
             }
-            return res.json(result);
-          }
-        );
+          );
+        },
       },
     },
     "/players/{account_id}/wl": {
@@ -265,34 +265,34 @@ You can find data that can be used to convert hero and ability IDs and other inf
             },
           },
         },
-      },
-      route: () => "/players/:account_id/wl",
-      func: (req, res, cb) => {
-        const result = {
-          win: 0,
-          lose: 0,
-        };
-        req.queryObj.project = req.queryObj.project.concat(
-          "player_slot",
-          "radiant_win"
-        );
-        queries.getPlayerMatches(
-          req.params.account_id,
-          req.queryObj,
-          (err, cache) => {
-            if (err) {
-              return cb(err);
-            }
-            cache.forEach((m) => {
-              if (utility.isRadiant(m) === m.radiant_win) {
-                result.win += 1;
-              } else {
-                result.lose += 1;
+        route: () => "/players/:account_id/wl",
+        func: (req, res, cb) => {
+          const result = {
+            win: 0,
+            lose: 0,
+          };
+          req.queryObj.project = req.queryObj.project.concat(
+            "player_slot",
+            "radiant_win"
+          );
+          queries.getPlayerMatches(
+            req.params.account_id,
+            req.queryObj,
+            (err, cache) => {
+              if (err) {
+                return cb(err);
               }
-            });
-            return sendDataWithCache(req, res, result, "wl");
-          }
-        );
+              cache.forEach((m) => {
+                if (utility.isRadiant(m) === m.radiant_win) {
+                  result.win += 1;
+                } else {
+                  result.lose += 1;
+                }
+              });
+              return sendDataWithCache(req, res, result, "wl");
+            }
+          );
+        },
       },
     },
     "/players/{account_id}/recentMatches": {
