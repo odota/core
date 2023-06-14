a97c138e7343edebf1cdcffe38704486e3499bb0
ff137 <ff137@proton.me>
2023-06-14 23:38:26 +0200

    move PlayerWordCloudResponse

---
+++ routes/responses/player/PlayerWordCloudResponse.js
@@ -0,0 +1,16 @@
+module.exports = {
+  PlayerWordCloudResponse: {
+    title: "PlayerWordCloudResponse",
+    type: "object",
+    properties: {
+      my_word_counts: {
+        description: "my_word_counts",
+        type: "object",
+      },
+      all_word_counts: {
+        description: "all_word_counts",
+        type: "object",
+      },
+    },
+  },
+};
