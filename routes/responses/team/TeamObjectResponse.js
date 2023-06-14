ae7caa03cd2bfa45dcf58782bf23dabd66a04171
ff137 <ff137@proton.me>
2023-06-14 23:35:59 +0200

    move TeamObjectResponse

---
+++ routes/responses/team/TeamObjectResponse.js
@@ -0,0 +1,35 @@
+const commonProperties = require("../../commonProperties");
+
+module.exports = {
+  TeamObjectResponse: {
+    title: "TeamObjectResponse",
+    type: "object",
+    properties: {
+      team_id: {
+        description: "Team's identifier",
+        type: "integer",
+      },
+      rating: {
+        description: "The Elo rating of the team",
+        type: "number",
+      },
+      wins: {
+        description: "The number of games won by this team",
+        type: "integer",
+      },
+      losses: {
+        description: "The number of losses by this team",
+        type: "integer",
+      },
+      last_match_time: {
+        description: "The Unix timestamp of the last match played by this team",
+        type: "integer",
+      },
+      name: commonProperties.team_name,
+      tag: {
+        description: "The team tag/abbreviation",
+        type: "string",
+      },
+    },
+  },
+};
