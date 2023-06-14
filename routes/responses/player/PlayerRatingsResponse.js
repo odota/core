dbae4423283442cfebdead646f3ad9f3b5f716d4
ff137 <ff137@proton.me>
2023-06-14 23:40:36 +0200

    move PlayerRatingsResponse

---
+++ routes/responses/player/PlayerRatingsResponse.js
@@ -0,0 +1,25 @@
+const commonProperties = require("../../commonProperties");
+
+module.exports = {
+  PlayerRatingsResponse: {
+    title: "PlayerRatingsResponse",
+    type: "object",
+    properties: {
+      account_id: commonProperties.account_id,
+      match_id: commonProperties.match_id,
+      solo_competitive_rank: {
+        description: "solo_competitive_rank",
+        type: "integer",
+        nullable: true,
+      },
+      competitive_rank: {
+        description: "competitive_rank",
+        type: "integer",
+      },
+      time: {
+        description: "time",
+        type: "integer",
+      },
+    },
+  },
+};
