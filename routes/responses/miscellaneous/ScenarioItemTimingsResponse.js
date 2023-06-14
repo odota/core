024dcb4c531786a98a36d524c3a718ce4d5f684d
ff137 <ff137@proton.me>
2023-06-14 23:46:07 +0200

    move ScenarioItemTimingsResponse

---
+++ routes/responses/miscellaneous/ScenarioItemTimingsResponse.js
@@ -0,0 +1,29 @@
+const commonProperties = require("../../commonProperties");
+
+module.exports = {
+  ScenarioItemTimingsResponse: {
+    title: "ScenarioItemTimingsResponse",
+    type: "object",
+    properties: {
+      hero_id: commonProperties.hero_id,
+      item: {
+        description: "Purchased item",
+        type: "string",
+      },
+      time: {
+        description: "Ingame time in seconds before the item was purchased",
+        type: "integer",
+      },
+      games: {
+        description:
+          "The number of games where the hero bought this item before this time",
+        type: "string",
+      },
+      wins: {
+        description:
+          "The number of games won where the hero bought this item before this time",
+        type: "string",
+      },
+    },
+  },
+};
