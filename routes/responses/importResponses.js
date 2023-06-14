3a0e6c433aec79226a50d0e15fb9c946d8e765f9
ff137 <ff137@proton.me>
2023-06-15 00:13:40 +0200

    modify `importResponses` script

--- routes/responses/importResponses.js
+++ routes/responses/importResponses.js
@@ -1,34 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
-// Function to import all files in a directory
-function importDirectory(directory) {
-  const files = {};
+// Recursive function to import all files in a directory and its subdirectories
+function importAll(directory) {
+  let files = {};
 
-  // Read all files in the directory
-  fs.readdirSync(directory).forEach((file) => {
-    // Ignore non-JS files
-    if (path.extname(file) !== ".js") return;
+  // Read all files and subdirectories in the directory
+  fs.readdirSync(directory).forEach((item) => {
+    const itemPath = path.join(directory, item);
 
-    // Import the file and add it to the files object
-    const filePath = path.join(directory, file);
-    const fileName = path.basename(file, ".js");
-    files[fileName] = require(`./${filePath}`);
+    if (fs.lstatSync(itemPath).isDirectory()) {
+      // If the item is a subdirectory, call this function with the subdirectory as the new starting point
+      files = { ...files, ...importAll(itemPath) };
+    } else if (path.extname(item) === ".js") {
+      // If the item is a JS file, import it
+      const fileName = path.basename(item, ".js");
+      files[fileName] = require(itemPath);
+    }
   });
 
   return files;
 }
 
-// Import all directories in the responses directory
-const responses = {};
-fs.readdirSync(".").forEach((dir) => {
-  // Ignore the current file
-  if (dir === "importResponses.js") return;
-
-  // Only process directories
-  if (fs.lstatSync(dir).isDirectory()) {
-    responses[dir] = importDirectory(dir);
-  }
-});
+// Import all files in the responses directory and its subdirectories
+const responses = importAll(__dirname);
 
 module.exports = responses;
