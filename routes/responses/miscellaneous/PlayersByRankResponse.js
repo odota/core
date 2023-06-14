1a14ed3560b1bd03fec380932b32836ac62e47eb
ff137 <ff137@proton.me>
2023-06-14 23:47:49 +0200

    move PlayersByRankResponse

---
+++ routes/responses/miscellaneous/PlayersByRankResponse.js
@@ -0,0 +1,24 @@
+const commonProperties = require("../../commonProperties");
+
+module.exports = {
+  PlayersByRankResponse: {
+    title: "PlayersByRankResponse",
+    type: "array",
+    items: {
+      type: "object",
+      properties: {
+        account_id: commonProperties.account_id,
+        rank_tier: {
+          description: "Integer indicating the rank/medal of the player",
+          type: "number",
+        },
+        fh_unavailable: {
+          description:
+            "Indicates if we were unable to fetch full history for this player due to privacy settings",
+          type: "boolean",
+          nullable: true,
+        },
+      },
+    },
+  },
+};
