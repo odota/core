0d4f880692d67f9361248139dbb2d1d9f98f0c75
ff137 <ff137@proton.me>
2023-06-14 23:37:48 +0200

    move ParsedMatchesResponse

---
+++ routes/responses/public/ParsedMatchesResponse.js
@@ -0,0 +1,11 @@
+const commonProperties = require("../../commonProperties");
+
+module.exports = {
+  ParsedMatchesResponse: {
+    title: "ParsedMatchesResponse",
+    type: "object",
+    properties: {
+      match_id: commonProperties.match_id,
+    },
+  },
+};
