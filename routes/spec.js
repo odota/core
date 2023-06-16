5472b5e0b920805592270e031a79d6908fea9416
ff137 <ff137@proton.me>
2023-06-16 21:50:33 +0200

    :art: Update API description

--- routes/spec.js
+++ routes/spec.js
@@ -79,12 +79,12 @@ const spec = {
   info: {
     title: "OpenDota API",
     description: `# Introduction
-The OpenDota API provides Dota 2 related data including advanced match data extracted from match replays.
-
-You can find data that can be used to convert hero and ability IDs and other information provided by the API from the [dotaconstants](https://github.com/odota/dotaconstants) repository.
-
-**Beginning 2018-04-22, the OpenDota API is limited to 50,000 free calls per month and 60 requests/minute** We offer a Premium Tier with unlimited API calls and higher rate limits. Check out the [API page](https://www.opendota.com/api-keys) to learn more.
-`,
+    The OpenDota API provides Dota 2 related data including advanced match data extracted from match replays.
+    
+    You can find data that can be used to convert hero and ability IDs and other information provided by the API from the [dotaconstants](https://github.com/odota/dotaconstants) repository.
+    
+    The OpenDota API offers 50,000 free calls per month and a rate limit of 60 requests/minute. We also offer a Premium Tier with unlimited API calls and higher rate limits. Check out the [API page](https://www.opendota.com/api-keys) to learn more.
+    `,
     version: packageJson.version,
   },
   servers: [
