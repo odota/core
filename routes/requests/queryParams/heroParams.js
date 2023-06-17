6b620cb11b463b24d5255aa194a046afac696220
ff137 <ff137@proton.me>
2023-06-17 16:10:51 +0200

    :art: triage common request properties into submodules

---
+++ routes/requests/queryParams/heroParams.js
@@ -0,0 +1,11 @@
+module.exports = {
+  heroIdPathParam: {
+    name: "hero_id",
+    in: "path",
+    description: "Hero ID",
+    required: true,
+    schema: {
+      type: "integer",
+    },
+  },
+};
---
+++ routes/requests/queryParams/scenarioParams.js
@@ -0,0 +1,13 @@
+const su = require("../../../util/scenariosUtil");
+
+module.exports = {
+  scenarioParam: {
+    name: "scenario",
+    in: "query",
+    description: su.teamScenariosQueryParams.toString(),
+    required: false,
+    schema: {
+      type: "string",
+    },
+  },
+};
