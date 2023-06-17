d7bdd24cabcdb95665c08263089c400d7c47254e
ff137 <ff137@proton.me>
2023-06-17 16:11:37 +0200

    :art: imports

--- routes/spec.js
+++ routes/spec.js
@@ -18,8 +18,8 @@ const db = require("../store/db");
 const redis = require("../store/redis");
 const packageJson = require("../package.json");
 const cacheFunctions = require("../store/cacheFunctions");
-const params = require("./properties/requests/commonProperties");
-const responses = require("./responseSchemas/importResponses");
+const params = require("./requests/importParams");
+const responses = require("./responses/schemas/importResponseSchemas");
 const generateOperationId = require("./generateOperationId");
 
 const { redisCount, countPeers, isContributor, matchupToString } = utility;
