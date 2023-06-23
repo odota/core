42784a408272872ee5dd068578638edcdd53f6b2
ff137 <ff137@proton.me>
2023-06-23 20:21:24 +0200

    set early-game item cost to >= 600

--- store/queries.js
+++ store/queries.js
@@ -324,7 +324,7 @@ function getHeroItemPopularity(db, redis, heroId, options, cb) {
     );
     const earlyGameItems = countItemPopularity(
       items.filter(
-        (item) => item.time > 0 && item.time < 60 * 10 && item.cost > 700
+        (item) => item.time > 0 && item.time < 60 * 10 && item.cost >= 600
       )
     );
     const midGameItems = countItemPopularity(
