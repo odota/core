356751e6908c243a96d4953cb41e152f1e1f011d
ff137 <ff137@proton.me>
2023-09-03 15:28:11 +0300

    Replace await in loop with an array of promises and execute all concurrently with `Promise.all()`

--- svc/syncSubs.js
+++ svc/syncSubs.js
@@ -11,26 +11,24 @@ const { invokeInterval } = utility;
 
 async function run() {
   // Get list of current subscribers
-  const result = [];
-  for await (const sub of stripe.subscriptions.list({
+  const result = await stripe.subscriptions.list({
     limit: 100,
     status: "active",
     price: "price_1LE5NqCHN72mG1oKg2Y9pqXb",
-  })) {
-    result.push(sub);
-  }
+  }).autoPagingToArray({limit: 100});
+
   console.log(result.length, "subs");
   await db.raw("BEGIN TRANSACTION");
   // Delete all status from subscribers
   await db.raw("UPDATE subscriber SET status = NULL");
-  for (let i = 0; i < result.length; i++) {
-    const sub = result[i];
-    // Mark list of subscribers as active
-    await db.raw("UPDATE subscriber SET status = ? WHERE customer_id = ?", [
+  const updatePromises = result.map(sub => {
+    return db.raw("UPDATE subscriber SET status = ? WHERE customer_id = ?", [
       sub.status,
       sub.customer,
     ]);
-  }
+  });
+
+  await Promise.all(updatePromises);
   await db.raw("COMMIT");
 }
 
