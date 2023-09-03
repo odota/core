653327b84661f4a9a459591b95e8b267405027b5
ff137 <ff137@proton.me>
2023-09-03 15:47:24 +0300

    :art: remove file extension from import

--- scripts/convertSpec.js
+++ scripts/convertSpec.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
-const spec = require("../routes/spec.js");
+const spec = require("../routes/spec");
 
 fs.writeFileSync("../spec.json", JSON.stringify(spec, null, 2), "utf-8");
