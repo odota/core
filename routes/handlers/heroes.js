488348e08964c9c89fa3bbc6c00ad3fd3bfb80ae
ff137 <ff137@proton.me>
2023-09-15 21:28:56 +0300

    :art: refactor: move getRecentMatchesByHeroId route handler to heroes module

--- routes/handlers/heroes.js
+++ routes/handlers/heroes.js
@@ -49,9 +49,42 @@ function getHeroStats(req, res, cb) {
   });
 }
 
+function getRecentMatchesByHeroId(req, res, cb) {
+  const heroId = req.params.hero_id;
+  db.raw(
+    `SELECT
+    matches.match_id,
+    matches.start_time,
+    matches.duration,
+    matches.radiant_win,
+    matches.leagueid,
+    leagues.name as league_name,
+    ((player_matches.player_slot < 128) = matches.radiant_win) radiant,
+    player_matches.player_slot,
+    player_matches.account_id,
+    player_matches.kills,
+    player_matches.deaths,
+    player_matches.assists
+    FROM matches
+    JOIN player_matches using(match_id)
+    JOIN leagues using(leagueid)
+    LEFT JOIN heroes on heroes.id = player_matches.hero_id
+    WHERE player_matches.hero_id = ?
+    ORDER BY matches.match_id DESC
+    LIMIT 100`,
+    [heroId]
+  ).asCallback((err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    return res.json(result.rows);
+  });
+}
+
 module.exports = {
   getHeroBenchmarks,
   getHeroRankings,
   getHeroData,
   getHeroStats,
+  getRecentMatchesByHeroId,
 };
