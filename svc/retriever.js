5e64e72746ee21d20059fc4897cad2d89a01f106
Ubuntu <howardzchung@gmail.com>
2023-10-10 01:39:12 +0000

    increase matchrequestdelay

--- svc/retriever.js
+++ svc/retriever.js
@@ -29,8 +29,8 @@ const timeoutMs = 1500;
 const accountsToUse = 2;
 const matchRequestLimit = 950;
 const port = config.PORT || config.RETRIEVER_PORT;
-const matchRequestDelay = 400;
-const matchRequestDelayStep = 3;
+const matchRequestDelay = 500;
+const matchRequestDelayStep = 100;
 
 let matchRequestDelayIncr = 0;
 let lastRequestTime;
