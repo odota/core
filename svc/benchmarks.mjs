55465f935370be98e6a4c0145dd94ee4496cf4e1
Ubuntu <howardzchung@gmail.com>
2023-12-02 10:19:53 +0000

    inline benchmarks

--- svc/benchmarks.mjs
+++ svc/benchmarks.mjs
@@ -1,10 +1,39 @@
 import queue from '../store/queue.mjs';
-import benchmarksUtil from '../util/benchmarksUtil.js';
 import buildMatch from '../store/buildMatch.mjs';
 import utility from '../util/utility.js';
 import config from '../config.js';
 import redis from '../store/redis.mjs';
-const { benchmarks } = benchmarksUtil;
+
+const benchmarks = {
+  gold_per_min(m, p) {
+    return p.gold_per_min;
+  },
+  xp_per_min(m, p) {
+    return p.xp_per_min;
+  },
+  kills_per_min(m, p) {
+    return (p.kills / m.duration) * 60;
+  },
+  last_hits_per_min(m, p) {
+    return (p.last_hits / m.duration) * 60;
+  },
+  hero_damage_per_min(m, p) {
+    return (p.hero_damage / m.duration) * 60;
+  },
+  hero_healing_per_min(m, p) {
+    return (p.hero_healing / m.duration) * 60;
+  },
+  tower_damage(m, p) {
+    return p.tower_damage;
+  },
+  // stuns_per_min(m, p) {
+  //   return (p.stuns / m.duration) * 60;
+  // },
+  // lhten(m, p) {
+  //   return p.lh_t && p.lh_t[10];
+  // },
+};
+
 async function doBenchmarks(matchID, cb) {
   try {
     const match = await buildMatch(matchID);
