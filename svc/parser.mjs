f3443d76357a08eb88927aaf85ca27e615980100
Ubuntu <howardzchung@gmail.com>
2023-12-02 12:45:46 +0000

    inline runParse

--- svc/parser.mjs
+++ svc/parser.mjs
@@ -26,48 +26,45 @@ app.get('/healthz', (req, res) => {
   res.end('ok');
 });
 app.listen(PORT || PARSER_PORT);
-async function runParse(match, url) {
-  if (NODE_ENV === 'test') {
-    url = `https://odota.github.io/testfiles/${match.match_id}_1.dem`;
-  }
-  console.log(new Date(), url);
-  const { stdout } = await execPromise(
-    `curl --max-time 180 --fail ${url} | ${
-      url && url.slice(-3) === 'bz2' ? 'bunzip2' : 'cat'
-    } | curl -X POST -T - ${PARSER_HOST} | node processors/createParsedDataBlob.mjs ${
-      match.match_id
-    }`,
-    { shell: true, maxBuffer: 10 * 1024 * 1024 }
-  );
-  const result = { ...JSON.parse(stdout), ...match };
-  await insertMatchPromise(result, {
-    type: 'parsed',
-    skipParse: true,
-  });
-  // Mark this match parsed
-  await db.raw(
-    'INSERT INTO parsed_matches(match_id) VALUES(?) ON CONFLICT DO NOTHING',
-    [Number(match.match_id)]
-  );
-  // Decide if we want to do scenarios (requires parsed match)
-  // Only if it originated from scanner to avoid triggering on requests
-  if (
-    match.origin === 'scanner' &&
-    match.match_id % 100 < config.SCENARIOS_SAMPLE_PERCENT
-  ) {
-    return redis.rpush('scenariosQueue', match.match_id, cb);
-  }
-}
 async function parseProcessor(job, cb) {
   const match = job;
   try {
-    const result = await getGcData(match);
-    const url = buildReplayUrl(
-      result.match_id,
-      result.cluster,
-      result.replay_salt
+    const gcdata = await getGcData(match);
+    let url = buildReplayUrl(
+      gcdata.match_id,
+      gcdata.cluster,
+      gcdata.replay_salt
     );
-    await runParse(match, url);
+    if (NODE_ENV === 'test') {
+      url = `https://odota.github.io/testfiles/${match.match_id}_1.dem`;
+    }
+    console.log('[PARSER] parsing replay at:', url);
+    const { stdout } = await execPromise(
+      `curl --max-time 180 --fail ${url} | ${
+        url && url.slice(-3) === 'bz2' ? 'bunzip2' : 'cat'
+      } | curl -X POST -T - ${PARSER_HOST} | node processors/createParsedDataBlob.mjs ${
+        match.match_id
+      }`,
+      { shell: true, maxBuffer: 10 * 1024 * 1024 }
+    );
+    const result = { ...JSON.parse(stdout), ...match };
+    await insertMatchPromise(result, {
+      type: 'parsed',
+      skipParse: true,
+    });
+    // Mark this match parsed
+    await db.raw(
+      'INSERT INTO parsed_matches(match_id) VALUES(?) ON CONFLICT DO NOTHING',
+      [Number(match.match_id)]
+    );
+    // Decide if we want to do scenarios (requires parsed match)
+    // Only if it originated from scanner to avoid triggering on requests
+    if (
+      match.origin === 'scanner' &&
+      match.match_id % 100 < config.SCENARIOS_SAMPLE_PERCENT
+    ) {
+      return redis.rpush('scenariosQueue', match.match_id, cb);
+    }
     console.log('[PARSER] completed parse of match %s', match.match_id);
     cb(null, match.match_id);
   } catch (e) {
