d428bb37d01c3de2c24a89d1483210cb6157f68e
Ubuntu <howardzchung@gmail.com>
2023-12-02 15:58:49 +0000

    rewrite monitor with axios and async

--- svc/monitor.mjs
+++ svc/monitor.mjs
@@ -1,142 +1,11 @@
 // Runs health checks periodically and writes result to Redis
-// TODO use top level await
-import request from 'request';
+import axios from 'axios';
 import config from '../config.js';
 import redis from '../store/redis.mjs';
 import db from '../store/db.mjs';
 import cassandra from '../store/cassandra.mjs';
-import utility from '../util/utility.mjs';
 const apiKey = config.STEAM_API_KEY.split(',')[0];
-function invokeInterval(func) {
-  // invokes the function immediately, waits for callback, waits the delay, and then calls it again
-  (function invoker() {
-    console.log('running %s', func.name);
-    console.time(func.name);
-    func((err, result) => {
-      if (err) {
-        console.error(err);
-      }
-      const final = result || {
-        metric: 1,
-        threshold: 0,
-      };
-      final.timestamp = Math.floor(new Date() / 1000);
-      redis.hset('health', func.name, JSON.stringify(final));
-      redis.expire('health', 900);
-      console.timeEnd(func.name);
-      setTimeout(invoker, final && final.delay ? final.delay : 10000);
-    });
-  })();
-}
-function steamApi(cb) {
-  request(
-    `${'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key='}${apiKey}`,
-    (err, resp, body) => {
-      if (err || resp.statusCode !== 200) {
-        return cb('bad http response');
-      }
-      try {
-        const fail =
-          err ||
-          resp.statusCode !== 200 ||
-          JSON.parse(body).result.status !== 1;
-        return cb(fail, {
-          metric: Number(fail),
-          threshold: 1,
-        });
-      } catch (e) {
-        return cb('malformed http response');
-      }
-    }
-  );
-}
-function seqNumDelay(cb) {
-  utility.getData(utility.generateJob('api_history', {}).url, (err, body) => {
-    if (err) {
-      return cb('failed to get current sequence number');
-    }
-    // get match_seq_num, compare with real seqnum
-    const currSeqNum = body.result.matches[0].match_seq_num;
-    return redis.get('match_seq_num', (err, num) => {
-      if (err) {
-        return cb(err);
-      }
-      num = Number(num);
-      const metric = currSeqNum - num;
-      return cb(err, {
-        metric,
-        threshold: 50000,
-      });
-    });
-  });
-}
-function parseDelay(cb) {
-  db.raw("select count(*) from queue where type = 'parse'").asCallback(
-    (err, result) => {
-      if (err) {
-        return cb(err);
-      }
-      return cb(err, {
-        metric: result.rows[0].count,
-        threshold: 3000,
-      });
-    }
-  );
-}
-function gcDelay(cb) {
-  redis.llen('gcQueue', (err, result) => {
-    if (err) {
-      return cb(err);
-    }
-    return cb(err, {
-      metric: result,
-      threshold: 300000,
-    });
-  });
-}
-function postgresUsage(cb) {
-  db.raw("select pg_database_size('yasp')").asCallback((err, result) => {
-    if (err) {
-      return cb(err);
-    }
-    return cb(err, {
-      metric: result.rows[0].pg_database_size,
-      threshold: 4 * 10 ** 11,
-    });
-  });
-}
-function cassandraUsage(cb) {
-  cassandra.execute(
-    "select mean_partition_size, partitions_count from system.size_estimates where keyspace_name = 'yasp'",
-    (err, result) => {
-      if (err) {
-        return cb(err);
-      }
-      let size = 0;
-      result.rows.forEach((r) => {
-        size += r.mean_partition_size * r.partitions_count * 0.4;
-      });
-      return cb(err, {
-        metric: size,
-        threshold: 1.8 * 10 ** 12,
-      });
-    }
-  );
-}
-async function redisUsage(cb) {
-  try {
-    const info = await redis.info();
-    const line = info
-      .split('\n')
-      .find((line) => line.startsWith('used_memory'));
-    return cb(null, {
-      metric: Number(line.split(':')[1]),
-      threshold: 4 * 10 ** 9,
-    });
-  } catch (e) {
-    cb(e);
-  }
-}
+
 const health = {
   steamApi,
   seqNumDelay,
@@ -146,6 +15,88 @@ const health = {
   cassandraUsage,
   redisUsage,
 };
-Object.keys(health).forEach((key) => {
-  invokeInterval(health[key]);
-});
+
+setInterval(() => {
+  Object.entries(health).forEach(async ([key, value]) => {
+    let final = { 
+      metric: 1,
+      threshold: 1,
+    };
+    try {
+      final = await value();
+      final.timestamp = Math.floor(new Date() / 1000);
+      console.log('[%s]: %s', key, JSON.stringify(final));
+    } catch(e) {
+      console.log('[%s] error: %s', key, e);
+    }
+    await redis.hset('health', key, JSON.stringify(final));
+    await redis.expire('health', 900);
+  });
+}, 10000);
+
+async function steamApi() {
+  const resp = await axios.get('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=' + apiKey);
+  const body = resp.data;
+  const fail = body.result.status !== 1;
+  return {
+    metric: Number(fail),
+    threshold: 1,
+  };
+}
+async function seqNumDelay() {
+  const resp = await axios.get('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=' + apiKey);
+  const body = resp.data;
+  // get match_seq_num, compare with real seqnum
+  const currSeqNum = body.result.matches[0].match_seq_num;
+  let num = await redis.get('match_seq_num');
+  num = Number(num);
+  const metric = currSeqNum - num;
+  return {
+    metric,
+    threshold: 50000,
+  };
+}
+async function parseDelay() {
+  const result = await db.raw("select count(*) from queue where type = 'parse'");
+  return {
+    metric: result.rows[0].count,
+    threshold: 5000,
+  }
+}
+async function gcDelay() {
+  const result = await redis.llen('gcQueue');
+  return {
+    metric: result,
+    threshold: 100000,
+  };
+}
+async function postgresUsage() {
+  const result = await db.raw("select pg_database_size('yasp')");
+  return {
+    metric: result.rows[0].pg_database_size,
+    threshold: 4 * 10 ** 11,
+  };
+}
+async function cassandraUsage() {
+  const result = await cassandra.execute(
+    "select mean_partition_size, partitions_count from system.size_estimates where keyspace_name = 'yasp'"
+  );
+  let size = 0;
+  result.rows.forEach((r) => {
+    size += r.mean_partition_size * r.partitions_count
+  });
+  return {
+    metric: size,
+    threshold: 3 * 10 ** 12,
+  };
+}
+async function redisUsage() {
+  const info = await redis.info();
+  const line = info
+    .split('\n')
+    .find((line) => line.startsWith('used_memory'));
+  return {
+    metric: Number(line.split(':')[1]),
+    threshold: 4 * 10 ** 9,
+  };
+}
