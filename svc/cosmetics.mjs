21ef85b11686ab684e91e9493ac530c2f920d9c2
builder_247 <14019974+builder-247@users.noreply.github.com>
2023-12-03 22:11:10 +0200

    don't try catch

--- svc/cosmetics.mjs
+++ svc/cosmetics.mjs
@@ -8,48 +8,45 @@ const { cleanItemSchema, getDataPromise } = utility;
 
 while (true) {
   console.time('doCosmetics');
-  try {
-    const items = await getDataPromise({
-      url: 'https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/items/items_game.txt',
-      raw: true,
-    });
-    const icons = await getDataPromise(
-      'https://raw.githubusercontent.com/builder-247/node-dota2-cdn/main/build/icons.json'
-    );
-    const itemData = vdf.parse(cleanItemSchema(items));
+  const items = await getDataPromise({
+    url: 'https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/items/items_game.txt',
+    raw: true,
+  });
+  const icons = await getDataPromise(
+    'https://raw.githubusercontent.com/builder-247/node-dota2-cdn/main/build/icons.json'
+  );
+  const itemData = vdf.parse(cleanItemSchema(items));
 
-    async function processItem(itemId) {
-      const item = itemData.items_game.items[itemId];
-      item.item_id = Number(itemId);
-      const hero =
-        item.used_by_heroes &&
-        typeof item.used_by_heroes === 'object' &&
-        Object.keys(item.used_by_heroes)[0];
-      if (hero) {
-        item.used_by_heroes = hero;
-      }
-      if (!item.item_id) {
-        return;
-      }
-      if (item.image_inventory) {
-        const spl = item.image_inventory.split('/');
-        const iconname = spl[spl.length - 1];
-        if (icons[iconname]) {
-          item.image_path = icons[iconname];
-        }
-      }
-      await queries.upsertPromise(db, 'cosmetics', item, {
-        item_id: item.item_id,
-      });
+  async function processItem(itemId) {
+    const item = itemData.items_game.items[itemId];
+    item.item_id = Number(itemId);
+    const hero =
+      item.used_by_heroes &&
+      typeof item.used_by_heroes === 'object' &&
+      Object.keys(item.used_by_heroes)[0];
+    if (hero) {
+      item.used_by_heroes = hero;
     }
-
-    const promises = Object.keys(itemData.items_game.items).map(
-      (i)=> processItem(i)
-    );
-    await eachLimit(promises, 10);
-  } catch (error) {
-    console.log(error);
+    if (!item.item_id) {
+      return;
+    }
+    if (item.image_inventory) {
+      const spl = item.image_inventory.split('/');
+      const iconname = spl[spl.length - 1];
+      if (icons[iconname]) {
+        item.image_path = icons[iconname];
+      }
+    }
+    await queries.upsertPromise(db, 'cosmetics', item, {
+      item_id: item.item_id,
+    });
   }
+
+  const promises = Object.keys(itemData.items_game.items).map((i) =>
+    processItem(i)
+  );
+  await eachLimit(promises, 10);
+
   console.timeEnd('doCosmetics');
   await new Promise((resolve) => setTimeout(resolve, 12 * 60 * 60 * 1000));
 }
