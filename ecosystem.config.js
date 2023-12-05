abe7c06182148e350127b3ed3c17163a94992237
Ubuntu <howardzchung@gmail.com>
2023-12-05 05:54:08 +0000

    prettier

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,158 +1,157 @@
 const { GROUP, NODE_ENV } = require('./config.js');
 
 let arr = [
-    {
-        "name": "web",
-        "script": "svc/web.mjs",
-        "group": "web",
-      },
-      {
-        "name": "retriever",
-        "script": "svc/retriever.mjs",
-        "group": "retriever",
-      },
-      {
-        "name": "proxy",
-        "script": "svc/proxy.mjs",
-        "group": "proxy",
-      },
-      {
-        "name": "parser",
-        "script": "svc/parser.mjs",
-      },
-    {
-      "name":  "apiadmin",
-      "script": "svc/apiadmin.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "mmr",
-      "script": "svc/mmr.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "profiler",
-      "script": "svc/profiler.mjs",
-      "group": "backend",
-    },
-    {
-     "name": "scanner",
-      "script": "svc/scanner.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "fullhistory",
-      "script": "svc/fullhistory.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "autofullhistory",
-      "script": "svc/autofullhistory.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "monitor",
-      "script": "svc/monitor.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "gcdata",
-      "script": "svc/gcdata.mts",
-      "interpreter": "node_modules/.bin/tsx",
-      "group": "backend",
-    },
-    {
-      "name": "buildsets",
-      "script": "svc/buildsets.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "cosmetics",
-      "script": "svc/cosmetics.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "distributions",
-      "script": "svc/distributions.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "heroes",
-      "script": "svc/heroes.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "herostats",
-      "script": "svc/herostats.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "items",
-      "script": "svc/items.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "leagues",
-      "script": "svc/leagues.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "livegames",
-      "script": "svc/livegames.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "proplayers",
-      "script": "svc/proplayers.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "teams",
-      "script": "svc/teams.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "scenarios",
-      "script": "svc/scenarios.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "scenariosCleanup",
-      "script": "svc/scenariosCleanup.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "counts",
-      "script": "svc/counts.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "syncSubs",
-      "script": "svc/syncSubs.mjs",
-      "group": "backend",
-    },
-    {
-      "name": "cassandraDelete",
-      "script": "svc/cassandraDelete.mjs",
-      "group": "backend",
-    }
-  ];
+  {
+    name: 'web',
+    script: 'svc/web.mjs',
+    group: 'web',
+  },
+  {
+    name: 'retriever',
+    script: 'svc/retriever.mjs',
+    group: 'retriever',
+  },
+  {
+    name: 'proxy',
+    script: 'svc/proxy.mjs',
+    group: 'proxy',
+  },
+  {
+    name: 'parser',
+    script: 'svc/parser.mjs',
+  },
+  {
+    name: 'apiadmin',
+    script: 'svc/apiadmin.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'mmr',
+    script: 'svc/mmr.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'profiler',
+    script: 'svc/profiler.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'scanner',
+    script: 'svc/scanner.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'fullhistory',
+    script: 'svc/fullhistory.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'autofullhistory',
+    script: 'svc/autofullhistory.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'monitor',
+    script: 'svc/monitor.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'gcdata',
+    script: 'svc/gcdata.mts',
+    interpreter: 'node_modules/.bin/tsx',
+    group: 'backend',
+  },
+  {
+    name: 'buildsets',
+    script: 'svc/buildsets.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'cosmetics',
+    script: 'svc/cosmetics.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'distributions',
+    script: 'svc/distributions.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'heroes',
+    script: 'svc/heroes.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'herostats',
+    script: 'svc/herostats.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'items',
+    script: 'svc/items.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'leagues',
+    script: 'svc/leagues.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'livegames',
+    script: 'svc/livegames.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'proplayers',
+    script: 'svc/proplayers.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'teams',
+    script: 'svc/teams.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'scenarios',
+    script: 'svc/scenarios.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'scenariosCleanup',
+    script: 'svc/scenariosCleanup.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'counts',
+    script: 'svc/counts.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'syncSubs',
+    script: 'svc/syncSubs.mjs',
+    group: 'backend',
+  },
+  {
+    name: 'cassandraDelete',
+    script: 'svc/cassandraDelete.mjs',
+    group: 'backend',
+  },
+];
 
 // If GROUP is set filter to only the matching group
-arr = arr.filter(app => !GROUP || app.group === GROUP);
+arr = arr.filter((app) => !GROUP || app.group === GROUP);
 
-const apps = arr.map(app => {
-    const dev = NODE_ENV === 'development';
-    return {
-        ...app,
-        watch: dev ? true : false,
-        ignore_watch: [".git", "node_modules"],
-        exec_mode: 'fork',
-        instances: 1,
-    }
+const apps = arr.map((app) => {
+  const dev = NODE_ENV === 'development';
+  return {
+    ...app,
+    watch: dev ? true : false,
+    ignore_watch: ['.git', 'node_modules'],
+    exec_mode: 'fork',
+    instances: 1,
+  };
 });
 
 module.exports = {
-  apps
+  apps,
 };
-  
--- store/queue.mjs
+++ store/queue.mjs
@@ -5,26 +5,25 @@ import db from './db.mjs';
 async function runQueue(queueName, parallelism, processor) {
   Array.from(new Array(parallelism), (v, i) => i).forEach(async (i) => {
     try {
-    while (true) {
-      const job = await redis.blpop(queueName, '0');
-      const jobData = JSON.parse(job[1]);
-      await processor(jobData);
+      while (true) {
+        const job = await redis.blpop(queueName, '0');
+        const jobData = JSON.parse(job[1]);
+        await processor(jobData);
+      }
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
     }
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  }
   });
-
 }
 
 async function runReliableQueue(queueName, parallelism, processor) {
   Array.from(new Array(parallelism), (v, i) => i).forEach(async (i) => {
     try {
-    while (true) {
-      const trx = await db.transaction();
-      const result = await trx.raw(
-        `
+      while (true) {
+        const trx = await db.transaction();
+        const result = await trx.raw(
+          `
         UPDATE queue SET attempts = attempts - 1, next_attempt_time = ?
         WHERE id = (
         SELECT id
@@ -37,33 +36,33 @@ async function runReliableQueue(queueName, parallelism, processor) {
         )
         RETURNING *
         `,
-        [moment().add(2, 'minute'), queueName]
-      );
-      const job = result && result.rows && result.rows[0];
-      if (job) {
-        // Handle possible exception here since we still need to commit the transaction to update attempts
-        let success = false;
-        try {
-          success = await processor(job.data);
-        } catch (e) {
-          // Don't crash the process as we expect some processing failures
-          console.error(e);
+          [moment().add(2, 'minute'), queueName]
+        );
+        const job = result && result.rows && result.rows[0];
+        if (job) {
+          // Handle possible exception here since we still need to commit the transaction to update attempts
+          let success = false;
+          try {
+            success = await processor(job.data);
+          } catch (e) {
+            // Don't crash the process as we expect some processing failures
+            console.error(e);
+          }
+          if (success || job.attempts <= 0) {
+            // remove the job from the queue if successful or out of attempts
+            await trx.raw('DELETE FROM queue WHERE id = ?', [job.id]);
+          }
+          await trx.commit();
+        } else {
+          await trx.commit();
+          // console.log('no job available, waiting');
+          await new Promise((resolve) => setTimeout(resolve, 5000));
         }
-        if (success || job.attempts <= 0) {
-          // remove the job from the queue if successful or out of attempts
-          await trx.raw('DELETE FROM queue WHERE id = ?', [job.id]);
-        }
-        await trx.commit();
-      } else {
-        await trx.commit();
-        // console.log('no job available, waiting');
-        await new Promise((resolve) => setTimeout(resolve, 5000));
       }
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
     }
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  }
   });
 }
 async function addJob(queueName, job) {
