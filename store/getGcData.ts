317a6d7159203915339e890d8d830e1cc2834c43
Ubuntu <howardzchung@gmail.com>
2023-12-08 09:28:00 +0000

    check blobstore for cached gcdata

--- store/getGcData.ts
+++ store/getGcData.ts
@@ -3,6 +3,7 @@ import config from '../config.js';
 import { insertMatchPromise, upsertPromise } from './queries';
 import db from './db';
 import redis from './redis';
+import cassandra from './cassandra';
 import { getDataPromise, getRetrieverArr, redisCount } from '../util/utility';
 const secret = config.RETRIEVER_SECRET;
 
@@ -32,8 +33,6 @@ async function getGcDataFromRetriever(match: GcDataJob) {
   );
   // NOTE: account ids are not anonymous in this call so we don't include it in the data to insert
   // We could start storing this data but then the API also needs to respect the user's match history setting
-  // NOTE: extra player fields won't be set on repeated parses unless we manually delete match_gcdata row
-  // if someone requests a reparse we won't have this data
   const players = body.match.players.map((p: GcPlayer, i: number) => ({
     player_slot: p.player_slot,
     party_id: p.party_id?.low,
@@ -48,6 +47,8 @@ async function getGcDataFromRetriever(match: GcDataJob) {
     players,
     series_id: body.match.series_id,
     series_type: body.match.series_type,
+    // With match_id, cluster, and replay salt we can construct a replay URL
+    cluster: body.match.cluster,
     replay_salt: body.match.replay_salt,
   };
   const gcdata = {
@@ -71,21 +72,25 @@ async function getGcDataFromRetriever(match: GcDataJob) {
   });
   return gcdata;
 }
-export default async function getGcData(match: GcDataJob) {
+export default async function getGcData(match: GcDataJob): Promise<{ match_id: number, cluster: number, replay_salt: number }> {
   const matchId = match.match_id;
   if (!matchId || Number.isNaN(Number(matchId)) || Number(matchId) <= 0) {
     throw new Error('invalid match_id');
   }
-  // Check if we have it in DB already
-  const saved = await db.raw(
-    'select match_id, cluster, replay_salt from match_gcdata where match_id = ?',
-    [match.match_id]
-  );
-  const gcdata = saved.rows[0];
-  if (config.NODE_ENV !== 'development' && gcdata) {
+  // Check if we have it saved already
+  // NOTE: We don't check against match_gcdata here
+  // This is because we write data to blobstore as part of this and we might want to dump the blobs table periodically or clean it up
+  // if we do that we want to refetch the gcdata so the data is complete on re-archival
+  // (otherwise e.g. pro matches might be missing permanent_buffs and party ids)
+  // because that match still has match_gcdata values
+  const result = await cassandra.execute('SELECT gcdata FROM match_blobs WHERE match_id = ?', [Number(match.match_id)], { prepare: true, fetchSize: 1, autoPage: true });
+  const row = result.rows[0];
+  // Extract match_id, cluster, replay_salt
+  const gcdata = row?.gcdata ? JSON.parse(row?.gcdata) : null;
+  if (config.NODE_ENV !== 'development' && gcdata && gcdata.match_id && gcdata.cluster && gcdata.replay_salt) {
     // console.log('found cached gcdata for %s', matchId);
     redisCount(redis, 'cached_gcdata');
-    return gcdata;
+    return { match_id: gcdata.match_id, cluster: gcdata.cluster, replay_salt: gcdata.replay_salt };
   }
   return await getGcDataFromRetriever(match);
 }
