92cbb3fe629e2c45869614bd565c715d2666842c
Ubuntu <howardzchung@gmail.com>
2023-12-10 21:14:43 +0000

    use buildscript and extend timeout

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 # Exit on error
 set -e
 
-docker-compose up -d && sleep 20 && docker exec -it odota-core sh -c 'npm run test'
+docker-compose -f docker-compose.ci.yml up -d && sleep 45 && docker ps -a && docker logs odota-core && docker exec -i odota-core sh -c 'npm run test'
 
