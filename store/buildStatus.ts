d45065a76153bc9659b0b207e1b60fe422a27dd0
Ubuntu <howardzchung@gmail.com>
2023-12-16 01:39:42 +0000

    use parallelPromise for buildStatus and reimplement redis counters using incr/mget

--- store/buildStatus.ts
+++ store/buildStatus.ts
@@ -1,6 +1,5 @@
-import async from 'async';
 import redis from '../store/redis';
-import { getRedisCountDay, getRedisCountHour } from '../util/utility';
+import { getRedisCountDay, getRedisCountHour, parallelPromise } from '../util/utility';
 
 function generatePercentiles(arr: string[]) {
   // sort the list
@@ -19,183 +18,96 @@ function generatePercentiles(arr: string[]) {
 }
 
 export async function buildStatus() {
-  return async.parallel({
-    user_players(cb) {
-      redis.zcard('visitors', cb);
-    },
-    tracked_players(cb) {
-      redis.zcard('tracked', cb);
-    },
-    matches_last_day(cb) {
-      getRedisCountDay(redis, 'added_match', cb);
-    },
-    matches_prev_hour(cb) {
-      getRedisCountHour(redis, 'added_match', cb);
-    },
-    auto_parse_last_day(cb) {
-      getRedisCountDay(redis, 'auto_parse', cb);
-    },
-    retriever_matches_last_day(cb) {
-      getRedisCountDay(redis, 'retriever', cb);
-    },
-    retriever_players_last_day(cb) {
-      getRedisCountDay(redis, 'retriever_player', cb);
-    },
-    // backup_retriever_last_day(cb) {
-    //   getRedisCountDay(redis, "backup", cb);
-    // },
-    parsed_matches_last_day(cb) {
-      getRedisCountDay(redis, 'parser', cb);
-    },
-    parse_fails_last_day(cb) {
-      getRedisCountDay(redis, 'parser_fail', cb);
-    },
-    meta_parsed_last_day(cb) {
-      getRedisCountDay(redis, 'meta_parse', cb);
-    },
-    reparse_last_day(cb) {
-      getRedisCountDay(redis, 'reparse', cb);
-    },
-    regcdata_last_day(cb) {
-      getRedisCountDay(redis, 'regcdata', cb);
-    },
-    requests_last_day(cb) {
-      getRedisCountDay(redis, 'request', cb);
-    },
-    requests_api_key_last_day(cb) {
-      getRedisCountDay(redis, 'request_api_key', cb);
-    },
-    steam_api_calls_last_day(cb) {
-      getRedisCountDay(redis, 'steam_api_call', cb);
-    },
-    steam_api_backfill_last_day(cb) {
-      getRedisCountDay(redis, 'steam_api_backfill', cb);
-    },
-    match_archive_read_last_day(cb) {
-      getRedisCountDay(redis, 'match_archive_read', cb);
-    },
-    match_archive_write_last_day(cb) {
-      getRedisCountDay(redis, 'match_archive_write', cb);
-    },
-    incomplete_archive_last_day(cb) {
-      getRedisCountDay(redis, 'incomplete_archive', cb);
-    },
-    build_match_last_day(cb) {
-      getRedisCountDay(redis, 'build_match', cb);
-    },
-    error_last_day(cb) {
-      getRedisCountDay(redis, '500_error', cb);
-    },
-    web_crash_last_day(cb) {
-      getRedisCountDay(redis, 'web_crash', cb);
-    },
-    fullhistory_last_day(cb) {
-      getRedisCountDay(redis, 'fullhistory', cb);
-    },
-    skip_seq_num_last_day(cb) {
-      getRedisCountDay(redis, 'skip_seq_num', cb);
-    },
-    api_hits_last_day(cb) {
-      getRedisCountDay(redis, 'api_hits', cb);
-    },
-    api_hits_ui_last_day(cb) {
-      getRedisCountDay(redis, 'api_hits_ui', cb);
-    },
-    scanner_exception_last_day(cb) {
-      getRedisCountDay(redis, 'scanner_exception', cb);
-    },
-    seqNumDelay(cb) {
+  const obj = {
+    user_players: async () => redis.zcard('visitors'),
+    tracked_players: async () => redis.zcard('tracked'),
+    matches_last_day: async () => getRedisCountDay(redis, 'added_match'),
+    matches_prev_hour: async () => getRedisCountHour(redis, 'added_match'),
+    auto_parse_last_day: async () => getRedisCountDay(redis, 'auto_parse'),
+    retriever_matches_last_day: async () => getRedisCountDay(redis, 'retriever'),
+    retriever_players_last_day: async () => getRedisCountDay(redis, 'retriever_player'),
+    parsed_matches_last_day: async () => getRedisCountDay(redis, 'parser'),
+    parse_fails_last_day: async () => getRedisCountDay(redis, 'parser_fail'),
+    meta_parsed_last_day: async () => getRedisCountDay(redis, 'meta_parse'),
+    reparse_last_day: async () => getRedisCountDay(redis, 'reparse'),
+    regcdata_last_day: async () => getRedisCountDay(redis, 'regcdata'),
+    requests_last_day: async () => getRedisCountDay(redis, 'request'),
+    requests_api_key_last_day: async () => getRedisCountDay(redis, 'request_api_key'),
+    steam_api_calls_last_day: async () => getRedisCountDay(redis, 'steam_api_call'),
+    steam_api_backfill_last_day: async () => getRedisCountDay(redis, 'steam_api_backfill'),
+    match_archive_read_last_day: async () => getRedisCountDay(redis, 'match_archive_read'),
+    match_archive_write_last_day: async () => getRedisCountDay(redis, 'match_archive_write'),
+    incomplete_archive_last_day: async () => getRedisCountDay(redis, 'incomplete_archive'),
+    build_match_last_day: async () =>  getRedisCountDay(redis, 'build_match'),
+    error_last_day: async () => getRedisCountDay(redis, '500_error'),
+    web_crash_last_day: async () => getRedisCountDay(redis, 'web_crash'),
+    fullhistory_last_day: async () => getRedisCountDay(redis, 'fullhistory'),
+    skip_seq_num_last_day: async () => getRedisCountDay(redis, 'skip_seq_num'),
+    api_hits_last_day: async () => getRedisCountDay(redis, 'api_hits'),
+    api_hits_ui_last_day: async () => getRedisCountDay(redis, 'api_hits_ui'),
+    scanner_exception_last_day: async () => getRedisCountDay(redis, 'scanner_exception'),
+    seqNumDelay: async () => {
       // It's slow to query Steam API so use the value saved by monitor
-      redis.hget('health', 'seqNumDelay', (err, data) => {
-        cb(err, data ? JSON.parse(data)?.metric : null);
-      });
+      const data = await redis.hget('health', 'seqNumDelay');
+      return data ? JSON.parse(data)?.metric : null;
     },
-    parseQueue(cb) {
+    parseQueue: async () => {
       // It's slow to count in postgres so use the value saved by monitor
-      redis.hget('health', 'parseDelay', (err, data) => {
-        cb(err, data ? JSON.parse(data)?.metric : null);
-      });
+      const data = await redis.hget('health', 'parseDelay');
+      return data ? JSON.parse(data)?.metric : null;
     },
-    fhQueue(cb) {
-      redis.llen('fhQueue', cb);
-    },
-    gcQueue(cb) {
-      redis.llen('gcQueue', cb);
-    },
-    mmrQueue(cb) {
-      redis.llen('mmrQueue', cb);
-    },
-    countsQueue(cb) {
-      redis.llen('countsQueue', cb);
-    },
-    scenariosQueue(cb) {
-      redis.llen('scenariosQueue', cb);
-    },
-    benchmarksQueue(cb) {
-      redis.llen('parsedBenchmarksQueue', cb);
-    },
-    retriever(cb) {
-      redis.zrangebyscore(
+    fhQueue: async () => redis.llen('fhQueue'),
+    gcQueue: async () => redis.llen('gcQueue'),
+    mmrQueue: async () => redis.llen('mmrQueue'),
+    countsQueue: async () => redis.llen('countsQueue'),
+    scenariosQueue: async () => redis.llen('scenariosQueue'),
+    benchmarksQueue: async () => redis.llen('parsedBenchmarksQueue'),
+    retriever: async () => {
+      const results = await redis.zrangebyscore(
         'retrieverCounts',
         '-inf',
         'inf',
-        'WITHSCORES',
-        (err, results) => {
-          if (err) {
-            return cb(err);
-          }
-          const response: any[] = [];
-          results?.forEach((result, i) => {
-            if (i % 2 === 0) {
-              response.push({
-                hostname: result.split('.')[0],
-                count: results[i + 1],
-              });
-            }
+        'WITHSCORES');
+      const response: any[] = [];
+      results?.forEach((result, i) => {
+        if (i % 2 === 0) {
+          response.push({
+            hostname: result.split('.')[0],
+            count: results[i + 1],
           });
-          return cb(err, response);
         }
-      );
+      });
+      return response;
     },
-    api_paths(cb) {
-      redis.zrangebyscore(
+    api_paths: async () => {
+      const results = await redis.zrangebyscore(
         'api_paths',
         '-inf',
         'inf',
-        'WITHSCORES',
-        (err, results) => {
-          if (err) {
-            return cb(err);
-          }
-          const response: any[] = [];
-          results?.forEach((result, i) => {
-            if (i % 2 === 0) {
-              response.push({
-                hostname: result.split('.')[0],
-                count: results[i + 1],
-              });
-            }
+        'WITHSCORES');
+      const response: any[] = [];
+      results?.forEach((result, i) => {
+        if (i % 2 === 0) {
+          response.push({
+            hostname: result.split('.')[0],
+            count: results[i + 1],
           });
-          return cb(err, response);
         }
-      );
-    },
-    load_times(cb) {
-      redis.lrange('load_times', 0, -1, (err, arr) => {
-        cb(err, generatePercentiles(arr ?? []));
       });
+      return response;
     },
-    health(cb) {
-      redis.hgetall('health', (err, result) => {
-        if (err) {
-          return cb(err);
-        }
-        const response = result || {};
-        Object.keys(response).forEach((key) => {
-          response[key] = JSON.parse(response[key]);
-        });
-        return cb(err, response);
+    load_times: async () => {
+      const arr = await redis.lrange('load_times', 0, -1);
+      return generatePercentiles(arr ?? []);
+    },
+    health: async () => {
+      const result = await redis.hgetall('health');
+      const response = result || {};
+      Object.keys(response).forEach((key) => {
+        response[key] = JSON.parse(response[key]);
       });
+      return response;
     },
-  });
+  };
+  return parallelPromise<{[P in keyof typeof obj]: Awaited<ReturnType<typeof obj[P]>>}>(obj);
 }
