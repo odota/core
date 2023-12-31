1213033bbce82071f95d4617fa5ece43c83b4225
Howard Chung <howardzchung@gmail.com>
2023-12-30 23:41:45 -0800

    Update utility.ts

--- util/utility.ts
+++ util/utility.ts
@@ -253,7 +253,7 @@ function getSteamAPIDataCallback(url: string | GetDataOptions, cb: ErrorCb) {
           body.result.error === 'Match ID not found' ||
           (body.result.status === 2 &&
             body.result.statusDetail === 'Error retrieving match data.' &&
-            Math.random() < 0.1)
+            Math.random() < 0.05)
         ) {
           // private match history or attempting to get practice match/invalid id, don't retry
           // non-retryable
