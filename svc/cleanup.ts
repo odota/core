735c2a847869a3eef1a078a377453065097b27c6
Ubuntu <howardzchung@gmail.com>
2024-01-02 09:48:41 +0000

    remove console logs

--- svc/cleanup.ts
+++ svc/cleanup.ts
@@ -5,39 +5,27 @@ import { epochWeek, invokeIntervalAsync } from '../util/utility';
 
 async function cleanup() {
   const currentWeek = epochWeek();
-  console.log(
-    'teamScenarios',
-    await db('team_scenarios')
-      .whereNull('epoch_week')
-      .orWhere(
-        'epoch_week',
-        '<=',
-        currentWeek - Number(config.MAXIMUM_AGE_SCENARIOS_ROWS),
-      )
-      .del(),
+  await db('team_scenarios')
+    .whereNull('epoch_week')
+    .orWhere(
+      'epoch_week',
+      '<=',
+      currentWeek - Number(config.MAXIMUM_AGE_SCENARIOS_ROWS),
+    )
+    .del();
+  await db('scenarios')
+    .whereNull('epoch_week')
+    .orWhere(
+      'epoch_week',
+      '<=',
+      currentWeek - Number(config.MAXIMUM_AGE_SCENARIOS_ROWS),
+    )
+    .del();
+  await db.raw(
+    "DELETE from public_matches where start_time < extract(epoch from now() - interval '6 month')::int",
   );
-  console.log(
-    'scenarios',
-    await db('scenarios')
-      .whereNull('epoch_week')
-      .orWhere(
-        'epoch_week',
-        '<=',
-        currentWeek - Number(config.MAXIMUM_AGE_SCENARIOS_ROWS),
-      )
-      .del(),
-  );
-  console.log(
-    'publicMatches',
-    await db.raw(
-      "DELETE from public_matches where start_time < extract(epoch from now() - interval '6 month')::int",
-    ),
-  );
-  console.log(
-    'heroSearch',
-    await db.raw(
-      'delete from hero_search where match_id < (select max(match_id) - 150000000 from hero_search)',
-    ),
+  await db.raw(
+    'delete from hero_search where match_id < (select max(match_id) - 150000000 from hero_search)',
   );
   return;
 }
