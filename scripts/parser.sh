7f7137274038683700b59a3a02621120fc8c86bd
Ubuntu <howardzchung@gmail.com>
2024-01-08 09:55:36 +0000

    update dc and scripts

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sudo iptables -w -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -w -A INPUT -p tcp --dport 5600 -j ACCEPT
 
 sudo docker run -d --name parseServer --restart=always --net=host --log-opt max-size=1g odota/parser
-sudo docker run -d --name=parser --restart=always --net=host --log-opt max-size=1g -e PROVIDER=gce -e PORT=80 -e ROLE=parser odota/core:latest sh -c "npm start"
 sudo docker start parseServer
+
+sudo docker run -d --name=parser --restart=always --net=host --log-opt max-size=1g -e PARSER_HOST=http://localhost:5600 -e PARSER_PARALLELISM=8 -e PROVIDER=gce -e PORT=80 -e ROLE=parser odota/core:latest sh -c "npm start"
 sudo docker start parser
--- scripts/parser_external.sh
+++ scripts/parser_external.sh
@@ -4,4 +4,3 @@ curl -sSL https://get.docker.com/ | sh
 
 sudo docker run -d --name parseServer --restart=always --net=host --log-opt max-size=1g odota/parser
 sudo docker start parseServer
-
