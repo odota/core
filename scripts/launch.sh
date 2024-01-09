0585a55582f309a070486c53e6661196d833e71c
Ubuntu <howardzchung@gmail.com>
2024-01-09 11:11:13 +0000

    try bash

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start web server by default if not in CI
-if [[ -z "$CI" ]]
+if [[ -z "${CI}" ]]
 then
     pm2 start ecosystem.config.js --only web
 fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose up -d && sleep 80 && docker ps -a && docker logs odota-core && docker exec -i odota-core sh -c 'npm run test'
+docker-compose up -d && sleep 80 && docker ps -a && docker logs odota-core && docker exec -i odota-core bash -c 'npm run test'
