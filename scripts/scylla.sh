7cc8a6012e9127e7ee28ced18a4cff6e1ceac297
Ubuntu <howardzchung@gmail.com>
2024-01-27 21:57:31 +0000

    overprovisioned

--- scripts/scylla.sh
+++ scripts/scylla.sh
@@ -8,5 +8,5 @@
 sudo mkdir -p /var/lib/scylla
 sudo mount -o discard,defaults /dev/disk/by-id/google-persistent-disk-1 /var/lib/scylla
 
-sudo docker run --name scylla --restart=always --log-opt max-size=1g -d --net=host -v /var/lib/scylla:/var/lib/scylla scylladb/scylla:5.4 --developer-mode=0
+sudo docker run --name scylla --restart=always --log-opt max-size=1g -d --net=host -v /var/lib/scylla:/var/lib/scylla scylladb/scylla:5.4 --developer-mode=0 --overprovisioned=0
 sudo docker start scylla
