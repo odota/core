2a19e7eaa20cdbbb3720717a8cf625957b93d8d0
Ubuntu <howardzchung@gmail.com>
2024-08-24 23:54:07 +0000

    update docker compose

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml up -d && sleep 80 && docker ps -a && docker logs odota-core && docker exec -i odota-core sh -c 'npm run test'
+docker compose -f docker-compose.yml up -d && sleep 80 && docker ps -a && docker logs odota-core && docker exec -i odota-core sh -c 'npm run test'
