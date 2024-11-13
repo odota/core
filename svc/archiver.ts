1761023166bf27b8f3db5beafe1d26f94b37391c
Howard Chung <howardzchung@gmail.com>
2024-11-13 02:34:51 -0800

    Update archiver.ts

--- svc/archiver.ts
+++ svc/archiver.ts
@@ -1,12 +1,12 @@
 // Cleans up old data from Cassandra and optionally archives it
-import { archiveToken, getCurrentMaxArchiveID } from '../store/getArchivedData';
+import { archivePostgresStream, getCurrentMaxArchiveID } from '../store/getArchivedData';
 
 async function start() {
   const max = await getCurrentMaxArchiveID();
   // Delete/archive a random page of matches (maybe unparsed)
   while (true) {
     try {
-      await archiveToken(max);
+      // await archivePostgresStream(max);
       await new Promise((resolve) => setTimeout(resolve, 1000));
     } catch (e) {
       console.error(e);
