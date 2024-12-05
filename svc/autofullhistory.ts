394301c58635a07ebf5d2fec30650d518ed0f77f
Ubuntu <howardzchung@gmail.com>
2024-12-05 09:19:38 +0000

    adjust sampling rate

--- svc/autofullhistory.ts
+++ svc/autofullhistory.ts
@@ -5,7 +5,7 @@ import { invokeIntervalAsync } from '../util/utility';
 
 async function doAutoFullHistory() {
   const result = await db.raw(
-    "SELECT account_id from players TABLESAMPLE SYSTEM_ROWS(100)",
+    "SELECT account_id from players TABLESAMPLE SYSTEM_ROWS(15)",
   );
   console.log(result.rows);
   await Promise.all(
@@ -19,4 +19,4 @@ async function doAutoFullHistory() {
     ),
   );
 }
-invokeIntervalAsync(doAutoFullHistory, 60 * 1000);
+invokeIntervalAsync(doAutoFullHistory, 10 * 1000);
