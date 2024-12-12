2ba50f6e539699d18b11af61df9de78aefada0e5
Ubuntu <howardzchung@gmail.com>
2024-12-12 14:29:59 +0000

    uncomment update

--- dev/copyUsage.ts
+++ dev/copyUsage.ts
@@ -9,6 +9,7 @@ async function start() {
         SELECT
         account_id,
         api_key,
+        customer_id,
         ip,
         MAX(usage_count) as max_usage
         FROM api_key_usage
@@ -27,12 +28,12 @@ async function start() {
         const curr = await db.raw('select * from api_key_usage where api_key = ? and timestamp = ?', [row.api_key, apiTimestamp]);
         console.log(row.max_usage, curr.rows[0]?.usage_count);
         // Copy the usage to the first day of the month
-        // await db.raw(`
-        // INSERT INTO api_key_usage
-        // (account_id, api_key, customer_id, timestamp, ip, usage_count) VALUES
-        // (?, ?, ?, ?, ?, ?)
-        // ON CONFLICT ON CONSTRAINT api_key_usage_pkey DO UPDATE SET usage_count = ?
-        // `, [row.account_id, row.api_key, row.customer_id, apiTimestamp, '', row.max_usage, row.max_usage]);
+        await db.raw(`
+        INSERT INTO api_key_usage
+        (account_id, api_key, customer_id, timestamp, ip, usage_count) VALUES
+        (?, ?, ?, ?, ?, ?)
+        ON CONFLICT ON CONSTRAINT api_key_usage_pkey DO UPDATE SET usage_count = ?
+        `, [row.account_id, row.api_key, row.customer_id, apiTimestamp, '', row.max_usage, row.max_usage]);
     }
 }
 start();
