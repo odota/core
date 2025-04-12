fa9686c03b3b0ea84c2cab4ac2c215bd28e41aa7
Howard Chung <howardzchung@gmail.com>
2025-04-11 17:49:47 -0700

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -175,6 +175,7 @@ CREATE TABLE IF NOT EXISTS players (
 CREATE INDEX IF NOT EXISTS players_cheese_idx on players(cheese) WHERE cheese IS NOT NULL AND cheese > 0;
 CREATE INDEX IF NOT EXISTS players_personaname_idx_gin ON players USING GIN(personaname gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS players_personaname_idx_gist ON players USING GIST(personaname gist_trgm_ops);
+CREATE INDEX IF NOT EXISTS players_full_history_time_idx ON players(full_history_time);
 
 CREATE TABLE IF NOT EXISTS player_ratings (
   PRIMARY KEY(account_id, time),
