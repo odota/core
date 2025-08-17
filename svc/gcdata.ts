5e3cb150ca8f11869e6812d625b981b46b03f02e
Howard Chung <howardzchung@gmail.com>
2025-08-17 20:01:43 +0000

    add reconcile placeholder to gcdata

--- svc/gcdata.ts
+++ svc/gcdata.ts
@@ -5,6 +5,7 @@ import config from '../config';
 import { apiFetcher } from './fetcher/getApiData';
 import { gcFetcher } from './fetcher/getGcData';
 import { getPGroup } from './util/pgroup';
+import { redisCount } from './util/utility';
 
 
 async function processGcData(job: GcDataJob) {
@@ -18,9 +19,21 @@ async function processGcData(job: GcDataJob) {
   if (!pgroup) {
     return;
   }
-  // We don't need the result, but we do want to respect the DISABLE_REGCDATA setting
   // Currently, just attempt it once and skip if failed
-  await gcFetcher.getOrFetchData(job.match_id, { pgroup });
+  const gcResult = await gcFetcher.getOrFetchData(job.match_id, { pgroup });
+  const gcMatch = gcResult.data;
+  if (gcMatch) {
+    // Reconcile anonymous players
+    // We don't need to do this twice if the match is going to be parsed
+    await Promise.all(gcMatch.players
+      .filter(p => !Boolean(pgroup[p.player_slot]?.account_id))
+      .map(async (p) => {
+        // await db.raw('INSERT INTO player_match_history(account_id, match_id, player_slot) VALUES (?, ?, ?) ON CONFLICT DO NOTHING', [p.account_id, gcMatch.match_id, p.player_slot]);
+        await redisCount('pmh_gcdata');
+      })
+    );
+  }
+
   await new Promise((resolve) => setTimeout(resolve, 1));
 }
 
