197eeed01ce2ced60fd4e6bcd7a6bf049aa759a0
Howard Chung <howardzchung@gmail.com>
2025-09-10 21:49:45 +0000

    reduce threshold

--- svc/web.ts
+++ svc/web.ts
@@ -258,7 +258,7 @@ app.get('/retrieverData', async (req, res, next) => {
         const ratio = success / reqs;
         const isLowRatio = reqs > 25 && ratio <= 0;
         // Don't add high usage logons or high fail logons
-        if (reqs < 150) {
+        if (reqs < 100) {
           await redis.sadd('retrieverDataSet', accountData[i]);
         }
       }
