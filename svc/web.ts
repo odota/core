bdf12d3be61fa23ad0d60f0333eb4884d84c98d8
Howard Chung <howardzchung@gmail.com>
2025-10-11 23:01:33 +0000

    increase limit

--- svc/web.ts
+++ svc/web.ts
@@ -246,7 +246,7 @@ app.get('/retrieverData', async (req, res, next) => {
       const ratio = success / reqs;
       const isLowRatio = reqs > 25 && ratio <= 0;
       // Don't add high usage logons or high fail logons
-      if (reqs < 150) {
+      if (reqs < 180) {
         await redis.sadd('retrieverDataSet', accountData[i]);
       }
     }
