b52a1af04747aafd4440449b4bcf37266edb51fc
Howard Chung <howardzchung@gmail.com>
2025-11-09 07:58:53 +0000

    adjust reconcile

--- svc/reconcile.ts
+++ svc/reconcile.ts
@@ -9,24 +9,21 @@ import { reconcileMatch } from './util/reconcileUtil.ts';
 import { runInLoop } from './util/utility.ts';
 
 runInLoop(async function reconcile() {
-  const result = await db.raw('SELECT match_id FROM player_match_history ORDER BY retries ASC NULLS FIRST LIMIT 3');
+  const result = await db.raw('SELECT match_id FROM player_match_history ORDER BY retries ASC NULLS FIRST LIMIT 2');
   if (!result.rows.length) {
     await new Promise(resolve => setTimeout(resolve, 1000));
     return;
   }
   // If there are multiple player rows for the match they could be the same ID
   const dedupIds = [...new Set<number>(result.rows.map((r: any) => Number(r.match_id)))];
+  console.log(dedupIds);
   await Promise.all(dedupIds.map(async (matchId) => {
     // Fetch rows for a single match (could be multiple players to fill)
     const { rows }: { rows: HistoryType[] } = await db.raw(
       'UPDATE player_match_history SET retries = coalesce(retries, 0) + 1 WHERE match_id = ? RETURNING *',
       [matchId],
     );
-    const first = rows[0];
-    if (!first) {
-      return;
-    }
-    console.log(first.match_id, first.retries);
-    await reconcileMatch(rows, first.retries >= 5);
+    console.log('%s: %s rows', matchId, rows.length);
+    await reconcileMatch(rows, rows[0]?.retries >= 5);
   }));
 }, 0);
