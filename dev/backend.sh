2cfba9d9f39accd9e55c733d3c550b53dd69c114
Howard Chung <howardzchung@gmail.com>
2025-11-19 09:57:52 +0000

    add cassandra to backend

--- dev/backend.sh
+++ dev/backend.sh
@@ -3,15 +3,21 @@ sudo iptables -w -A INPUT -p tcp --dport 80 -j ACCEPT
 
 sudo mkdir -p /var/lib/redis
 sudo mount -o discard,defaults /dev/disk/by-id/google-disk-redis /var/lib/redis
-# Disable --restart=always on redis if using stateful boot disk
+# Disable --restart=always if using stateful boot disk
 # Otherwise we may restart before mount and then overwrite saved data with empty
 sudo docker run -d --name redis --log-opt max-size=1g --net=host -v /var/lib/redis:/data redis:7 /usr/local/bin/redis-server
 sudo docker start redis
 
 sudo mkdir -p /var/lib/postgresql/data
 sudo mount -o discard,defaults /dev/disk/by-id/google-disk-postgres-4 /var/lib/postgresql/data
-sudo docker run -d --name postgres --restart=always --log-opt max-size=1g -e "PGDATA=/var/lib/postgresql/data/pgdata" -v /var/lib/postgresql/data:/var/lib/postgresql/data --net=host postgres:9.5
+sudo docker run -d --name postgres --log-opt max-size=1g -e "PGDATA=/var/lib/postgresql/data/pgdata" -v /var/lib/postgresql/data:/var/lib/postgresql/data --net=host postgres:9.5
 sudo docker start postgres
+
+sudo mkdir -p /var/lib/cassandra
+sudo mount -o discard,defaults /dev/disk/by-id/google-disk-cassandra-3 /var/lib/cassandra
+#-e CASSANDRA_SEEDS=cassandra-1
+sudo docker run --name cassandra --log-opt max-size=1g -d --net=host -e CASSANDRA_NUM_TOKENS=256 -v /var/lib/cassandra:/var/lib/cassandra cassandra:5
+sudo docker start cassandra
 
 sudo docker run -d --name backend --restart=always --net=host --log-opt max-size=1g -e PROVIDER=gce -e GROUP=backend -e FRONTEND_PORT=80 odota/core:latest sh -c "npm start"
 sudo docker start backend
--- dev/cassandra.sh
+++ dev/cassandra.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-#curl -sSL https://get.docker.com/ | sh
-#sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-persistent-disk-1
+#sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-disk-cassandra-3
+sudo iptables -I INPUT -p tcp --dport 7000 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 9042 -j ACCEPT
 
 sudo mkdir -p /var/lib/cassandra
-sudo mount -o discard,defaults /dev/disk/by-id/google-persistent-disk-1 /var/lib/cassandra
+sudo mount -o discard,defaults /dev/disk/by-id/google-disk-cassandra-3 /var/lib/cassandra
 
 #remove seed if initial node
-sudo docker run --name cassandra --restart=always --log-opt max-size=1g -d --net=host -v /var/lib/cassandra:/var/lib/cassandra -e CASSANDRA_SEEDS=cassandra-1 cassandra:4
+#-e CASSANDRA_SEEDS=cassandra-1
+sudo docker run --name cassandra --restart=always --log-opt max-size=1g -d --net=host -v /var/lib/cassandra:/var/lib/cassandra -e CASSANDRA_NUM_TOKENS=256 cassandra:5
+
 sudo docker start cassandra
