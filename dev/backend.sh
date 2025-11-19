84e83f02dc0863b09deded4b4a0ee6338205b7b5
Howard Chung <howardzchung@gmail.com>
2025-11-19 08:11:06 +0000

    update backend

--- dev/backend.sh
+++ dev/backend.sh
@@ -9,7 +9,7 @@ sudo docker start redis
 sudo mkdir -p /var/lib/postgresql/data
 sudo mount -o discard,defaults /dev/disk/by-id/google-disk-postgres-4 /var/lib/postgresql/data
 sudo docker run -d --name postgres --restart=always --log-opt max-size=1g -e "PGDATA=/var/lib/postgresql/data/pgdata" -v /var/lib/postgresql/data:/var/lib/postgresql/data --net=host postgres:9.5
+sudo docker start postgres
 
 sudo docker run -d --name backend --restart=always --net=host --log-opt max-size=1g -e PROVIDER=gce -e GROUP=backend -e FRONTEND_PORT=80 odota/core:latest sh -c "npm start"
 sudo docker start backend
-sudo docker logs -f backend
