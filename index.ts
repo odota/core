1cf0a5fb90ce6f511e9cb4fb9b75f73d8ec26e83
Howard Chung <howardzchung@gmail.com>
2026-02-04 08:04:10 +0000

    only in prod

--- index.ts
+++ index.ts
@@ -28,8 +28,10 @@ if (process.env.ROLE) {
     await import("./svc/" + process.env.ROLE + ".ts");
   } catch (e: any) {
     console.error(e);
-    // Wait to avoid excessively fast restart loop if services aren't up yet
-    await new Promise((resolve) => setTimeout(resolve, 500));
+    if (process.env.NODE_ENV === "production") {
+      // Wait to avoid excessively fast restart loop if services aren't up yet
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
     process.exit(1);
   }
 } else if (process.env.GROUP) {
