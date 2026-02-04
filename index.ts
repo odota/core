7e3b49661766dce50a07ce5cbe4c4668f4671ea1
Howard Chung <howardzchung@gmail.com>
2026-02-04 08:19:27 +0000

    add back setinterval

--- index.ts
+++ index.ts
@@ -28,10 +28,8 @@ if (process.env.ROLE) {
     await import("./svc/" + process.env.ROLE + ".ts");
   } catch (e: any) {
     console.error(e);
-    if (process.env.NODE_ENV === "production") {
-      // Wait to avoid excessively fast restart loop if services aren't up yet
-      await new Promise((resolve) => setTimeout(resolve, 500));
-    }
+    // Wait to avoid excessively fast restart loop if services aren't up yet
+    await new Promise((resolve) => setTimeout(resolve, 500));
     process.exit(1);
   }
 } else if (process.env.GROUP) {
@@ -46,10 +44,11 @@ if (process.env.ROLE) {
 } else {
   console.log("starting web");
   execSync("pm2 start ecosystem.config.js --only web");
-  // setInterval(
-  //   () => {
-  //     execSync('pm2 flush');
-  //   },
-  //   12 * 60 * 60 * 1000,
-  // );
+  // Start an interval to keep process running
+  setInterval(
+    () => {
+      // execSync('pm2 flush');
+    },
+    12 * 60 * 60 * 1000,
+  );
 }
