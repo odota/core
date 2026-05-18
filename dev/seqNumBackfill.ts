d289a1abc34c2334f4812f0c118bf11ae9b5133f
Howard Chung <howardzchung@gmail.com>
2026-05-18 20:30:45 +0000

    try backfill

--- dev/seqNumBackfill.ts
+++ dev/seqNumBackfill.ts
@@ -1,3 +1,3 @@
 import { apiFetcher } from "../svc/fetcher/allFetchers.ts";
 
-await apiFetcher.getOrFetchData(8549411550, { seqNumBackfill: true });
+await apiFetcher.getOrFetchData(8814800807, { seqNumBackfill: true });
